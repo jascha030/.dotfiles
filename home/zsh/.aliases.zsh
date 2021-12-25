bc65dba78bef36cedaf1853d090f9b544d9fdade
Jascha030 <contact@jaschavanaalst.nl>
2021-12-25 02:04:02 +0100

    🔥 Cleanup on ile alias... ilias?

--- home/zsh/.aliases.zsh
+++ home/zsh/.aliases.zsh
@@ -1,10 +1,37 @@
 # Ali(J)asses
+
+# Edit shortcuts
 alias edit="${EDITOR} $1"
 alias zshconfig="edit $DOTFILES/.zshrc"
 alias e:zshrc="zshconfig"
-alias e:aliases="edit ${DOTZSH}/aliases.zsh"
-alias e:path="edit ${DOTZSH}/path.zsh"
-alias e:tmux="edit ${DOTCONFIGDIR}/tmux/tmux.conf"
+alias e:aliases="edit ${DOTZSH}/.aliases.zsh"
+alias e:path="edit ${HOME}/.path.zsh"
+
+# Terminal / general
+alias rr="source $HOME/.zshrc"
+alias x="exit"
+alias c="clear"
+
+
+# Navigation
+alias ll="lsd -Ahl --color --group-dirs=first"
+alias lc="ll | lolcat"
+alias colorls="ll"
+alias llb="ll | bat"
+alias lr="colorls --tree=5"
+
+# Shortcuts to dirs
+alias dotfiles="cd ${DOTFILES}"
+
+
+# Utils
+
+# Copy Broodje Ben to clipboard.
+alias cpbroodje="echo \"38. Witte Italiaanse Bol met Gerookte Kip, Avocado mayonaise met extra avocado en een blikkie cola. - €8,50\" | pbcopy"
+
+# Start screensaver
+alias scrsvr="open -a ScreenSaverEngine"
+
 
 # Bat instead of cat
 alias cat="bat"
@@ -12,36 +39,15 @@ alias ogcat="\cat"
 alias fpb="fzf --preview 'bat --color=always --style=numbers --line-range=:500 {}'"
 alias readme="bat ${PWD}/README.md"
 
-# Navigation
-alias rr="source $HOME/.zshrc"
-alias x="exit"
-alias q="exit"
-alias c="clear"
-
-# Copy Broodje Ben to clipboard.
-alias cpbroodje="echo \"38. Witte Italiaanse Bol met Gerookte Kip, Avocado mayonaise met extra avocado en een blikkie cola. - €8,50\" | pbcopy"
-
-alias scrsvr="open -a ScreenSaverEngine"
-alias ssv="open -a ScreenSaverEngine"
-
-alias ll="lsd -Ahl --color --group-dirs=first"
-alias lc="ll | lolcat"
-alias colorls="ll"
-# alias ll="colorls -Ahl --color --group-directories-first"
-alias llb="ll | bat"
-alias lr="colorls --tree=5"
-
-alias confdir="cd $HOME/.config"
-alias dotfiles="cd $DOTFILES"
-
 # Clean and update Brew
 alias brewup="brew update && brew outdated; brew upgrade && brew cleanup; brew doctor"
+
 # Faster Time Machine Backup
 alias tm-auto="sudo tmutil startbackup --auto; tmutil status"
+
 # Remove valet.sock and restart.
 alias valetrm="rm $HOME/.config/valet/valet.sock"
 alias valetr="valetrm && valet restart" # Make a symbolic link in current dir.
-alias linkdir="ln -s $PWD/"
 
 # Chrome with just a little bit less RAMpage.
 alias chrome="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --process-per-site"
@@ -50,36 +56,19 @@ alias chromi="chrome -incognito"
 
 # Open current dir in...
 alias kraken='$(open gitkraken://repo/$PWD)' # Gitkraken
+
 alias pstorm="phpstorm ." # Phpstorm
 alias wstorm="webstorm ." # Webstorm
-
-# Navigation
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
+alias idea="idea ."       # Intellij IDEA
 
 # Working dir
 alias dev="cd ${HOME}/.development/Projects"
 alias library="cd $HOME/Library"
 
-alias wwwd="cd $HOME/.development/www"
-alias remotesites="cd $HOME/.development/www/Remotes"
-alias sbsites="cd $HOME/.development/www/socialbrothers"
-alias projx="cd ${HOME}/.development/Projects"
-
 alias cache:spotify="cd ${HOME}/Library/Application Support/spotify"
 
 # Start composer project start-composerpr
 alias cmprj="start-composerpr"
-
-# PHP
-alias cc="&& composer "
-alias cpsrcl="composer clear-cache"
-alias cpsrrm="rm -rf vendor/ composer.lock"
-alias cpsrfrup="cpscl cc u"
-alias cpsrfri="cpscl cc i"
-alias cpsrfresh="comprm && cpsfri"
-alias cpsndev="composer install --no-dev"
 
 # Git
 alias gst="git status"
@@ -112,9 +101,10 @@ alias shrug="echo '¯\_(ツ)_/¯' | pbcopy"
 alias ccdir="echo `echo $PWD` | pbcopy"
 
 # Wordpress related aliases
-alias c:wpindex="echo \"<?php\n//silence is golden\" >index.php"
 alias clone:sb-starter="git clone git@bitbucket.org:socialbrothers/wordpress-starter-theme.git"
 alias clone:wp="git clone git@github.com:wordpress/wordpress"
+
+alias c:wpindex="echo \"<?php\n//silence is golden\" >index.php"
 alias vim="nvim"
 alias composerreset="rm -rf vendor/ composer.lock && composer clear-cache && composer install"
 alias checkitalics="echo -e '\e[3mitalic\e[23m'"
