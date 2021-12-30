08d59889a5d5d9fbbfdfb52b3cd55e33fa6d4dba
Jascha030 <contact@jaschavanaalst.nl>
2021-12-30 22:59:31 +0100

    ✨ lsp module init

---
+++ nvim/.config/nvim/lua/lsp/init.lua
@@ -0,0 +1,8 @@
+-- Check if lspconfig can be loaded or return
+local status_ok _ = pcall(require, 'lspconfig')
+if not status_ok then
+    return
+end
+
+require 'lsp.installer'
+require 'lsp.handlers'.setup()
