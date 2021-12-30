7404dd2509cf6e191e0cde420ed049a307da43c8
Jascha030 <contact@jaschavanaalst.nl>
2021-12-30 22:58:39 +0100

    ✨ Add lsp installer config with server config looptyloop

---
+++ nvim/.config/nvim/lua/lsp/installer.lua
@@ -0,0 +1,23 @@
+-- Check if nvim-lsp-installer can be loaded or return
+local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
+if not status_ok then
+    return
+end
+
+lsp_installer.on_server_ready(function(server)
+    local opts = {
+        on_attach = require'lsp.handlers'.on_attach,
+        capabilities = require'lsp.handlers'.capabilities,
+    }
+
+    local custom_server_options = { 'intelephense', }
+
+    for server_name in custom_server_options do
+        if server.name == name then
+            local server_opts = require('lsp.settings' .. server_name)
+            opts = vim.tbl_deep_extend("force", server_opts, opts)
+        end
+    end
+
+    server:setup(opts)
+    end)
