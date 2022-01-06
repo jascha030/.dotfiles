1664614fd0c05ffdcc85bfecdb20355120e0ef14
Jascha030 <contact@jaschavanaalst.nl>
2022-01-06 11:00:38 +0100

    🔥 Remove git aliases I never use

--- zsh/.aliases.zsh
+++ zsh/.aliases.zsh
@@ -70,25 +70,6 @@ alias cache:spotify="cd ${HOME}/Library/Application Support/spotify"
 # Start composer project start-composerpr
 alias cmprj="start-composerpr"
 
-# Git
-alias gst="git status"
-alias gb="git branch"
-alias gc="git checkout"
-alias gl="git log --oneline --decorate --color"
-
-alias amend="git add . && git commit --amend --no-edit"
-alias commit="git add . && git commit -m"
-alias diff="git diff"
-alias force="git push --force"
-alias nuke="git clean -df && git reset --hard"
-alias pop="git stash pop"
-alias pull="git pull"
-alias push="git push"
-alias resolve="git add . && git commit --no-edit"
-alias stash="git stash -u"
-alias unstage="git restore --staged ."
-alias wip="commit wip"
-
 # Suffix Aliases
 alias -s php=pstorm
 alias -s {.zshrc,zsh,sh,json,yaml,.env,cs,ts,html}=nvim
