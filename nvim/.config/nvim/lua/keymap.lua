15d08187692218d9f3ae27a0a84a61e19dce7122
Jascha030 <contact@jaschavanaalst.nl>
2022-01-09 00:09:32 +0100

    🔧 Add Telescope keymaps

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -5,4 +5,12 @@ local map = api.nvim_set_keymap;
 map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
 map('n', '<leader>f', ":lua require('plugins.telescope').project_files()<CR>", {noremap = true, silent = true})
 
+--  Telescope
+map('n', 'ss', ':Telescope<CR>', {noremap = true})
+map('n', 'ff', ":lua require('telescope.builtin').find_files()<CR>", {noremap = true})
+map('n', 'FF', ":lua require('telescope.builtin').file_browser()<CR>", {noremap = true})
+map('n', 'fg', ":lua require('telescope.builtin').live_grep()<CR>", {noremap = true})
+
+-- Lsp
+map('n', '<C-l>', ":lua vim.lsp.buf.formatting()<CR>", {noremap = true})
 
