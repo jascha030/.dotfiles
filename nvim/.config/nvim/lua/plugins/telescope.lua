6351df5bbe12b6184700113bd0a66c822d95efcc
Jascha030 <contact@jaschavanaalst.nl>
2022-01-09 00:32:53 +0100

    ➕ Add extensions for telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,13 +4,10 @@ require('telescope').setup({
     defaults = {
         prompt_prefix = " > ",
         color_devicons = true,
-
         file_sorter =  require('telescope.sorters').get_fzy_sorter,
         generic_sorter =  require('telescope.sorters').get_generic_fuzzy_sorter,
-        
         use_less = true,
         set_env = { ['COLORTERM'] = 'truecolor' },
-
         file_previewer = require('telescope.previewers').vim_buffer_cat.new,
         grep_previewer = require('telescope.previewers').vim_buffer_vimgrep.new,
         qflist_previewer = require('telescope.previewers').vim_buffer_qflist.new
@@ -19,9 +16,12 @@ require('telescope').setup({
         fzy_native = {
             override_generic_sorter = false,
             override_file_sorter = true,
+        },
+        file_browser = {
         }
     }
 })
 
-require('telescope').load_extension('fzy_native')
+require'telescope'.load_extension('fzy_native')
+require'telescope'.load_extension('file_browser')
 
