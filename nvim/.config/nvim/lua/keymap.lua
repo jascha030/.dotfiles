a8737c7ebefb58d84ec4e937c4c6917621931d3e
Jascha030 <contact@jaschavanaalst.nl>
2022-01-09 00:08:58 +0100

    🔥 Remove nvim-compe keymaps

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -5,10 +5,4 @@ local map = api.nvim_set_keymap;
 map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
 map('n', '<leader>f', ":lua require('plugins.telescope').project_files()<CR>", {noremap = true, silent = true})
 
--- Compe
-map('i', '<C-Space>', [[ compe#complete() ]], {noremap = true, silent = true, expr = true})
-map('i', '<CR>', [[ compe#confirm('<CR>') ]], {noremap = true, silent = true, expr = true})
-map('i', '<C-e>', [[ compe#close('<C-e>') ]], {noremap = true, silent = true, expr = true})
-map('i', '<C-f>', [[ compe#scroll({ 'delta': +4 }) ]], {noremap = true, silent = true, expr = true})
-map('i', '<C-d>', [[ compe#scroll({ 'delta': -4 }) ]], {noremap = true, silent = true, expr = true})
 
