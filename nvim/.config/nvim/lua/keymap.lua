f8c033ed8344d707887bd8224575cb3b6d4df364
Jascha030 <contact@jaschavanaalst.nl>
2022-01-09 14:03:44 +0100

    🔧 Add terminal mode keymappings

--- nvim/.config/nvim/lua/keymap.lua
+++ nvim/.config/nvim/lua/keymap.lua
@@ -1,16 +1,27 @@
 local api = vim.api
 local map = api.nvim_set_keymap;
 
+local opts =  {noremap = true}
+
 -- Nvim Tree
 map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true})
 map('n', '<leader>f', ":lua require('plugins.telescope').project_files()<CR>", {noremap = true, silent = true})
 
 --  Telescope
-map('n', 'ss', ':Telescope<CR>', {noremap = true})
-map('n', 'ff', ":lua require('telescope.builtin').find_files()<CR>", {noremap = true})
-map('n', 'FF', ":lua require('telescope.builtin').file_browser()<CR>", {noremap = true})
-map('n', 'fg', ":lua require('telescope.builtin').live_grep()<CR>", {noremap = true})
+map('n', 'ss', ':Telescope<CR>', opts)
+map('n', 'ff', ":lua require('telescope.builtin').find_files()<CR>", opts)
+map('n', 'FF', ":lua require('telescope.builtin').file_browser()<CR>", opts)
+map('n', 'fg', ":lua require('telescope.builtin').live_grep()<CR>", opts)
 
 -- Lsp
-map('n', '<C-l>', ":lua vim.lsp.buf.formatting()<CR>", {noremap = true})
+map('n', '<C-l>', ":lua vim.lsp.buf.formatting()<CR>", opts)
+
+-- Other
+map('v', '<C-c>', ':OSCYank<CR>', opts)
+
+-- Terminal
+map('n', '<C-t>', ':terminal<CR>i', opts)
+map('t', '<Esc>', '<C-\\><C-n>', opts)
+map('t', '<M-[>', '<Esc>', opts)
+map('t', '<C-v><Esc>', '<Esc>', opts)
 
