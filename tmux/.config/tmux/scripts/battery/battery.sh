9656c7db8a0164f058ae96dbdf2b14ca2e8b7533
Jascha030 <contact@jaschavanaalst.nl>
2022-01-16 15:53:44 +0100

    💄 Add gradually changing battery glyphs for each tenth percentage of battery power.

--- tmux/.config/tmux/scripts/battery/battery.sh
+++ tmux/.config/tmux/scripts/battery/battery.sh
@@ -1,2 +1,27 @@
 #!/usr/bin/env bash
-pmset -g batt | grep -Eo '\d{1,3}%'
+BATT_STAT=$(pmset -g batt | grep -Eo '\d{1,3}%' | cut -c -3)
+
+if (($BATT_STAT < 10)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 20)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 30)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 40)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 50)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 60)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 70)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 80)); then
+  BATT_ICON=""
+elif (($BATT_STAT < 90)); then
+  BATT_ICON=""
+else
+  BATT_ICON=""
+fi
+
+echo "$BATT_ICON $BATT_STAT%"
+
