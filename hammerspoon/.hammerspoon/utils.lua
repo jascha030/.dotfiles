8d067d6e41921d91c556c00e80ae769aa0c8bc54
Jascha030 <contact@jaschavanaalst.nl>
2022-01-27 11:54:51 +0100

    🚧 Create utility function to merge lua tables

---
+++ hammerspoon/.hammerspoon/utils.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.tableMerge = function(t1, t2)
+    for k,v in pairs(t2) do
+        if type(v) == "table" then
+            if type(t1[k] or false) == "table" then
+                M.tableMerge(t1[k] or {}, t2[k] or {})
+            else
+                t1[k] = v
+            end
+        else
+            t1[k] = v
+        end
+    end
+    return t1
+end
+
+return M
+
