7fee4115ae583475f087833f068594837c43cebe
Jascha030 <contact@jaschavanaalst.nl>
2022-02-20 19:33:39 +0100

    🎨 update/cleanup packer config

--- nvim/.config/nvim/lua/packer-plugins.lua
+++ nvim/.config/nvim/lua/packer-plugins.lua
@@ -1,14 +1,14 @@
 return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
+	use { 'wbthomason/packer.nvim' }
   use {'lewis6991/impatient.nvim', config = [[require('impatient')]]}
 	use {
 		'neovim/nvim-lspconfig',
     'williamboman/nvim-lsp-installer',
 	}
-	use 'marko-cerovac/material.nvim'
+	use { 'marko-cerovac/material.nvim' }
 	use { 'nvim-treesitter/nvim-treesitter', irun = ':TSUpdate' }
-	use 'nvim-treesitter/playground'
-	use 'onsails/lspkind-nvim'
+	use { 'nvim-treesitter/playground' }
+	use { 'onsails/lspkind-nvim' }
 	use { 'phaazon/hop.nvim', branch = 'v1' }
   use {
 		'hoob3rt/lualine.nvim',
@@ -21,8 +21,8 @@ return require('packer').startup(function()
         require'alpha'.setup(require'alpha.themes.startify'.opts)
     end
   }
-	use 'kyazdani42/nvim-tree.lua'
-  use 'norcalli/nvim-colorizer.lua'
+	use { 'kyazdani42/nvim-tree.lua' }
+  use { 'norcalli/nvim-colorizer.lua' }
   use {
     'nvim-telescope/telescope.nvim',
       requires = {
@@ -33,26 +33,26 @@ return require('packer').startup(function()
   use { 'nvim-telescope/telescope-file-browser.nvim' }
   use { 'nvim-telescope/telescope-fzy-native.nvim' }
   -- cmp
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
+  use { 'hrsh7th/nvim-cmp' }
+  use { 'hrsh7th/cmp-nvim-lsp' }
   use { 'L3MON4D3/LuaSnip' }
   use { 'saadparwaiz1/cmp_luasnip' }
-  use 'tjdevries/colorbuddy.nvim'
-  use 'karb94/neoscroll.nvim'
-  use 'iamcco/markdown-preview.nvim'
-  use 'ojroques/vim-oscyank'
-  use 'folke/tokyonight.nvim'
-  use 'ncm2/ncm2'
-  use 'wakatime/vim-wakatime'
-  use 'voldikss/vim-floaterm'
-  use 'petertriho/nvim-scrollbar'
-  use 'terryma/vim-multiple-cursors'
-  use 'nvim-treesitter/nvim-treesitter-textobjects'
+  use { 'tjdevries/colorbuddy.nvim' }
+  use { 'karb94/neoscroll.nvim' }
+  use { 'iamcco/markdown-preview.nvim' }
+  use { 'ojroques/vim-oscyank' }
+  use { 'folke/tokyonight.nvim' }
+  use { 'ncm2/ncm2' }
+  use { 'wakatime/vim-wakatime' }
+  use { 'voldikss/vim-floaterm' }
+  use { 'petertriho/nvim-scrollbar' }
+  -- use { 'terryma/vim-multiple-cursors' }
+  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
   use({
     'noib3/nvim-cokeline',
     requires = 'kyazdani42/nvim-web-devicons', -- If you want devicons
-    config = function()
-      require('cokeline').setup()
-    end
+--    config = function()
+--      require('cokeline').setup()
+--    end
   })
 end)
