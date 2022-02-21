55b319237f4525f4da1b5ea3f79766ee7c9fdef6
Jascha030 <contact@jaschavanaalst.nl>
2022-02-21 09:28:35 +0100

    🎨 Cleanup main hammerspoon module

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -1,47 +1,21 @@
-local spaces = require("hs._asm.undocumented.spaces")
-local doubleTap = require("doubleTap")
-local utils = require("utils")
-local settings = require("settings")
+local termUtils = require 'term'
+local doubleTap = require 'doubleTap'
+local settings  = require 'settings'
 
+-- Load settings
 settings:setup({})
 
+-- Terminal Emulator App (default: 'Alacritty.app')
+local termApp = settings:get('termApp')
+
+-- Default screen
+local builtinScreen = settings:get('builtInScreen')
+
+
+-- DoubleTap
+--    key = cmd,
+--    map = term.toggle
 doubleTap.action = function ()
-  local APP_NAME = settings:get('termApp')
-  local termApp = hs.application.get(APP_NAME)
-  local builtinScreen = settings:get('builtInScreen')
-
-  if termApp ~= nil and termApp:isFrontmost() then
-    termApp:hide()
-  else
-    local space = spaces.activeSpace()
-    local mainScreen = hs.screen.find(spaces.mainScreenUUID())
-
-    if termApp == nil and hs.application.launchOrFocus(APP_NAME) then
-      local appWatcher = nil
-
-      appWatcher = hs.application.watcher.new(function(name, event, app)
-        if event == hs.application.watcher.launched and name == APP_NAME then
-          app:hide()
-
-          utils.window.move(app, space, mainScreen, builtinScreen)
-          appWatcher:stop()
-        end
-      end)
-
-      appWatcher:start()
-    end
-
-    if termApp ~= nil then
-      utils.window.move(termApp, space, mainScreen, builtinScreen)
-    end
-  end
-
-  -- hs.window.filter.default:subscribe(hs.window.filter.windowFocused, function(window, appName)
-    -- local termApp = hs.application.get(termApp)
-    -- if termApp ~= nil then
-    -- uncomment to unfocus
-    -- termApp:hide()
-    -- end
-  -- end)
+  termUtils.toggle(termApp, builtinScreen)
 end
 
