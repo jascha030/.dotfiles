f4601282240da6e1b0082037a5b597d2c06677fd
Jascha030 <contact@jaschavanaalst.nl>
2022-02-21 09:28:18 +0100

    🎨 Abstract terminal related window function and started alacritty specific module

---
+++ hammerspoon/.hammerspoon/term/alacritty.lua
@@ -0,0 +1,4 @@
+local spawnNvim = function (instance)
+  hs.execute("alacritty -o font.size=20 -o font.normal.family='Dank Mono'")
+end
+
---
+++ hammerspoon/.hammerspoon/term/init.lua
@@ -0,0 +1,38 @@
+local spaces = require 'hs._asm.undocumented.spaces'
+local utils = require 'utils'
+
+local toggle = function (appName, screen)
+  local instance = hs.application.get(appName)
+
+  if instance ~= nil and instance:isFrontmost() then
+    instance:hide()
+  else
+    local space = spaces.activeSpace()
+    local mainScreen = hs.screen.find(spaces.mainScreenUUID())
+
+    if instance == nil and hs.application.launchOrFocus(appName) then
+      local appWatcher = nil
+
+      appWatcher = hs.application.watcher.new(function(name, event, app)
+        if event == hs.application.watcher.launched and name == appName then
+          app:hide()
+
+          utils.window.move(app, space, mainScreen, screen)
+          appWatcher:stop()
+        end
+      end)
+
+      appWatcher:start()
+    end
+
+    if instance ~= nil then
+      utils.window.move(instance, space, mainScreen, screen)
+    end
+  end
+end
+
+return {
+  toggle = toggle,
+  alacritty = require 'term.alacritty'
+}
+
