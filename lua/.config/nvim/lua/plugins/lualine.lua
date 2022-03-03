e8dcfc2ae3209a1250b982c593edb4e78544a2fd
Jascha030 <contact@jaschavanaalst.nl>
2022-03-03 12:53:35 +0100

    🐛 Fix lualine seperators.

--- lua/.config/nvim/lua/plugins/lualine.lua
+++ lua/.config/nvim/lua/plugins/lualine.lua
@@ -3,15 +3,26 @@ require'lualine'.setup {
     icons_enabled = true,
     --theme = 'palenight',
     theme = 'tokyonight',
-	component_separators = {'', ''},
-    section_separators = {'', ''},
-    disabled_filetypes = {}
+	  component_separators = {
+      left = '',
+      right = ''
+    },
+    section_separators = {
+      left = '', 
+      right = ''
+    },
+    disabled_filetypes = {
+    }
   },
   sections = {
     lualine_a = {'mode'},
     lualine_b = {'branch'},
     lualine_c = {'filename'},
-    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_x = {
+      'encoding',
+      'fileformat',
+      'filetype'
+    },
     lualine_y = {'progress'},
     lualine_z = {'location'}
   },
