9cb9d01369864eb4876bd2daea7f045554f007f4
Jascha030 <contact@jaschavanaalst.nl>
2022-03-15 21:53:37 +0100

    🚨 Added stylua and on save formatting 🙏🏻

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 require 'plugins.treesitter'
 require 'plugins.cmp'
 require 'plugins.lualine'
+require 'plugins.null-ls'
 require 'plugins.nvim-colorizer'
 require 'plugins.nvim-tree'
 require 'plugins.markdown-preview'
