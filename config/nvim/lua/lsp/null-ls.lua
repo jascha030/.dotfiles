cf59fc44a1399576f6c4b4059691880b5d43eb8d
Jascha030 <contact@jaschavanaalst.nl>
2022-03-18 22:03:31 +0100

    🔧 Add new stylua.toml to null-ls arguments

--- config/nvim/lua/lsp/null-ls.lua
+++ config/nvim/lua/lsp/null-ls.lua
@@ -1,13 +1,14 @@
-local null_ls = require("null-ls")
+local null_ls = require('null-ls')
 local fmt = null_ls.builtins.formatting
 
 local sources = {
-  fmt.stylua,
-  null_ls.builtins.diagnostics.eslint,
-  null_ls.builtins.completion.spell
+    fmt.stylua.with({
+        extra_args = { '--config-path', vim.fn.expand('~/.config/stylua.toml') },
+    }),
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.completion.spell,
 }
 
 null_ls.setup({
-	sources = sources
+    sources = sources,
 })
-
