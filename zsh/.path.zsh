ee8df80c7b559c0bbd2345a8f8133eb0eaa979c3
Jascha030 <contact@jaschavanaalst.nl>
2022-04-05 12:39:02 +0200

    🔧 Add yarn to path

--- zsh/.path.zsh
+++ zsh/.path.zsh
@@ -6,6 +6,7 @@ export PATH="$HOME/.composer/vendor/bin:$PATH"
 
 # Load Node global installed binaries
 export PATH="$HOME/.node/bin:$PATH"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
