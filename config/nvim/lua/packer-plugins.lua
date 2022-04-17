11f157b3dfd03573603987a9759cd931af9820ee
Jascha030 <contact@jaschavanaalst.nl>
2022-04-17 03:07:01 +0200

    🎨 Re-organize packer plugins.

--- config/nvim/lua/packer-plugins.lua
+++ config/nvim/lua/packer-plugins.lua
@@ -1,28 +1,45 @@
 return require('packer').startup(function(use)
+    -- Packer
     use({ 'wbthomason/packer.nvim' })
+
+    -- Impatient should be first plugin after packer.
     use({ 'lewis6991/impatient.nvim', config = [[require('impatient')]] })
+
+    -- Language/syntax
+    --
+    -- LSP
     use({
         'neovim/nvim-lspconfig',
         'williamboman/nvim-lsp-installer',
     })
-    use({ 'marko-cerovac/material.nvim' })
-    use({ 'nvim-treesitter/nvim-treesitter', irun = ':TSUpdate' })
-    use({ 'nvim-treesitter/playground' })
     use({ 'onsails/lspkind-nvim' })
-    use({ 'phaazon/hop.nvim', branch = 'v1' })
-    use({
-        'hoob3rt/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    })
+    use({ 'jose-elias-alvarez/null-ls.nvim' })
+
+    -- Cmp
+    use({ 'hrsh7th/nvim-cmp' })
+    use({ 'hrsh7th/cmp-nvim-lsp' })
+    use({ 'hrsh7th/cmp-path' })
+    use({ 'hrsh7th/cmp-buffer' })
+    use({ 'hrsh7th/cmp-vsnip' })
+
+    -- Snippet engine/completion
+    use({ 'L3MON4D3/LuaSnip' })
+    use({ 'saadparwaiz1/cmp_luasnip' })
+    use({ 'simrat39/rust-tools.nvim' })
+    use({ 'ncm2/ncm2' })
+
+    -- Visual/UI components
+    --
+    -- Startup screen
     use({
         'goolord/alpha-nvim',
-        requires = { 'kyazdani42/nvim-web-devicons' },
+        requires = 'kyazdani42/nvim-web-devicons',
         config = function()
             require('alpha').setup(require('alpha.themes.startify').opts)
         end,
     })
-    use({ 'kyazdani42/nvim-tree.lua' })
-    use({ 'norcalli/nvim-colorizer.lua' })
+
+    -- Telescope
     use({
         'nvim-telescope/telescope.nvim',
         requires = {
@@ -32,38 +49,36 @@ return require('packer').startup(function(use)
     })
     use({ 'nvim-telescope/telescope-file-browser.nvim' })
     use({ 'nvim-telescope/telescope-fzy-native.nvim' })
-    -- cmp
-    use({ 'hrsh7th/nvim-cmp' })
-    use({ 'hrsh7th/cmp-nvim-lsp' })
-    use({ 'hrsh7th/cmp-path' })
-    use({ 'hrsh7th/cmp-buffer' })
-    use({ 'hrsh7th/cmp-vsnip' })
-    use({ 'L3MON4D3/LuaSnip' })
-    use({ 'saadparwaiz1/cmp_luasnip' })
-    use({ 'tjdevries/colorbuddy.nvim' })
-    use({ 'simrat39/rust-tools.nvim' })
-    use({ 'mfussenegger/nvim-dap' })
-    use({ 'karb94/neoscroll.nvim' })
-    use({
-        'iamcco/markdown-preview.nvim',
-        config = function()
-            require('plugins.markdown-preview')
-        end,
-        run = 'cd app && pnpm i',
-    })
-    use({ 'ojroques/vim-oscyank' })
-    use({ 'folke/tokyonight.nvim' })
-    use({ 'ncm2/ncm2' })
-    use({ 'wakatime/vim-wakatime' })
-    use({ 'voldikss/vim-floaterm' })
-    use({ 'petertriho/nvim-scrollbar' })
-    -- use { 'terryma/vim-multiple-cursors' }
-    use({ 'p00f/nvim-ts-rainbow' })
+
+    -- Treesitter
     use({ 'nvim-treesitter/nvim-treesitter-textobjects' })
+    use({ 'nvim-treesitter/nvim-treesitter', irun = ':TSUpdate' })
+    use({ 'nvim-treesitter/playground' })
+    use({ 'p00f/nvim-ts-rainbow' })
+
+    use({ 'karb94/neoscroll.nvim' })
+    use({ 'kyazdani42/nvim-tree.lua' })
+    use({ 'petertriho/nvim-scrollbar' })
+    use({ 'voldikss/vim-floaterm' })
     use({
         'noib3/nvim-cokeline',
         requires = 'kyazdani42/nvim-web-devicons',
     })
+    use({
+        'hoob3rt/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+    })
+
+    -- Theme
+    use({ 'tjdevries/colorbuddy.nvim' })
     use({ 'norcalli/nvim-colorizer.lua' })
-    use({ 'jose-elias-alvarez/null-ls.nvim' })
+    use({ 'marko-cerovac/material.nvim' })
+    use({ 'folke/tokyonight.nvim' })
+
+    -- Other
+    use({ 'mfussenegger/nvim-dap' })
+    use({ 'ojroques/vim-oscyank' })
+    use({ 'phaazon/hop.nvim', branch = 'v1' })
+    use({ 'wakatime/vim-wakatime' })
+    -- use { 'terryma/vim-multiple-cursors' }
 end)
