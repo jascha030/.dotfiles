4a9d6b9e86c661e739347543aa1c072ca8e018a7
Jascha030 <contact@jaschavanaalst.nl>
2022-05-16 12:01:32 +0200

    🌐 Refactor sumneko_lua LSP config to include multiple lua runtimes (hammerspoon, nvim).

--- config/nvim/lua/lsp/settings/sumneko_lua.lua
+++ config/nvim/lua/lsp/settings/sumneko_lua.lua
@@ -1,28 +1,48 @@
+local util = require('util')
+
+-- Language server paths
+local binary_path = vim.fn.exepath('lua-language-server')
+local root_path = vim.fn.fnamemodify(binary_path, ':h:h:h')
+
+-- Loaded runtime package paths
 local runtime_path = vim.split(package.path, ';')
+
+-- Hammerspoon runtime
+local hs_version = vim.fn.system('hs -c _VERSION'):gsub('[\n\r]', '')
+local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
+
+-- Wildcards
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-local hs_version = vim.fn.system('hs -c _VERSION'):gsub('[\n\r]', '')
-local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
+-- Merge runtime_path with hammerspoon paths
+runtime_path = util.tbl_merge(runtime_path, hs_path)
+
+-- Merge Neovim runtime with other libraries
+local library = vim.api.nvim_get_runtime_file('', true)
+library = util.tbl_merge(library, {
+    vim.fn.expand('$WEZTERM_CONFIG_DIR'),
+    vim.fn.expand('$VIMRUNTIME/lua'),
+    vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'),
+    vim.fn.expand('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/'),
+    vim.fn.expand('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations'),
+})
 
 return {
     settings = {
         Lua = {
             runtime = {
-                version = hs_version,
-                path = hs_path,
+                version = {
+                    'LuaJIT',
+                    hs_version,
+                },
+                path = runtime_path,
             },
             diagnostics = {
                 globals = { 'hs', 'vim' },
             },
             workspace = {
-                library = {
-                    vim.api.nvim_get_runtime_file('', true),
-                    [vim.fn.expand('$VIMRUNTIME/lua')] = true,
-                    [vim.fn.expand('$VIMRUNTIME/lua/vim/lsp')] = true,
-                    [vim.fn.expand('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/')] = true,
-                    os.getenv('HOME') .. '/.hammerspoon/Spoons/EmmyLua.spoon/annotations',
-                },
+                library = library,
             },
             telemetry = {
                 enable = false,
