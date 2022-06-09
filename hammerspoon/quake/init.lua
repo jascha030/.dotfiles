91cbc8a205e349f43981b677fbde41b982af95c1
Jascha030 <contact@jaschavanaalst.nl>
2022-06-09 11:27:26 +0200

    🚚 Move settings/setup logic outside of quake module.

--- hammerspoon/quake/init.lua
+++ hammerspoon/quake/init.lua
@@ -1,18 +1,15 @@
 local term = require('quake.term')
 local tap = require('quake.tap')
-local settings = require('settings')
-
--- Load settings
-settings:setup({})
-
--- Terminal Emulator App (default: 'Alacritty.app')
--- Default screen
-local app = settings:get('termApp')
-local screen = settings:get('builtinScreen')
 
 -- DoubleTap
 --    key = cmd,
 --    map = term.toggle
-tap.action = function()
-    term.toggle(app, screen)
+local function set(app, screen)
+    tap.action = function()
+        term.toggle(app, screen)
+    end
 end
+
+return {
+    set = set,
+}
