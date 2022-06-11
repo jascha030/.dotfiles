39e9ec069093932df583603400a4956142684c73
Jascha030 <contact@jaschavanaalst.nl>
2022-06-12 00:14:13 +0200

    🎨 Random cleanup in wezterm config.

--- config/wezterm/fonts/init.lua
+++ config/wezterm/fonts/init.lua
@@ -1,40 +1,21 @@
 local wezterm = require('wezterm')
 
-local font_with_fallback = function(name, args)
+local function fb_font(name, args)
     return wezterm.font_with_fallback({ 'nonicons', name }, args)
 end
 
 local fonts = {
     default = wezterm.font('MesloLGS Nerd Font'),
-    italic = wezterm.font('Dank Mono', {
-        italic = true,
-        weight = 500,
-    }),
-    normal = font_with_fallback({
-        family = 'MesloLGS Nerd Font',
-        italic = false,
-        weight = 600,
-    }),
+    italic = wezterm.font('Dank Mono', { italic = true, weight = 500 }),
+    normal = fb_font({ family = 'MesloLGS Nerd Font', italic = false, weight = 600 }),
 }
 
 return {
     size = 19,
     default = fonts.default,
     rules = {
-        {
-            italic = false,
-            intensity = 'Normal',
-            font = fonts.normal,
-        },
-        {
-            italic = true,
-            intensity = 'Bold',
-            font = fonts.normal,
-        },
-        {
-            italic = true,
-            intensity = 'Normal',
-            font = fonts.italic,
-        },
+        { italic = false, intensity = 'Normal', font = fonts.normal },
+        { italic = true, intensity = 'Bold', font = fonts.normal },
+        { italic = true, intensity = 'Normal', font = fonts.italic },
     },
 }
