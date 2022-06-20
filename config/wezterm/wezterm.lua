62d9bfce45577564ccc9ef33f40ced5eb5a1cf30
Jascha030 <contact@jaschavanaalst.nl>
2022-06-20 21:07:01 +0200

    🔧 Add keymap config for wezterm and wezterm-mux related stuff

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -12,42 +12,52 @@ wezterm.on('window-config-reloaded', function(window, pane)
     end
 end)
 
+local keys = {
+    -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
+    { key = 's', mods = 'LEADER', action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }) },
+    { key = 'v', mods = 'LEADER', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
+    { key = 'z', mods = 'LEADER', action = 'TogglePaneZoomState' },
+    { key = 'c', mods = 'LEADER', action = wezterm.action({ SpawnTab = 'CurrentPaneDomain' }) },
+    { key = 'j', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Prev' }) },
+    { key = 'l', mods = 'LEADER', action = 'ActivateLastTab' },
+    { key = 'H', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Left', 5 } }) },
+    { key = 'J', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Down', 5 } }) },
+    { key = 'K', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Up', 5 } }) },
+    { key = 'L', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Right', 5 } }) },
+    { key = '1', mods = 'LEADER', action = wezterm.action({ ActivateTab = 0 }) },
+    { key = '2', mods = 'LEADER', action = wezterm.action({ ActivateTab = 1 }) },
+    { key = '3', mods = 'LEADER', action = wezterm.action({ ActivateTab = 2 }) },
+    { key = '4', mods = 'LEADER', action = wezterm.action({ ActivateTab = 3 }) },
+    { key = '5', mods = 'LEADER', action = wezterm.action({ ActivateTab = 4 }) },
+    { key = '6', mods = 'LEADER', action = wezterm.action({ ActivateTab = 5 }) },
+    { key = '7', mods = 'LEADER', action = wezterm.action({ ActivateTab = 6 }) },
+    { key = '8', mods = 'LEADER', action = wezterm.action({ ActivateTab = 7 }) },
+    { key = '9', mods = 'LEADER', action = wezterm.action({ ActivateTab = 8 }) },
+    { key = '&', mods = 'LEADER|SHIFT', action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+    { key = 'x', mods = 'LEADER', action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+
+    { key = 'n', mods = 'SHIFT|CTRL', action = 'ToggleFullScreen' },
+    { key = 'v', mods = 'SHIFT|CTRL', action = 'Paste' },
+    { key = 'c', mods = 'SHIFT|CTRL', action = 'Copy' },
+}
+
+--local leader_key = function()
+--    return os.getenv('TERMINAL_PROGRAM') == 'tmux' and 'b' or 'd'
+--end
+
+local leader_mod = 'CTRL'
+
+if os.getenv('TERM_PROGRAM') == 'tmux' then
+    leader_mod = 'CTRL|SHIFT'
+end
+
 return {
     default_prog = { '/usr/local/bin/zsh', '--login' },
 
-    leader = { key = 'd', mods = 'CTRL' },
+    leader = { key = 'd', mods = leader_mod },
     disable_default_key_bindings = true,
-    keys = {
-        -- Send "CTRL-A" to the terminal when pressing CTRL-A, CTRL-A
-        { key = 'd', mods = 'LEADER|CTRL', action = wezterm.action({ SendString = '\x01' }) },
-        { key = 'v', mods = 'LEADER', action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }) },
-        { key = 's', mods = 'LEADER', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
-        { key = 'z', mods = 'LEADER', action = 'TogglePaneZoomState' },
-        { key = 'c', mods = 'LEADER', action = wezterm.action({ SpawnTab = 'CurrentPaneDomain' }) },
-        { key = 'h', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Left' }) },
-        { key = 'j', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Down' }) },
-        { key = 'k', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Up' }) },
-        { key = 'l', mods = 'LEADER', action = wezterm.action({ ActivatePaneDirection = 'Right' }) },
-        { key = 'H', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Left', 5 } }) },
-        { key = 'J', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Down', 5 } }) },
-        { key = 'K', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Up', 5 } }) },
-        { key = 'L', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Right', 5 } }) },
-        { key = '1', mods = 'LEADER', action = wezterm.action({ ActivateTab = 0 }) },
-        { key = '2', mods = 'LEADER', action = wezterm.action({ ActivateTab = 1 }) },
-        { key = '3', mods = 'LEADER', action = wezterm.action({ ActivateTab = 2 }) },
-        { key = '4', mods = 'LEADER', action = wezterm.action({ ActivateTab = 3 }) },
-        { key = '5', mods = 'LEADER', action = wezterm.action({ ActivateTab = 4 }) },
-        { key = '6', mods = 'LEADER', action = wezterm.action({ ActivateTab = 5 }) },
-        { key = '7', mods = 'LEADER', action = wezterm.action({ ActivateTab = 6 }) },
-        { key = '8', mods = 'LEADER', action = wezterm.action({ ActivateTab = 7 }) },
-        { key = '9', mods = 'LEADER', action = wezterm.action({ ActivateTab = 8 }) },
-        { key = '&', mods = 'LEADER|SHIFT', action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-        { key = 'x', mods = 'LEADER', action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
 
-        { key = 'n', mods = 'SHIFT|CTRL', action = 'ToggleFullScreen' },
-        { key = 'v', mods = 'SHIFT|CTRL', action = 'Paste' },
-        { key = 'c', mods = 'SHIFT|CTRL', action = 'Copy' },
-    },
+    keys = keys,
 
     enable_tab_bar = false,
 
