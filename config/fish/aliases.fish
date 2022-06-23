ef492961a2d2f8194cd4b39cf36315628bbb3c69
Jascha030 <contact@jaschavanaalst.nl>
2022-06-23 18:31:19 +0200

    🎉 Start Feeesh confeeeg.

---
+++ config/fish/aliases.fish
@@ -0,0 +1,11 @@
+alias rr="exec /usr/local/bin/fish --login"
+
+alias ls=lsd -Ahl --color
+alias lazygit=lazygit -ucf $LAZYGIT_CONFIG
+alias vim=nvim
+alias cat=bat
+
+alias idea "idea ."
+alias pstorm="phpstorm ."
+alias wstorm="webstorm ."
+
--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,29 @@
+set -g DOTFILES $HOME/.dotfiles
+set -g DOT_ZSH $DOTFILES/zsh
+set -g XDG_CONFIG $HOME/.config
+set -g DOT_RC $XDG_CONFIG/fish/config.fish  
+
+set -U EDITOR nvim
+set -U LAZYGIT_CONFIG $XDG_CONFIG/lazygit/config.yml 
+
+set -U fish_user_paths /usr/local/sbin \
+                       /usr/local/opt/openjdk/bin \
+                       /usr/local/opt/openssl@1.1/bin \
+                       node_modules/.bin \
+                       vendor/bin \
+                       $HOME/bin \
+                       $HOME/.composer/vendor/bin \
+                       $HOME/.node/bin \
+                       $HOME/.yarn/bin  \
+                       $HOME/.config/yarn/global/node_modules/.bin \
+                       $HOME/.gem/ruby/2.6.0/bin \
+                       $HOME/tools/lua-language-server/bin/macOS
+
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    set -U EDITOR nvim
+    set -U LAZYGIT_CONFIG $XDG_CONFIG/lazygit/config.yml 
+
+    alias idea "idea ."
+
+    source $DOTFILES/aliases
 end
