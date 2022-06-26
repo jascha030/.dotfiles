37305244e2bd49a36bba0cb7c5e103c526be39b4
Jascha030 <contact@jaschavanaalst.nl>
2022-06-26 03:23:14 +0200

    🐛 Fix oopsie

--- zsh/plugins/lolmsg/lolmsg.plugin.zsh
+++ zsh/plugins/lolmsg/lolmsg.plugin.zsh
@@ -1,9 +1,10 @@
+# Reset blink effect in case it wasn't ended in previous session.
 if ! (( ${+DOT_DEFAULT_LOL_MSG} )); then 
     # Normal msg displayed with figlet in funky colors.
     export DOT_DEFAULT_LOL_MSG="Hackerman Mode 030"   
 fi 
 
-if ! (( ${+DOT_DEFAULT_LOL_MSG} )); then 
+if ! (( ${+DOT_NEOVIM_LOL_MSG} )); then 
     # Alt msg displayed when in Neovim term.
     export DOT_NEOVIM_LOL_MSG="NVIM 030"
 fi
