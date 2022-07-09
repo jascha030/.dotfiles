314c1b8d784317487e0ed7fbe91cc36f8fafb11c
Jascha030 <contact@jaschavanaalst.nl>
2022-07-09 23:39:07 +0200

    ♻️ Create folder module from ui.lua

---
+++ config/nvim/lua/plugins/config/ui/init.lua
@@ -0,0 +1,77 @@
+require('scheme.utils')
+
+local function cokeline()
+    require('cokeline').setup({
+        show_if_buffers_are_at_least = 2,
+        buffers = {
+            focus_on_delete = 'prev',
+            new_buffers_position = 'next',
+        },
+    })
+end
+
+local function devicons()
+    require('nvim-web-devicons').setup({})
+end
+
+local function lualine()
+    require('lualine').setup({
+        options = {
+            icons_enabled = true,
+            theme = 'tokyonight',
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            disabled_filetypes = { 'NvimTree' },
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch' },
+            lualine_c = { 'filename' },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {},
+        extensions = {},
+    })
+end
+
+local function scrollbar()
+    local colors = require('tokyonight.colors').setup({})
+    local user_colors = require('scheme.colors.jassie030')
+
+    require('scrollbar').setup({
+        handle = {
+            --color = user_colors[DarkmodeEnabled() and 'dark' or 'light'],
+        },
+        marks = {
+            Search = { color = colors.orange },
+            Error = { color = colors.error },
+            Warn = { color = colors.warning },
+            Info = { color = colors.info },
+            Hint = { color = colors.hint },
+            Misc = { color = colors.purple },
+        },
+    })
+end
+
+local function colorizer()
+    require('colorizer').setup()
+end
+
+return {
+    devicons = devicons,
+    cokeline = cokeline,
+    colorizer = colorizer,
+    lualine = lualine,
+    scrollbar = scrollbar,
+    tree = require('plugins.config.ui.tree'),
+}
--- config/nvim/lua/plugins/config/ui/tree.lua
+++ config/nvim/lua/plugins/config/ui/tree.lua
@@ -1,70 +1,4 @@
-require('scheme.utils')
-
-local function cokeline()
-    require('cokeline').setup({
-        show_if_buffers_are_at_least = 2,
-        buffers = {
-            focus_on_delete = 'prev',
-            new_buffers_position = 'next',
-        },
-    })
-end
-
-local function lualine()
-    require('lualine').setup({
-        options = {
-            icons_enabled = true,
-            theme = 'tokyonight',
-            component_separators = { left = '', right = '' },
-            section_separators = { left = '', right = '' },
-            disabled_filetypes = { 'NvimTree' },
-        },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch' },
-            lualine_c = { 'filename' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' },
-        },
-        inactive_sections = {
-            lualine_a = {},
-            lualine_b = {},
-            lualine_c = { 'filename' },
-            lualine_x = { 'location' },
-            lualine_y = {},
-            lualine_z = {},
-        },
-        tabline = {},
-        extensions = {},
-    })
-end
-
-local function scrollbar()
-    local colors = require('tokyonight.colors').setup({})
-    local user_colors = require('scheme.colors.jassie030')
-
-
-    require('scrollbar').setup({
-        handle = {
-            --color = user_colors[DarkmodeEnabled() and 'dark' or 'light'],
-        },
-        marks = {
-            Search = { color = colors.orange },
-            Error = { color = colors.error },
-            Warn = { color = colors.warning },
-            Info = { color = colors.info },
-            Hint = { color = colors.hint },
-            Misc = { color = colors.purple },
-        },
-    })
-end
-
-local function colorizer()
-    require('colorizer').setup()
-end
-
-local function tree()
+return function()
     vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
 
     require('nvim-tree').setup({
@@ -86,7 +20,6 @@ local function tree()
             width = 40,
             height = 30,
             side = 'right',
-
             mappings = {
                 custom_only = false,
                 list = {
@@ -157,7 +90,7 @@ local function tree()
             debounce_delay = 50,
         },
         diagnostics = {
-            enable = false,
+            enable = true,
             show_on_dirs = false,
             icons = {
                 hint = '',
@@ -171,10 +104,3 @@ local function tree()
     vim.cmd([[set termguicolors]])
 end
 
-return {
-    cokeline = cokeline,
-    colorizer = colorizer,
-    lualine = lualine,
-    scrollbar = scrollbar,
-    tree = tree,
-}
