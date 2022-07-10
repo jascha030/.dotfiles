4d9a5f8314980882e0d1c6c9bb4aba9b95f98a0f
Jascha030 <contact@jaschavanaalst.nl>
2022-07-11 00:50:40 +0200

    ✨ More config editing in init.lua, keeping the things you prob need to edit more often directly visible.

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -11,8 +11,41 @@
 --                                                                      --
 --[[==========================Configuration===========================]]
 
-local kmap = require('util').kmap
+-- Options
+require('options').setup({
+    options = {
+        mouse = 'a',
+        termguicolors = true,
+        incsearch = true,
+        colorcolumn = '120',
+        backspace = 'indent,eol,start',
+        fileencoding = 'utf-8',
+        fillchars = 'eob: ,msgsep:‾',
+        scrolloff = 5,
+        showtabline = 2,
+        tabstop = 4,
+        shiftwidth = 2,
+        expandtab = true,
+        smartindent = true,
+        number = true,
+        cursorline = true,
+        modifiable = true,
+        updatetime = 400,
+    },
+    global = {
+        mapleader = '<space>',
+        material_italic_comments = true,
+        material_italic_keywords = true,
+        material_italic_functions = true,
+        material_italic_variables = false,
+        material_contrast = false,
+        material_borders = false,
+        material_style = 'tokyonight',
+        node_host_prog = os.getenv('HOME') .. '/.fnm/node-versions/v17.7.1/installation/bin/neovim-node-host',
+    },
+})
 
+-- Colorscheme overrides, uses color names as used by wezterm.
 require('scheme').setup({
     overrides = {
         dark = {
@@ -34,10 +67,6 @@ require('scheme').setup({
     },
 })
 
-kmap('CS', '<cmd>lua require("scheme").toggle()<CR>', 'n', { noremap = true })
-
-require('options')
 require('plugins')
 require('lsp')
 require('keymap')
-
