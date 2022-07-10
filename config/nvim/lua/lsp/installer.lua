5eb73fc0455de28e00134b96885b273f7353efe3
Jascha030 <contact@jaschavanaalst.nl>
2022-07-10 12:19:18 +0200

    ♻️ Refactor installer.lua, add setup mode, loop config files instead of if statements.

--- config/nvim/lua/lsp/installer.lua
+++ config/nvim/lua/lsp/installer.lua
@@ -1,26 +1,20 @@
--- Check if nvim-lsp-installer can be loaded or return
-local status_ok, lsp_installer = pcall(require, 'nvim-lsp-installer')
-if not status_ok then
-    return
+-- TODO: apparently nvim-lsp-installer.on_server_ready is deprecated, use builtin nvim-lsp method instead
+
+local function get_server_config(opts, server)
+    local mname = 'lsp.config.' .. server.name
+
+    local ok, config = pcall(function(m)
+        return require(m)
+    end, mname)
+
+    return not ok and opts or vim.tbl_deep_extend('force', config, opts)
 end
 
-lsp_installer.on_server_ready(function(server)
-    local opts = {
-        on_attach = require('lsp.handlers').on_attach,
-        capabilities = require('lsp.handlers').capabilities,
-    }
-
-    if server.name == 'intelephense' then
-        opts = vim.tbl_deep_extend('force', require('lsp.settings.intelephense'), opts)
-    end
-
-    if server.name == 'sumneko_lua' then
-        opts = vim.tbl_deep_extend('force', require('lsp.settings.sumneko_lua'), opts)
-    end
-
-    if server.name == 'rust_analyzer' then
-        opts = vim.tbl_deep_extend('force', require('lsp.settings.rust_analyzer'), opts)
-    end
-
-    server:setup(opts)
-end)
+return {
+    setup = function(opts)
+        opts = opts or {}
+        require('nvim-lsp-installer').on_server_ready(function(server)
+            server:setup(get_server_config(opts, server))
+        end)
+    end,
+}
