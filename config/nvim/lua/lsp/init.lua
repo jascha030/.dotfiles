a9203b466a2ffdb029730f5697cadef4abd5c951
Jascha030 <contact@jaschavanaalst.nl>
2022-07-10 12:21:18 +0200

    ♻️ Refactor lsp/init.lua

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -6,3 +6,23 @@ end
 
 local installer = require('lsp.installer')
 local handlers = require('lsp.handlers')
+
+local null_ls = require('null-ls')
+local fmt = null_ls.builtins.formatting
+
+local config = {
+    handlers = { on_attach = handlers.on_attach, capabilities = handlers.capabilities },
+    null_ls = {
+        sources = {
+            fmt.stylua.with({
+                extra_args = { '--config-path', os.getenv('HOME') .. '/.config/stylua.toml' },
+            }),
+            null_ls.builtins.diagnostics.eslint,
+            null_ls.builtins.completion.spell,
+        },
+    },
+}
+
+installer.setup(config.handlers)
+handlers.setup()
+null_ls.setup(config.null_ls)
