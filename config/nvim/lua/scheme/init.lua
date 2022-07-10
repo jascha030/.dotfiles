ccfff3004c7fd66f6ed44920d2b2c6f6f253f260
Jascha030 <contact@jaschavanaalst.nl>
2022-07-10 12:18:25 +0200

    ♻️ Cleanup scheme module.

--- config/nvim/lua/scheme/init.lua
+++ config/nvim/lua/scheme/init.lua
@@ -1,3 +1,10 @@
+require('scheme.utils')
+
+local config = require('scheme.config')
+local util = require('util')
+
+local loaded, loaded_scheme, user_config = false, nil, nil
+
 local _default_overrides = {
     yellow = 'yellow',
     red = 'red',
@@ -15,25 +22,31 @@ local default_tokyopts = {
     tokyonight_sidebars = { 'terminal', 'packer', 'nvim-tree' },
 }
 
-require('scheme.utils')
-
-local config = require('scheme.config')
-local UserSchemeStyles = require('scheme.styles')
-local UserSchemeOverRides = require('scheme.overrides')
-local util = require('util')
-
-local loaded = false
-local loaded_scheme, user_config = nil, nil
-
-local UserScheme = {}
+local UserSchemeStyles, UserSchemeOverRides, UserScheme = {}, {}, {}
+UserSchemeStyles.__index = UserSchemeStyles
+UserSchemeOverRides.__index = UserSchemeOverRides
 UserScheme.__index = UserScheme
+
+function UserSchemeStyles.create(dark, light)
+    return setmetatable({
+        dark = dark or 'storm',
+        light = light or 'day',
+    }, UserSchemeStyles)
+end
+
+function UserSchemeOverRides.create(dark, light)
+    return setmetatable({
+        dark = dark or {},
+        light = light or {},
+    }, UserSchemeOverRides)
+end
 
 function UserScheme.create(c)
     local user_scheme_loaded, _scheme = pcall(require, 'scheme.colors.' .. c.scheme)
     if not user_scheme_loaded then
         error('Could not find colorscheme: "' .. c.scheme .. '".')
     end
-    
+
     local self = {
         _config = c,
         _userscheme = _scheme,
@@ -64,7 +77,7 @@ function UserScheme.create(c)
             self['_' .. style][t_name] = _scheme[style][u_name]
         end
     end
-    
+
     return setmetatable(self, UserScheme)
 end
 
@@ -77,24 +90,31 @@ function UserScheme:update(dark)
     vim.g.tokyonight_transparent = default_tokyopts.tokyonight_transparent
     vim.g.tokyonight_transparent_sidebar = default_tokyopts.tokyonight_transparent_sidebar
     vim.g.tokyonight_sidebars = default_tokyopts.tokyonight_sidebars
-
     vim.g.tokyonight_style = self:getStyle(dark)
     vim.g.tokyonight_colors = self:getColors(dark)
-    
+
     vim.cmd([[colorscheme tokyonight]])
 end
 
-function UserScheme:init()
-    self:update(OSDarkmodeEnabled())
-
+function UserScheme:initAutocommand()
     vim.api.nvim_create_autocmd('Signal', {
         pattern = 'SIGUSR1',
         callback = function()
             self:update(OSDarkmodeEnabled())
         end,
     })
+end
 
+function UserScheme:init()
+    if loaded == true then
+        return self
+    end
+
+    self:update(OSDarkmodeEnabled())
+    self:initAutocommand()
     vim.api.nvim_create_autocmd('Signal', { pattern = 'SIGUSR1', command = 'PackerCompile' })
+
+    return self
 end
 
 function UserScheme:getStyle(dark)
@@ -137,6 +157,14 @@ local function default_config()
     return config
 end
 
+local function merge_userconfig(opts)
+    if opts ~= nil then
+        user_config = vim.tbl_deep_extend('force', default_config(), opts or {})
+    end
+
+    return user_config
+end
+
 return {
     config = function(default)
         if default == true or user_config == nil or loaded == false then
@@ -150,11 +178,9 @@ return {
             return loaded_scheme
         end
 
+        loaded_scheme = UserScheme.create(merge_userconfig(opts))
+        loaded_scheme:init()
         loaded = true
-        user_config = vim.tbl_deep_extend('force', default_config(), opts or {})
-        
-        loaded_scheme = UserScheme.create(user_config)
-        loaded_scheme:init()    
 
         return loaded_scheme
     end,
