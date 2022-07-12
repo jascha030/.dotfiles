b5a700957be0c7ae9ac62bbf1fa9744f50cc17cc
Jascha030 <contact@jaschavanaalst.nl>
2022-07-13 01:01:44 +0200

    ♻️ Move hotkey initialization to hotkey submodule.

---
+++ hammerspoon/jascha030/hotkey.lua
@@ -0,0 +1,67 @@
+local M = {}
+
+local types = {
+    apps = {
+        name = 'Apps',
+        description = 'Show, hide or open various apps.',
+        mods = { 'shift', 'alt' },
+        handler = function(mod, key, app)
+            hs.hotkey.bind(mod, key, function()
+                require('jascha030').quake.toggle(app)
+            end)
+        end,
+    },
+    control = {
+        name = 'App controls',
+        description = 'Bindings mapped to various actions related to specific apps and the window/layout system.',
+        mods = { 'ctrl', 'alt' },
+    },
+    system = {
+        name = 'System bindings',
+        description = 'Uncommon key-mod bindings for system opts or actions with potentially unwanted side-effects.',
+        mods = { 'ctrl', 'alt', 'cmd' },
+    },
+}
+
+function M.visualize(mod, key)
+    local key_msg = nil
+    for m in mod do
+        key_msg = not key_msg and m or key_msg .. ', ' .. m
+    end
+
+    return '[ ' .. key_msg .. ' ] + ' .. key .. '.'
+end
+
+local function default_handler(mod, key, arg)
+    if arg == nil then
+        return
+    end
+
+    if type(arg) ~= 'function' then
+        error('Invalid argument for hotkey binding: ' .. M.visualize(mod, key))
+    end
+
+    hs.hotkey.bind(mod, key, function()
+        local ok, res = pcall(arg)
+        if not ok then
+            error('Error processing callback for hotkey binding: ' .. M.visualize(mod, key))
+        end
+
+        return res
+    end)
+end
+
+function M.setup(bindings)
+    for t, values in pairs(bindings) do
+        if types[t] ~= nil then
+            local type = types[t]
+            local method = type.handler or default_handler
+
+            for key, action in pairs(values) do
+                pcall(method, type.mods, key, action)
+            end
+        end
+    end
+end
+
+return M
--- hammerspoon/jascha030/init.lua
+++ hammerspoon/jascha030/init.lua
@@ -1,92 +1,26 @@
 local M = {}
 
 local config = require('jascha030.config')
-local quake = require('jascha030.quake')
-local utils = require('jascha030.utils')
-local window = require('jascha030.window')
-local counter = require('jascha030.counter')
-local music = require('jascha030.music')
-local krk = require('jascha030.krk')
 
 config:setup({})
 
 local main_screen = config:get('builtinScreen')
 
-local types = {
-    apps = {
-        name = 'Apps',
-        description = 'Show, hide or open various apps.',
-        mods = { 'shift', 'alt' },
-        handler = function(mod, key, app)
-            hs.hotkey.bind(mod, key, function()
-                quake.toggle(app, main_screen)
-            end)
-        end,
-    },
-    control = {
-        name = 'App controls',
-        description = 'Bindings mapped to various actions related to specific apps and the window/layout system.',
-        mods = { 'ctrl', 'alt' },
-    },
-    system = {
-        name = 'System bindings',
-        description = 'Uncommon key-mod bindings for system opts or actions with potentially unwanted side-effects.',
-        mods = { 'ctrl', 'alt', 'cmd' },
-    },
-}
+M.hotkey = require('jascha030.hotkey')
+M.quake = require('jascha030.quake')
+M.utils = require('jascha030.utils')
+M.window = require('jascha030.window')
+M.counter = require('jascha030.counter')
+M.music = require('jascha030.music')
+M.krk = require('jascha030.krk')
+M.utils = require('jascha030.utils')
+M.system = require('jascha030.system')
 
-local function map_to_string(mod, key)
-    local key_msg = nil
 
-    for m in mod do
-        key_msg = not key_msg and m or key_msg .. ', ' .. m
     end
 
-    return '[ ' .. key_msg .. ' ] + ' .. key .. '.'
 end
 
-local function handler(mod, key, arg)
-    if arg == nil then
-        return
-    end
-
-    local ok, res, t = nil, nil, type(arg)
-
-    if t ~= 'function' then
-        error('Invalid argument for hotkey binding: ' .. map_to_string(mod, key))
-    end
-
-    hs.hotkey.bind(mod, key, function()
-        ok, res = pcall(arg)
-
-        if not ok then
-            error('Error processing callback for hotkey binding: ' .. map_to_string(mod, key))
-        else
-            return res
-        end
-    end)
-end
-
-M.config = config
-M.quake = quake
-M.utils = utils
-M.window = window
-M.counter = counter
-M.music = music
-M.krk = krk
-
-function M.setup(bindings)
-    local ok, res = nil, nil
-
-    for t, values in pairs(bindings) do
-        if types[t] ~= nil then
-            local type = types[t]
-            local method = type.handler or handler
-
-            for key, action in pairs(values) do
-                pcall(method, type.mods, key, action)
-            end
-        end
     end
 end
 
