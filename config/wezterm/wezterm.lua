fde0aa883e2caf1de2ceef420cee2cede0e04758
Jascha030 <contact@jaschavanaalst.nl>
2022-07-13 01:57:36 +0200

    🎨 Formatting

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,7 +1,6 @@
 local wezterm = require('wezterm')
 local theme = require('theme')
 local fonts = require('fonts')
-local keys = require('keymap')
 
 wezterm.on('window-config-reloaded', function(window)
     local current = window:get_appearance()
@@ -10,41 +9,29 @@ wezterm.on('window-config-reloaded', function(window)
 
     if overrides.colors ~= scheme then
         overrides.colors = scheme
-        --overrides.window_background_opacity = theme.get_opacity(current)
-
+        overrides.window_background_opacity = theme.get_opacity(current)
         window:set_config_overrides(overrides)
     end
 end)
 
 return {
     default_prog = { '/usr/local/bin/zsh', '--login' },
-
-    leader = { key = 'd', mods = 'CTRL' },
-    disable_default_key_bindings = true,
-    keys = keys,
-
     enable_tab_bar = false,
-
     window_decorations = 'NONE | RESIZE',
-    --window_padding = { left = 10, right = 10, top = 6, bottom = 2 },
     window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-
     default_cursor_style = 'BlinkingBlock',
     cursor_blink_rate = 250,
     cursor_blink_ease_in = 'Ease',
     cursor_blink_ease_out = 'Ease',
 
     font_size = fonts.size,
-    --font = fonts.default,
     font_rules = fonts.rules,
 
     colors = theme.get_scheme('Dark'),
+    inactive_pane_hsb = { saturation = 0.98, brightness = 0.9 },
+    window_background_opacity = theme.get_opacity('Dark'),
 
-    inactive_pane_hsb = {
-        saturation = 0.98,
-        brightness = 0.9,
-    },
-
-    window_background_opacity = 1,
-    -- window_background_opacity = theme.get_opacity('Dark'),
+    keys = require('keymap'),
+    disable_default_key_bindings = true,
+    leader = { key = 'd', mods = 'CTRL' },
 }
