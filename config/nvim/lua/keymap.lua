c158a99199838bb883907b0c65653f0039a9f184
Jascha030 <contact@jaschavanaalst.nl>
2022-07-13 14:29:17 +0200

    ♻️ Refactor keymap config.

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -1,47 +1,50 @@
-local kmap = require('util').kmap
+local M = {}
+
+local util = require('util')
+local kmap = util.kmap
+
 local default_opts = { noremap = true }
+local modes = { 'n', 'i', 'v', 't', 'x' }
 
-local function map(keymap, action, opts)
-    kmap(keymap, action, 'n', opts or default_opts)
+local config = {
+    default_opts = default_opts,
+    maps = {
+        ['n'] = {},
+        ['i'] = {},
+        ['v'] = {},
+        ['t'] = {},
+        ['x'] = {},
+    },
+}
+
+-- todo compatibility for composed modes.
+local function batch_map(type, opts)
+    if not util.tbl_contains(modes, type) then
+        error('Invalid mode: ' .. type)
+    end
+
+    for k, kopts in pairs(opts) do
+        local a, o = kopts[1], kopts[2] or default_opts
+        if k == nil or a == nil then
+            error('Not enough arguments passed to keymap')
+        end
+
+        kmap(k, a, type, o or default_opts)
+    end
 end
-local function vmap(keymap, action, opts)
-    kmap(keymap, action, 'v', opts or default_opts)
+
+function M.setup(userconfig)
+    userconfig = userconfig or {}
+    userconfig = vim.tbl_deep_extend('force', config, userconfig)
+    for k, v in pairs(userconfig.maps) do
+        batch_map(k, v)
+    end
 end
-local function tmap(keymap, action, opts)
-    kmap(keymap, action, 't', opts or default_opts)
-end
 
--- Cokeline
-map('<S-Tab>', '<Plug>(cokeline-focus-next)', { silent = true })
-map('<C-w>', ':close<CR>')
+function M.map(keymap, action, opts) kmap(keymap, action, 'n', opts or default_opts) end
+function M.imap(keymap, action, opts) kmap(keymap, action, 'i', opts or default_opts) end
+function M.vmap(keymap, action, opts) kmap(keymap, action, 'v', opts or default_opts) end
+function M.tmap(keymap, action, opts) kmap(keymap, action, 't', opts or default_opts) end
+function M.xmap(keymap, action, opts) kmap(keymap, action, 'x', opts or default_opts) end
 
--- Nvim Tree
-map('<C-n>', ':NvimTreeToggle<CR>', { noremap = true })
-map('<leader>f', ':lua require("plugins.telescope").project_files()<CR>', { noremap = true, silent = true })
-map('N', ':NvimTreeFocus<CR>', { noremap = true })
-
---  Telescope
-map('ss', ':Telescope<CR>')
-map('ff', ':lua require("telescope.builtin").find_files()<CR>')
-map('FF', ':lua require("telescope").extensions.file_browser.file_browser()<CR>')
-map('fg', ':lua require("telescope.builtin").live_grep()<CR>')
-map('<C-f>', ':lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>')
-
--- Lsp
-map('<C-l>', ':lua vim.lsp.buf.formatting()<CR>')
-map('TT', ':TroubleToggle<CR>')
-
--- Other
-vmap('<C-c>', ':OSCYank<CR>')
-
--- Terminal
-map('<C-t>', ':FloatermToggle[!]<CR>')
-
-tmap('<C-t>', '<C-\\><C-n> :FloatermToggle[!]<CR>')
-tmap('<M-[>', '<Esc>')
-tmap('<C-v><Esc>', '<Esc>')
-
--- Hop hop, keymappie erop
-map('<Tab><Tab>', ':HopWord<CR>')
-map('sR', ':source $MYVIMRC<CR>', { noremap = true, silent = true })
-
+return M
