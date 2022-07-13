bb0e9c0186e2a2bf52284c2ef9b87a62a0e5e63e
Jascha030 <contact@jaschavanaalst.nl>
2022-07-13 14:29:00 +0200

    ✨ Add some useful utils.

--- config/nvim/lua/util.lua
+++ config/nvim/lua/util.lua
@@ -1,5 +1,41 @@
 local M = {}
 
+local function value_index(table)
+    local set = {}
+
+    for _, v in ipairs(table) do
+        set[v] = true
+    end
+
+    return set
+end
+
+local function count(table)
+    local count = 0
+
+    for _, _ in ipairs(table) do
+        count = count + 1
+    end
+
+    return count
+end
+
+function M.tbl_contains(t, v)
+    local map = value_index(t)
+
+    return map[v] or false
+end
+
+function M.tbl_length(t)
+    assert(t == nil or type(t) == 'table', 'bad parameter #1: must be of type table or nil.')
+
+    if t == nil then
+        return 0
+    end
+
+    return count(t)
+end
+
 function M.tbl_merge(t1, t2)
     for _, v in ipairs(t2) do
         table.insert(t1, v)
