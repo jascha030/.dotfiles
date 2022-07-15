1c7ecf71149c3f2a84dd7868901f715256543bf7
Jascha030 <contact@jaschavanaalst.nl>
2022-07-15 13:56:36 +0200

    💄 More colorscheme updates.

--- config/colors/lua/jassie030.lua
+++ config/colors/lua/jassie030.lua
@@ -1,4 +1,6 @@
 -- Old/unused colors
+-- '#1a1423'
+-- '#1e2030'
 -- '#1f2335'
 -- '#252d53'
 -- '#3b4261'
@@ -6,14 +8,12 @@
 -- '#555b86'
 -- '#626793'
 -- '#545c7e'
--- '#1e2030'
 -- '#6183bb'
 -- '#1abc9c'
 -- '#cdffb0'
--- '#a395cf'
 -- '#d4eb70'
--- '#1a1423'
 -- '#c5c8c6'
+-- '#f0f0ff'
 -- '#c099ff'
 -- '#a395cf'
 -- '#7fefef'
@@ -47,19 +47,20 @@ local colors = {
     bright_blue =    '#6183bb',
     bright_cyan =    '#1abc9c',
     bright_magenta = '#bb9af7',
-    bright_white =   '#f0f0ff',
 }
 
 local variants = {
     light = {
-        background = '#c8d3f5',
-        foreground = '#444a73',
-        black =      '#1e2030',
+        background =   '#e9e9ec',
+        foreground =   '#444a73',
+        black =        '#1e2030',
+        bright_white = '#c8d3f5'
     },
     dark = {
-        background = '#1e2030',
-        foreground = '#c8d3f5',
-        black =      '#444a73',
+        background =   '#1e2030',
+        foreground =   '#c8d3f5',
+        black =        '#444a73',
+        bright_white = '#e9e9ec'
     },
 }
 
