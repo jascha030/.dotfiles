e8ed1d5588a84a0f2e6ef43426827554115cabb7
Jascha030 <contact@jaschavanaalst.nl>
2022-07-27 10:49:49 +0200

    ♻️ Cleanup plugins/init.lua

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,13 +1,5 @@
 require('plugins.config')
 
-local config = {
-    utils = require('plugins.config.utils'),
-    lsp = require('plugins.config.lsp'),
-    ui = require('plugins.config.ui'),
-}
-
-local devicons = { 'kyazdani42/nvim-web-devicons' }
-
 -- Auto commands
 vim.cmd([[autocmd BufWritePost plugins/init.lua PackerCompile]])
 vim.cmd([[packadd packer.nvim]], false)
@@ -16,16 +8,14 @@ require('packer').startup({
     function(use)
         use({ 'wbthomason/packer.nvim' })
         --use({ 'lewis6991/impatient.nvim', config = [[require('impatient')]] })
-
-        use({ 'kyazdani42/nvim-web-devicons', config = config.ui.devicons })
-        use({ 'yamatsum/nvim-nonicons', require = devicons })
-        use({ 'goolord/alpha-nvim', config = config.utils.alpha })
-
+        use({ 'kyazdani42/nvim-web-devicons', config = require('plugins.config.ui.devicons') })
+        use({ 'yamatsum/nvim-nonicons', requires = 'kyazdani42/nvim-web-devicons' })
+        use({ 'goolord/alpha-nvim', config = PluginConfig('utils', 'alpha') })
         -- Language/syntax
         use({ 'neovim/nvim-lspconfig', 'williamboman/nvim-lsp-installer' })
         use({ 'onsails/lspkind-nvim' })
         use({ 'jose-elias-alvarez/null-ls.nvim' })
-        use({ 'hrsh7th/nvim-cmp', config = config.lsp.cmp })
+        use({ 'hrsh7th/nvim-cmp', config = PluginConfig('lsp', 'cmp') })
         use({ 'hrsh7th/cmp-nvim-lsp' })
         use({ 'hrsh7th/cmp-path' })
         use({ 'hrsh7th/cmp-buffer' })
@@ -34,71 +24,48 @@ require('packer').startup({
         use({ 'saadparwaiz1/cmp_luasnip' })
         use({ 'simrat39/rust-tools.nvim' })
         use({ 'ncm2/ncm2' })
-
         -- Visual/UI components
         -- Telescope
         use({
             'nvim-telescope/telescope.nvim',
-            requires = {
-                { 'nvim-lua/popup.nvim' },
-                { 'nvim-lua/plenary.nvim' },
-            },
-            config = config.utils.telescope,
+            requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } },
+            config = PluginConfig('utils', 'telescope'),
         })
         use({ 'nvim-telescope/telescope-file-browser.nvim' })
         use({ 'nvim-telescope/telescope-fzy-native.nvim' })
         use({ 'nvim-telescope/telescope-ui-select.nvim' })
-        use({
-            'ziontee113/color-picker.nvim',
-            config = function()
-                local opts = { noremap = true, silent = true }
-
-                vim.keymap.set('n', '<C-p><C-p>', '<cmd>PickColor<cr>', opts)
-                vim.keymap.set('i', '<C-p><C-p>', '<cmd>PickColorInsert<cr>', opts)
-
-                require('color-picker').setup({})
-            end,
-        })
-        use({
-            'filipdutescu/renamer.nvim',
-            branch = 'master',
-            requires = { { 'nvim-lua/plenary.nvim' } },
-        })
-
+        use({ 'ziontee113/color-picker.nvim', config = PluginConfig('utils', 'color_picker') })
+        use({ 'filipdutescu/renamer.nvim', branch = 'master', requires = { { 'nvim-lua/plenary.nvim' } } })
         -- Treesitter
         use({ 'nvim-treesitter/nvim-treesitter-textobjects' })
         use({ 'nvim-treesitter/playground' })
         use({ 'p00f/nvim-ts-rainbow' })
-        use({ 'nvim-treesitter/nvim-treesitter', irun = ':TSUpdate', config = config.utils.treesitter })
+        use({ 'nvim-treesitter/nvim-treesitter', irun = ':TSUpdate', config = PluginConfig('utils', 'treesitter') })
         use({ 'voldikss/vim-floaterm' })
-        use({ 'kyazdani42/nvim-tree.lua', config = config.ui.tree })
-        use({ 'petertriho/nvim-scrollbar', config = config.ui.scrollbar })
-        use({ 'noib3/nvim-cokeline', config = config.ui.cokeline })
-        use({ 'hoob3rt/lualine.nvim', config = config.ui.lualine })
+        use({ 'kyazdani42/nvim-tree.lua', config = PluginConfig('ui', 'tree') })
+        use({ 'petertriho/nvim-scrollbar', config = PluginConfig('ui', 'scrollbar') })
+        use({ 'noib3/nvim-cokeline', config = PluginConfig('ui', 'cokeline') })
+        use({ 'hoob3rt/lualine.nvim', config = PluginConfig('ui', 'lualine') })
         use({ 'is0n/fm-nvim' })
         use({
             'folke/trouble.nvim',
             requires = 'kyazdani42/nvim-web-devicons',
-            config = config.lsp.trouble,
+            config = PluginConfig('lsp', 'trouble'),
         })
         use({
             'j-hui/fidget.nvim',
-            config = function()
-                require('fidget').setup({})
-            end,
+            config = PluginConfig('utils', 'fidget'),
         })
-
         -- Theme
         use({ 'tjdevries/colorbuddy.nvim' })
-        use({ 'norcalli/nvim-colorizer.lua', config = config.ui.colorizer })
+        use({ 'norcalli/nvim-colorizer.lua', config = PluginConfig('ui', 'colorizer') })
         use({ 'marko-cerovac/material.nvim' })
         use({ 'folke/tokyonight.nvim' })
-
         -- Other
         use({ 'karb94/neoscroll.nvim' })
         use({ 'mfussenegger/nvim-dap' })
         use({ 'ojroques/vim-oscyank' })
-        use({ 'phaazon/hop.nvim', branch = 'v1', config = config.utils.hop })
+        use({ 'phaazon/hop.nvim', branch = 'v1', config = PluginConfig('utils', 'hop') })
         use({ 'wakatime/vim-wakatime' })
     end,
     config = {
