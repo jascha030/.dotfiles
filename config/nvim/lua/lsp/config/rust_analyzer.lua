95437444fd285b275d4652814334734cc3795826
Jascha030 <contact@jaschavanaalst.nl>
2022-07-28 11:37:26 +0200

    ♻️ Forgot to commit a file tho :P

--- config/nvim/lua/lsp/config/rust_analyzer.lua
+++ config/nvim/lua/lsp/config/rust_analyzer.lua
@@ -1,16 +1,14 @@
 return {
-    settings = {
-        ['rust-analyzer'] = {
-            assist = {
-                importGranularity = 'module',
-                importPrefix = 'by_self',
-            },
-            cargo = {
-                loadOutDirsFromCheck = true,
-            },
-            procMacro = {
-                enable = true,
-            },
+    ['rust-analyzer'] = {
+        assist = {
+            importGranularity = 'module',
+            importPrefix = 'by_self',
+        },
+        cargo = {
+            loadOutDirsFromCheck = true,
+        },
+        procMacro = {
+            enable = true,
         },
     },
 }
