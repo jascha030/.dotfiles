ffb2d964a6f9f9fa81c342112aee00b55435c605
Jascha030 <contact@jaschavanaalst.nl>
2022-08-01 11:25:35 +0200

    ♻️ Implement Mason.

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -1,26 +1,38 @@
-if not require('util').validate({ 'lspconfig', 'nvim-lsp-installer', 'null-ls' }) then
+if not require('util').validate({ 'lspconfig', 'mason', 'mason-lspconfig', 'null-ls' }) then
     return
 end
 
-require('nvim-lsp-installer').setup({})
-
-local servers = require('nvim-lsp-installer.servers').get_installed_server_names()
 local lspconfig = require('lspconfig')
+local mason = require('mason')
+local mason_lsp = require('mason-lspconfig')
 local null_ls = require('null-ls')
 local handlers = require('lsp.handlers')
 
-for _, servername in ipairs(servers) do
-    local server_config = handlers.get_server_config(servername)
 
-    lspconfig[servername].setup(server_config)
-end
+mason.setup({})
+
+mason_lsp.setup({
+    ensure_installed = {
+        'bashls',
+        'html',
+        'intelephense',
+        'rust_analyzer',
+        'sumneko_lua',
+    },
+})
 
 handlers.setup()
+
+mason_lsp.setup_handlers({
+    function(servername)
+        lspconfig[servername].setup(handlers.get_server_config(servername))
+    end,
+})
 
 null_ls.setup({
     sources = {
         null_ls.builtins.formatting.stylua.with({
-            extra_args = { '--config-path', os.getenv('HOME') .. '/.config/stylua.toml' },
+            extra_args = { '--config-path', os.getenv('XDG_CONFIG') .. '/stylua.toml' },
         }),
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.completion.spell,
