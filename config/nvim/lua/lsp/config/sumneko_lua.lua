1c1c8d0e2b0db9750ff40222dbf817499da3af0a
Jascha030 <contact@jaschavanaalst.nl>
2022-08-01 11:25:23 +0200

    ♻️ Fix overkill load times.

--- config/nvim/lua/lsp/config/sumneko_lua.lua
+++ config/nvim/lua/lsp/config/sumneko_lua.lua
@@ -1,47 +1,135 @@
 local util = require('util')
 
--- Loaded runtime package paths
-local runtime_path = vim.split(package.path, ';')
+local root = os.getenv('HOME') .. '/tools/lua-language-server'
+local binary = root .. '/bin/lua-language-server'
 
--- Hammerspoon runtime
-local hs_version = vim.fn.system('hs -c _VERSION'):gsub('[\n\r]', '')
-local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
+-- workspace
+local WS = {}
 
--- Wildcards
-table.insert(runtime_path, 'lua/?.lua')
-table.insert(runtime_path, 'lua/?/init.lua')
+function WS.in_dotfiles()
+    return util.cwd_in(vim.fn.expand('$DOTFILES'))
+end
 
--- Merge runtime_path with hammerspoon paths
-runtime_path = util.tbl_merge(runtime_path, hs_path)
+function WS.in_hammerspoon()
+    return (util.cwd_in(vim.fn.expand('$HOME/.hammerspoon')) or WS.in_dotfiles())
+end
 
--- Merge Neovim runtime with other libraries
-local library = vim.api.nvim_get_runtime_file('', true)
+function WS.in_neovim()
+    return (util.cwd_in(vim.fn.stdpath('config')) or WS.in_dotfiles())
+end
 
-library = util.tbl_merge(library, {
-    vim.fn.expand('$WEZTERM_CONFIG_DIR'),
-    vim.fn.expand('$VIMRUNTIME/lua'),
-    vim.fn.expand('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs'),
-    vim.fn.expand('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations'),
-})
+function WS.in_wez()
+    return (util.cwd_in(vim.fn.expand('$HOME/.hammerspoon')) or WS.in_dotfiles())
+end
 
-return {
-    Lua = {
-        runtime = {
-            version = {
-                'LuaJIT',
-                hs_version,
+local M = {}
+
+function M.path()
+    local path = WS.in_neovim() and vim.split(package.path, ';') or {}
+
+    for _, p in
+        ipairs({
+            'lua/?.lua',
+            'lua/?/init.lua',
+        })
+    do
+        table.insert(path, p)
+    end
+
+    return path
+end
+
+function M.library()
+    --local ret = vim.api.nvim_get_runtime_file('', true)
+    local ret = {}
+
+    local function add(lib, filter)
+        for _, p in pairs(vim.fn.expand(lib, false, true)) do
+            p = vim.loop.fs_realpath(p)
+
+            if p and (not filter or filter[vim.fn.fnamemodify(p, ':h:t')]) then
+                table.insert(ret, p)
+            end
+        end
+    end
+
+    if WS.in_dotfiles() then
+        add('$VIMRUNTIME/lua')
+        add('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs')
+        add('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations')
+        add('$WEZTERM_CONFIG_DIR')
+    end
+
+    if WS.in_neovim() then
+        add('$VIMRUNTIME/lua')
+    end
+
+    if WS.in_hammerspoon() then
+        add('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs')
+        add('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations')
+    end
+
+    if WS.in_wez() then
+        add('$WEZTERM_CONFIG_DIR')
+    end
+
+    return ret
+end
+
+function M.version()
+    local ret = { 'LuaJIT' }
+
+    if WS.in_hammerspoon() then
+        --table.insert(ret, vim.fn.system('hs -c _VERSION'):gsub('[\n\r]', ''))
+    end
+
+    if util.tbl_length(ret) == 1 then
+        return 'LuaJIT'
+    end
+
+    return ret
+end
+
+function M.globals()
+    local ret = {}
+
+    local available_globals = {
+        ['vim'] = WS.in_neovim(),
+        ['hs'] = WS.in_hammerspoon(),
+    }
+
+    for gv, f in pairs(available_globals) do
+        if f then
+            table.insert(ret, gv)
+        end
+    end
+
+    return ret
+end
+
+--local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
+
+local config = {
+    cmd = { binary, '-E', root .. '/main.lua' },
+    settings = {
+        Lua = {
+            runtime = {
+                version = M.version(),
+                maxPreload = 1000,
+                preloadFileSize = 150,
             },
-            path = runtime_path,
-        },
-        diagnostics = {
-            globals = { 'hs', 'vim' },
-        },
-        workspace = {
-            library = library,
-            checkThirdParty = false,
-        },
-        telemetry = {
-            enable = false,
+            diagnostics = {
+                globals = M.globals(),
+            },
+            workspace = {
+                library = M.library(),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
         },
     },
 }
+
+return config
