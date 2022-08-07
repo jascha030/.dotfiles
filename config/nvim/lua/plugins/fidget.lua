90c649d645dbb72dfe31e648d8ddbad40352e30e
Jascha030 <contact@jaschavanaalst.nl>
2022-08-07 23:05:39 +0200

    💄 Fix fidget colors.

--- config/nvim/lua/plugins/fidget.lua
+++ config/nvim/lua/plugins/fidget.lua
@@ -1,10 +1,19 @@
 return function()
     local module = 'fidget'
+
     require(module).setup({
+        text = {
+            spinner = 'dots',
+        },
         window = {
-            relative = 'win',
-            blend = 100,
+            relative = 'editor',
+            blend = 0,
             zindex = nil,
         },
     })
+
+    local colors = require('colors').get_colors()
+
+    vim.cmd('hi FidgetTitle guifg=' .. colors.red)
+    vim.cmd('hi FidgetTask guibg=' .. colors.bg)
 end
