546b563adde2a61e39f2b62b7157e2d4e4674d39
Jascha030 <contact@jaschavanaalst.nl>
2022-08-07 23:05:49 +0200

    🔥 Remove bs comments.

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -1,44 +1,43 @@
-return function ()
+return function()
     return {
-        position = 'bottom', -- position of the list can be: bottom, top, left, right
-        height = 10, -- height of the trouble list when position is top or bottom
+        position = 'bottom',
+        height = 10,
         icons = true,
-        mode = 'workspace_diagnostics', -- "workspace_diagnostics", "document_diagnostics", "quickfix", "lsp_references", "loclist"
-        fold_open = '', -- icon used for open folds
-        fold_closed = '', -- icon used for closed folds
-        action_keys = { -- key mappings for actions in the trouble list
-            close = 'q', -- close the list
-            cancel = '<esc>', -- cancel the preview and get back to your last window / buffer / cursor
-            refresh = 'r', -- manually refresh
-            jump = { '<cr>', '<tab>' }, -- jump to the diagnostic or open / close folds
-            open_split = { '<c-x>' }, -- open buffer in new split
-            open_vsplit = { '<c-v>' }, -- open buffer in new vsplit
-            open_tab = { '<c-t>' }, -- open buffer in new tab
-            jump_close = { 'o' }, -- jump to the diagnostic and close the list
-            toggle_mode = 'm', -- toggle between "workspace" and "document" diagnostics mode
-            toggle_preview = 'P', -- toggle auto_preview
-            hover = 'K', -- opens a small popup with the full multiline message
-            preview = 'p', -- preview the diagnostic location
-            close_folds = { 'zM', 'zm' }, -- close all folds
-            open_folds = { 'zR', 'zr' }, -- open all folds
-            toggle_fold = { 'zA', 'za' }, -- toggle fold of current file
-            previous = 'k', -- preview item
-            next = 'j', -- next item
+        mode = 'workspace_diagnostics',
+        fold_open = '',
+        fold_closed = '',
+        action_keys = {
+            close = 'q',
+            cancel = '<esc>',
+            refresh = 'r',
+            jump = { '<cr>', '<tab>' },
+            open_split = { '<c-x>' },
+            open_vsplit = { '<c-v>' },
+            open_tab = { '<c-t>' },
+            jump_close = { 'o' },
+            toggle_mode = 'm',
+            toggle_preview = 'P',
+            hover = 'K',
+            preview = 'p',
+            close_folds = { 'zM', 'zm' },
+            open_folds = { 'zR', 'zr' },
+            toggle_fold = { 'zA', 'za' },
+            previous = 'k',
+            next = 'j',
         },
-        indent_lines = true, -- add an indent guide below the fold icons
-        auto_open = false, -- automatically open the list when you have diagnostics
-        auto_close = false, -- automatically close the list when you have no diagnostics
-        auto_preview = true, -- automatically preview the location of the diagnostic. <esc> to close preview and go back to last window
-        auto_fold = false, -- automatically fold a file trouble list at creation
-        auto_jump = { 'lsp_definitions' }, -- for the given modes, automatically jump if there is only a single result
+        indent_lines = true,
+        auto_open = false,
+        auto_close = false,
+        auto_preview = true,
+        auto_fold = false,
+        auto_jump = { 'lsp_definitions' },
         signs = {
-            -- icons / text used for a diagnostic
             error = '',
             warning = '',
             hint = '',
             information = '',
             other = '﫠',
         },
-        use_diagnostic_signs = false, -- enabling this will use the signs defined in your lsp client
+        use_diagnostic_signs = false,
     }
 end
