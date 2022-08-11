0529c3ae0919f6a1b24904d790e9c08b0230765a
Jascha030 <contact@jaschavanaalst.nl>
2022-08-11 10:27:14 +0200

    🚧 Starting over with this icon shiz.

--- config/nvim/lua/plugins/devicons.lua
+++ config/nvim/lua/plugins/devicons.lua
@@ -1,37 +1,33 @@
-return function()
-    local colors = require('colors')
+local devicons = require('nvim-web-devicons')
+local colors = require('colors').get_colors()
 
-    local icons = {
-        git = {
-            icon = '',
-            color = colors.bright_blue,
-            cterm_color = '12',
-        },
-        term = {
-            icon = '',
-            color = colors.green,
-            cterm_color = '71',
-        },
+local icons = {
+    git = {
+        icon = '',
+        color = colors.bright_blue,
+        cterm_color = '12',
+    },
+    term = {
+        icon = '',
+        color = colors.green,
+        cterm_color = '71',
+    },
+}
 
-    }
-
-    require('nvim-web-devicons').setup({
-        default_icon = '',
-        override = {
-            ['.zshrc'] = icons.term,
-            ['.zprofile'] = icons.term,
-            ['.zshenv'] = icons.term,
-            ['init'] = {
-                icon = '⏻',
-                color = colors.red,
-                cterm_color = '1',
-            },
-            --['hushlogin'] = {
-            --    icon = '',
-            --    color = colors.bright_black,
-            --    cterm_color = '8',
-            --    name = 'Muted',
-            --},
+require('nvim-web-devicons').setup({
+    default_icon = '',
+    override = {
+        ['init'] = {
+            icon = '⏻',
+            color = colors.red,
+            cterm_color = '1',
         },
-    })
-end
+    },
+})
+
+devicons.set_icon({
+    zsh = {
+        icons.term,
+        color = colors.magenta,
+    },
+})
