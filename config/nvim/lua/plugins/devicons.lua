29e5172db7b0deb2c8ceafa650851bd46c971f56
Jascha030 <contact@jaschavanaalst.nl>
2022-08-24 13:38:43 +0200

    💄 Add whole buncho cuteness.

--- config/nvim/lua/plugins/devicons.lua
+++ config/nvim/lua/plugins/devicons.lua
@@ -3,11 +3,33 @@ return function()
     local colors = require('colors').get_colors()
 
     local icons = {
-        git = '',
-        term = '',
-        init = '⏻',
-        bookmark = '',
         alias = '',
+        asterisk = '',
+        bookmark = '',
+        brush = '',
+        calendar = '',
+        computer = '',
+        database = '',
+        documentation = '',
+        editor = '',
+        fileinfo = '',
+        git_sync = '',
+        git_branch = '',
+        git_merge = '',
+        git_reject = '',
+        ignore = '',
+        init = '⏻',
+        key = '',
+        list = '',
+        loadspeaker = '',
+        mac = '',
+        mute = '',
+        pin = '',
+        rocket = '',
+        scholar = '',
+        telescope = ' ',
+        term = '',
+        wrench = '',
     }
 
     local function get(name)
@@ -22,13 +44,29 @@ return function()
 
     require('nvim-web-devicons').setup({
         default_icon = '',
-        override = {},
     })
 
+    local ignore = {
+        icon = get('ignore'),
+        color = colors.git.ignore,
+        name = 'Ignore',
+    }
+
+    local git_config = {
+        icon = get('git_branch'),
+        color = colors.yellow,
+        name = 'GitConfig',
+    }
+
     devicons.set_icon({
+        ['.editorconfig'] = {
+            icon = get('editor'),
+            color = colors.yellow,
+            name = 'EditorConfig',
+        },
         ['.zshrc'] = {
             icon = get('term'),
-            color = colors.bright_magenta,
+            color = colors.magenta,
             name = 'Zshrc',
         },
         ['.antigenrc'] = {
@@ -44,10 +82,52 @@ return function()
         ['init'] = {
             icon = get('init'),
             color = colors.red,
+            name = 'Init',
+        },
+        ['init.lua'] = {
+            icon = get('init'),
+            color = colors.magenta,
+            name = 'InitLua',
         },
         ['aliases'] = {
             icon = get('alias'),
             color = colors.yellow,
+            name = 'Alias',
+        },
+        ['auto-ls'] = {
+            icon = get('fileinfo'),
+            color = colors.cyan,
+            name = 'Autols',
+        },
+        ['fzf'] = {
+            icon = get('telescope'),
+            color = colors.red,
+            name = 'Fzf',
+        },
+        ['hushlogin'] = {
+            icon = get('mute'),
+            color = colors.git.ignore,
+            name = 'Hushlogin',
+        },
+        ['.gitignore'] = ignore,
+        ['gitignore_global'] = ignore,
+        ['gitconfig'] = git_config,
+        ['.gitconfig'] = git_config,
+        ['.styluaignore'] = ignore,
+        ['my.cnf'] = {
+            icon = get('database'),
+            color = colors.blue,
+            name = 'MyCnf',
+        },
+        ['README.md'] = {
+            icon = get('documentation'),
+            color = colors.orange,
+            name = 'README',
+        },
+        ['starship.toml'] = {
+            icon = get('rocket'),
+            color = colors.magenta,
+            name = 'Starship',
         },
     })
 end
