6594acff36ca38a63754b0b7970b8d969d8820da
Jascha030 <contact@jaschavanaalst.nl>
2022-08-27 16:45:18 +0200

    ✨ Start plugins module

---
+++ config/nvim/lua/plugins.lua
@@ -0,0 +1,33 @@
+local packer = nil
+
+function init()
+	if not packer then
+		vim.cmd([[packadd packer.nvim]])
+		packer = require("packer")
+	end
+
+	packer.init({
+		disable_commands = true,
+		display = {
+			open_fn = function()
+				return require("packer.util").float({ border = "single" })
+			end,
+		},
+		profile = {
+			enable = true,
+			threshold = 1,
+		},
+	})
+	local use = packer.use
+	packer.reset()
+
+	use({ "wbthomason/packer.nvim", opt = true })
+end
+local plugins = setmetatable({}, {
+	__index = function(_, key)
+		init()
+		return packer[key]
+	end,
+})
+
+return plugins
