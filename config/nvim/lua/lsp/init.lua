872c6b6de30aa91ff4d3a3c8f68b58eb9e406506
Jascha030 <contact@jaschavanaalst.nl>
2022-08-28 23:00:41 +0200

    🐛 Didn't seem to work, so back to old for a minute

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -1,57 +1,66 @@
+local utils = require('utils')
+if not utils.validate({ 'lspconfig', 'mason', 'mason-lspconfig', 'cmp_nvim_lsp', 'null-ls', 'rust-tools' }, 'lsp') then
+    return
+end
+
 local M = {}
 
 local loaded = false
-
 local cmp_lsp = require('cmp_nvim_lsp')
-local server_defaults = {
-    on_attach = require('lsp.on_attach'),
-    capabilities = cmp_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities()),
-    flags = { debounce_text = 150 },
-}
+local default = require('lsp.config')
+local capabilities = cmp_lsp.update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+local function get_default_server_config()
+    return vim.tbl_deep_extend('force', default.server, { capabilities = capabilities })
+end
 
 local function load_server_config(server_name)
     local ok, config = pcall(require, 'lsp.config.' .. server_name)
 
     if not ok or type(config) ~= 'table' then
-        return server_defaults
+        return get_default_server_config()
     end
 
     return config
 end
 
 local function get_server_config(server_name, opts)
-    opts = opts or server_defaults
+    opts = opts or get_default_server_config()
 
     return vim.tbl_deep_extend('force', opts, load_server_config(server_name))
 end
 
-local function init()
+function M.setup_lsp(config)
+    for _, sign in ipairs(config.diagnostic.signs.active) do
+        vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+    end
+
+    vim.diagnostic.config(config.diagnostic)
+    vim.lsp.handlers['textDocument/hover'] = config.handlers.hover
+    vim.lsp.handlers['textDocument/signatureHelp'] = config.handlers.signature_help
+end
+
+function M.setup_extensions(opts)
     local lspconfig = require('lspconfig')
+    local mason = require('mason')
     local mason_lsp = require('mason-lspconfig')
+    local null_ls = require('null-ls')
     local rt = require('rust-tools')
 
-    local stylua_path = os.getenv('XDG_CONFIG') .. '/stylua.toml'
-    local null_ls = require('null-ls')
-
-    require('mason').setup({})
-    mason_lsp.setup({
-        ensure_installed = {
-            'bashls',
-            'intelephense',
-            'rust_analyzer',
-            'sumneko_lua',
-        },
-    })
-
+    mason.setup({})
+    mason_lsp.setup(opts['mason-lspconfig'])
     mason_lsp.setup_handlers({
         function(server)
             lspconfig[server].setup(get_server_config(server))
         end,
         -- Overrides
         ['rust_analyzer'] = function()
-            rt.setup({ server = server_defaults })
+            rt.setup({ server = get_default_server_config() })
         end,
     })
+
+    local stylua_path = os.getenv('XDG_CONFIG') .. '/stylua.toml'
+
     null_ls.setup({
         sources = {
             null_ls.builtins.formatting.stylua.with({ extra_args = { '--config-path', stylua_path } }),
@@ -61,8 +70,16 @@ local function init()
     })
 end
 
-function M.init()
-    init()
+function M.init(opts)
+    if loaded == true then
+        return
+    end
+
+    loaded = true
+    opts = vim.tbl_deep_extend('force', default, opts or default)
+
+    M.setup_lsp(opts.lsp)
+    M.setup_extensions(opts.extensions)
 end
 
 return M
