956f5bc99170c90c13df434bda55de3eba593a8d
Jascha030 <contact@jaschavanaalst.nl>
2022-08-29 13:09:37 +0200

    🎨 Formatting and additional folder where $VIMRUNTIME should be loaded.

--- config/nvim/lua/lsp/config/sumneko_lua.lua
+++ config/nvim/lua/lsp/config/sumneko_lua.lua
@@ -1,127 +1,133 @@
-local util = require("utils")
-local root = os.getenv("HOME") .. "/tools/lua-language-server"
-local binary = root .. "/bin/lua-language-server"
+local util = require('utils')
+local root = os.getenv('HOME') .. '/tools/lua-language-server'
+local binary = root .. '/bin/lua-language-server'
 
 local WS = {}
 
 function WS.in_dotfiles()
-	return util.fs.cwd_in(vim.fn.expand("$DOTFILES"))
+    return util.fs.cwd_in(vim.fn.expand('$DOTFILES'))
 end
 
 function WS.in_hammerspoon()
-	return (util.fs.cwd_in(vim.fn.expand("$HOME/.hammerspoon")) or WS.in_dotfiles())
+    return (util.fs.cwd_in(vim.fn.expand('$HOME/.hammerspoon')) or WS.in_dotfiles())
 end
 
 function WS.in_neovim()
-	return (util.fs.cwd_in(vim.fn.stdpath("config")) or WS.in_dotfiles())
+    return (
+        util.fs.cwd_in(vim.fn.stdpath('config'))
+        or WS.in_dotfiles()
+        or util.fs.cwd_in(vim.fn.expand('$HOME/.development/Projects/lua/nitepal.nvim'))
+    )
 end
 
 function WS.in_wez()
-	return (util.fs.cwd_in(vim.fn.expand("$HOME/.hammerspoon")) or WS.in_dotfiles())
+    return (util.fs.cwd_in(vim.fn.expand('$HOME/.hammerspoon')) or WS.in_dotfiles())
 end
+
+function WS.in_nvim_project() end
 
 local M = {}
 
 function M.path()
-	local path = WS.in_neovim() and vim.split(package.path, ";") or {}
+    local path = WS.in_neovim() and vim.split(package.path, ';') or {}
 
-	for _, p in ipairs({
-		"lua/?.lua",
-		"lua/?/init.lua",
-	}) do
-		table.insert(path, p)
-	end
+    for _, p in ipairs({
+        'lua/?.lua',
+        'lua/?/init.lua',
+    }) do
+        table.insert(path, p)
+    end
 
-	return path
+    return path
 end
 
 function M.library()
-	--local ret = vim.api.nvim_get_runtime_file('', true)
-	local ret = {}
+    --local ret = vim.api.nvim_get_runtime_file('', true)
+    local ret = {}
 
-	local function add(lib, filter)
-		for _, p in pairs(vim.fn.expand(lib, false, true)) do
-			p = vim.loop.fs_realpath(p)
+    local function add(lib, filter)
+        for _, p in pairs(vim.fn.expand(lib, false, true)) do
+            p = vim.loop.fs_realpath(p)
 
-			if p and (not filter or filter[vim.fn.fnamemodify(p, ":h:t")]) then
-				table.insert(ret, p)
-			end
-		end
-	end
+            if p and (not filter or filter[vim.fn.fnamemodify(p, ':h:t')]) then
+                table.insert(ret, p)
+            end
+        end
+    end
 
-	if WS.in_dotfiles() then
-		add("$VIMRUNTIME/lua")
-		add("/Applications/Hammerspoon.app/Contents/Resources/extensions/hs")
-		add("$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations")
-		add("$WEZTERM_CONFIG_DIR")
-	end
+    if WS.in_dotfiles() then
+        add('$VIMRUNTIME/lua')
+        add('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs')
+        add('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations')
+        add('$WEZTERM_CONFIG_DIR')
+    end
 
-	if WS.in_neovim() then
-		add("$VIMRUNTIME/lua")
-	end
+    if WS.in_neovim() then
+        add('$VIMRUNTIME/lua')
+    end
 
-	if WS.in_hammerspoon() then
-		add("/Applications/Hammerspoon.app/Contents/Resources/extensions/hs")
-		add("$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations")
-	end
+    if WS.in_hammerspoon() then
+        add('/Applications/Hammerspoon.app/Contents/Resources/extensions/hs')
+        add('$HOME/.hammerspoon/Spoons/EmmyLua.spoon/annotations')
+    end
 
-	if WS.in_wez() then
-		add("$WEZTERM_CONFIG_DIR")
-	end
+    if WS.in_wez() then
+        add('$WEZTERM_CONFIG_DIR')
+    end
 
-	return ret
+    return ret
 end
 
 function M.version()
-	local ret = { "LuaJIT" }
+    local ret = { 'LuaJIT' }
 
-	if util.tbl.tbl_length(ret) == 1 then
-		return "LuaJIT"
-	end
+    if util.tbl.tbl_length(ret) == 1 then
+        return 'LuaJIT'
+    end
 
-	return ret
+    return ret
 end
 
 function M.globals()
-	local ret = {}
+    local ret = {}
 
-	local available_globals = {
-		["vim"] = WS.in_neovim(),
-		["hs"] = WS.in_hammerspoon(),
-	}
+    local available_globals = {
+        ['vim'] = WS.in_neovim(),
+        ['hs'] = WS.in_hammerspoon(),
+    }
 
-	for gv, f in pairs(available_globals) do
-		if f then
-			table.insert(ret, gv)
-		end
-	end
+    for gv, f in pairs(available_globals) do
+        if f then
+            table.insert(ret, gv)
+        end
+    end
 
-	return ret
+    return ret
 end
 
 --local hs_path = vim.split(vim.fn.system('hs -c package.path'):gsub('[\n\r]', ''), ';')
 
 local config = {
-	cmd = { binary, "-E", root .. "/main.lua" },
-	settings = {
-		Lua = {
-			runtime = {
-				version = M.version(),
-				maxPreload = 1000,
-				preloadFileSize = 150,
-			},
-			diagnostics = {
-				globals = M.globals(),
-			},
-			workspace = {
-				library = M.library(),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+    cmd = { binary, '-E', root .. '/main.lua' },
+    settings = {
+        Lua = {
+            runtime = {
+                version = M.version(),
+                maxPreload = 1000,
+                preloadFileSize = 150,
+            },
+            diagnostics = {
+                globals = M.globals(),
+            },
+            workspace = {
+                library = M.library(),
+                checkThirdParty = false,
+            },
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
 }
 
 return config
