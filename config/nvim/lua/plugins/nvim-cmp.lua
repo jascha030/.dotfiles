84bef3e77f3b15fdc2614221cd8177fbe3e5a3f3
Jascha030 <contact@jaschavanaalst.nl>
2022-09-02 11:02:20 +0200

    🐛 Fix tab not working

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -54,8 +54,12 @@ return function()
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
-            elseif luasnip.jumpable then
-                luasnip.jump()
+            elseif luasnip.expandable() then
+                luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif check_backspace() then
+                fallback()
             else
                 fallback()
             end
