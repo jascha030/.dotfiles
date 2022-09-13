e34656a4653fb23402197f52ef98feb63529cd7f
Jascha030 <contact@jaschavanaalst.nl>
2022-09-13 22:05:04 +0200

    🚚 Move plugin configs back to config module with new loader module thingeyy...

---
+++ config/nvim/lua/config/cokeline.lua
@@ -0,0 +1,7 @@
+return {
+    show_if_buffers_are_at_least = 2,
+    buffers = {
+        focus_on_delete = 'prev',
+        new_buffers_position = 'next',
+    },
+}
---
+++ config/nvim/lua/config/lualine.lua
@@ -0,0 +1,30 @@
+return function()
+    local git_blame = require('gitblame')
+
+    require('lualine').setup({
+        options = {
+            icons_enabled = true,
+            component_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+            disabled_filetypes = { 'NvimTree' },
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'branch' },
+            lualine_c = { 'filename', { git_blame.get_current_blame_text, cond = git_blame.is_blame_text_available } },
+            lualine_x = { 'encoding', 'fileformat', 'filetype' },
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
+        inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { 'filename' },
+            lualine_x = { 'location' },
+            lualine_y = {},
+            lualine_z = {},
+        },
+        tabline = {},
+        extensions = {},
+    })
+end
