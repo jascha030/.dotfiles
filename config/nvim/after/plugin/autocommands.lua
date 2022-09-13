376c96803359bccd14d9b7501516cebd8397de08
Jascha030 <contact@jaschavanaalst.nl>
2022-09-13 22:07:54 +0200

    🔧 Yes this is an autocommand, but it is so related to the colorscheme setup and toggle functionality that Imma move it over hieaur.

--- config/nvim/after/plugin/autocommands.lua
+++ config/nvim/after/plugin/autocommands.lua
@@ -1,15 +1,3 @@
 local utils = nil
 
-vim.api.nvim_create_autocmd('Signal', {
-    pattern = 'SIGUSR1',
-    callback = function()
-        if utils == nil then
-            utils = require('utils')
-        end
-
-        utils.theme.set_from_os()
-    end,
-})
-
 vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
-
--- config/nvim/after/plugin/colors.lua
+++ config/nvim/after/plugin/colors.lua
@@ -7,6 +7,18 @@ else
     vim.cmd('colorscheme ' .. config.colorscheme)
 end
 
+vim.api.nvim_create_autocmd('Signal', {
+    pattern = 'SIGUSR1',
+    callback = function()
+        if utils == nil then
+            utils = require('utils')
+        end
+
+        utils.theme.set_from_os()
+
+        require('config.nvim-tree')()
+    end,
+})
+
 -- Plugin setups
 utils.icons.setup(utils.conf.devicons)
-
