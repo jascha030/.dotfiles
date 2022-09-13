eaf9e618f521d0c98a8479323465aae3068feb40
Jascha030 <contact@jaschavanaalst.nl>
2022-09-13 11:07:14 +0200

    🎨 cleanup autocmds

--- config/nvim/after/plugin/autocommands.lua
+++ config/nvim/after/plugin/autocommands.lua
@@ -1,20 +1,15 @@
-local utils = require('utils')
+local utils = nil
 
 vim.api.nvim_create_autocmd('Signal', {
     pattern = 'SIGUSR1',
     callback = function()
-        utils.theme.set_from_os()
-    end,
-})
+        if utils == nil then
+            utils = require('utils')
+        end
 
-vim.api.nvim_create_autocmd('ColorScheme', {
-    pattern = 'nitepal',
-    callback = function()
-        utils.icons.setup(utils.conf.devicons)
+        utils.theme.set_from_os()
     end,
 })
 
 vim.cmd([[autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]])
 
-require('lsp').setup()
---vim.cmd([[autocmd Filetype zsh]])
