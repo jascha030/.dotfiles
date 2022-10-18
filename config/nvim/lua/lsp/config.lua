eef755ed49a34172eab02843cb7cadbde964f3e5
Jascha030 <contact@jaschavanaalst.nl>
2022-10-18 22:05:43 +0200

    🗑️ Fix config related to deprecations after update.

--- config/nvim/lua/lsp/config.lua
+++ config/nvim/lua/lsp/config.lua
@@ -94,7 +94,7 @@ local defaults = {
     },
     server = {
         on_attach = on_attach,
-        capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+        capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
         flags = { debounce_text = 150 },
     },
     extensions = {
