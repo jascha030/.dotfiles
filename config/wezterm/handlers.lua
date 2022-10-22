97a2e8bfca7dc15b77ac8bcc3fd13cd48626a60e
Jascha030 <contact@jaschavanaalst.nl>
2022-10-22 22:06:56 +0200

    ♻️ Extract event handlers from wezterm.lua to handlers.lua

---
+++ config/wezterm/handlers.lua
@@ -0,0 +1,156 @@
+local M = {}
+
+local wezterm = require('wezterm')
+local theme = require('theme')
+local font = require('fonts')
+
+M.options = {
+    opacity = 1,
+    colors = theme.get_scheme('Dark', true),
+    alt_font_active = false,
+}
+
+local proc_icons = {
+    ['default'] = ' ',
+    ['nvim'] = ' ',
+}
+
+local function icon(process_name)
+    return proc_icons[process_name] and proc_icons[process_name] .. ' ' or proc_icons['default']
+end
+
+function M.window_config_reloaded(window)
+    local current = window:get_appearance()
+    local overrides = window:get_config_overrides() or {}
+
+    M.options.colors = theme.get_scheme(current, true)
+
+    if overrides.colors ~= M.options.colors then
+        overrides.colors = M.options.colors
+        window:set_config_overrides(overrides)
+    end
+end
+
+-- TODO: extract common code in opacity_* font_size_* and line_height_*
+function M.opacity_up(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.window_background_opacity or M.options.opacity
+
+    if current < 1 then
+        overrides.window_background_opacity = current + 0.01
+        window:set_config_overrides(overrides)
+    end
+end
+
+function M.opacity_down(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.window_background_opacity or M.options.opacity
+
+    if current > 0 then
+        overrides.window_background_opacity = current - 0.01
+        window:set_config_overrides(overrides)
+    end
+end
+
+function M.font_size_up(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.font_size or font.options.size
+
+    overrides.font_size = (current + 0.5)
+    window:set_config_overrides(overrides)
+end
+
+function M.font_size_down(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.font_size or font.options.size
+
+    overrides.font_size = (current - 0.5)
+    window:set_config_overrides(overrides)
+end
+
+function M.line_height_up(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.line_height or font.options.line_height
+
+    if current < 2 then
+        overrides.line_height = (current + 0.01)
+        window:set_config_overrides(overrides)
+    end
+end
+
+function M.line_height_down(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = overrides.line_height or font.options.line_height
+
+    if current > 0 then
+        overrides.line_height = (current - 0.01)
+        window:set_config_overrides(overrides)
+    end
+end
+
+function M.toggle_font(window, _)
+    local overrides = window:get_config_overrides() or {}
+    local current = not M.options.alt_font_active
+
+    overrides.font_rules = font.get_rules(current)
+    M.options.alt_font_active = current
+
+    window:set_config_overrides(overrides)
+end
+
+function M.reset_font(window, _)
+    local overrides = window:get_config_overrides() or {}
+
+    M.options.alt_font_active = false
+
+    overrides.font_rules = font.get_rules(false)
+    overrides.fonts_size = font.options.size
+    overrides.line_height = font.options.line_height
+end
+
+function M.format_tab_title(tab)
+    local title_icon = icon(tab.active_pane.title)
+    local title = ' ' .. tab.tab_index + 1 .. ': ' .. title_icon .. tab.active_pane.title .. ' '
+    local first = tab.tab_index == 0
+
+    local c = {
+        fg = tab.is_active and M.options.colors.foreground or M.options.colors.background,
+        bg = tab.is_active and M.colors.background or M.colors.foreground,
+    }
+
+    return not first
+            and {
+                { Foreground = { Color = M.options.colors.background } },
+                { Background = { Color = M.options.colors.foreground } },
+                { Text = '┇' },
+                { Foreground = { Color = c.fg } },
+                { Background = { Color = c.bg } },
+                { Text = title },
+            }
+        or {
+            { Foreground = { Color = c.fg } },
+            { Background = { Color = c.bg } },
+            { Text = title },
+        }
+end
+
+function M.setup()
+    local events = {
+        ['window-config-reloaded'] = M.window_config_reloaded,
+        ['opacity-up'] = M.opacity_up,
+        ['opacity-down'] = M.opacity_down,
+        ['font-size-up'] = M.font_size_up,
+        ['font-size-down'] = M.font_size_down,
+        ['line-height-up'] = M.line_height_up,
+        ['line-height-down'] = M.line_height_down,
+        ['toggle-font'] = M.toggle_font,
+        ['format-tab-title'] = M.format_tab_title,
+        ['reset-font'] = M.reset_font,
+    }
+
+    for event, handler in pairs(events) do
+        wezterm.on(event, handler)
+    end
+end
+
+return M
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
-local wezterm = require('wezterm')
-local theme = require('theme')
 local font = require('fonts')
+local handlers = require('handlers')
 
 font.extend({
     main = 'MesloLGS Nerd Font',
@@ -8,135 +7,17 @@ font.extend({
     italic = 'Dank Mono',
 })
 
-local line_height_large = 1.7
-
-local proc_icons = {
-    ['default'] = ' ',
-    ['nvim'] = ' ',
-}
-
-local function icon(process_name)
-    return proc_icons[process_name] and proc_icons[process_name] .. ' ' or proc_icons['default']
-end
-
-local colors = theme.get_scheme('Dark', true)
-local opacity = 1
-
-wezterm.on('window-config-reloaded', function(window)
-    local current = window:get_appearance()
-    local overrides = window:get_config_overrides() or {}
-    colors = theme.get_scheme(current, true)
-    if overrides.colors ~= colors then
-        overrides.colors = colors
-        window:set_config_overrides(overrides)
-    end
-end)
-
-wezterm.on('opacity-up', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    local current = overrides.window_background_opacity or opacity
-
-    if current >= 1 then
-        return
-    end
-
-    overrides.window_background_opacity = (current + 0.01)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('opacity-down', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    local current = overrides.window_background_opacity or opacity
-
-    if current <= 0 then
-        return
-    end
-
-    overrides.window_background_opacity = (current - 0.01)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('resize-font-up', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    local current = overrides.font_size or font.default_size
-
-    overrides.font_size = (current + 0.5)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('resize-font-down', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    local current = overrides.font_size or font.default_size
-
-    overrides.font_size = (current - 0.5)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('reset-font', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-
-    overrides.font_size = font.default_size
-    overrides.line_height = font.options.line_height
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('big-font', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-
-    overrides.font_size = 19
-    overrides.line_height = line_height_large
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('set-font-alt', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    overrides.font_size = 19
-    overrides.font_rules = font.get_rules(true)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('set-font-default', function(window, _)
-    local overrides = window:get_config_overrides() or {}
-    overrides.font_size = 19
-    overrides.font_rules = font.get_rules(false)
-    window:set_config_overrides(overrides)
-end)
-
-wezterm.on('format-tab-title', function(tab)
-    local title_icon = icon(tab.active_pane.title)
-    local title = ' ' .. tab.tab_index + 1 .. ': ' .. title_icon .. tab.active_pane.title .. ' '
-    local first = tab.tab_index == 0
-
-    local c = {
-        fg = tab.is_active and colors.foreground or colors.background,
-        bg = tab.is_active and colors.background or colors.foreground,
-    }
-
-    return not first
-            and {
-                { Foreground = { Color = colors.background } },
-                { Background = { Color = colors.foreground } },
-                { Text = '┇' },
-                { Foreground = { Color = c.fg } },
-                { Background = { Color = c.bg } },
-                { Text = title },
-            }
-        or {
-            { Foreground = { Color = c.fg } },
-            { Background = { Color = c.bg } },
-            { Text = title },
-        }
-end)
+handlers.setup()
 
 return {
     default_prog = { '/usr/local/bin/zsh', '--login' },
     window_decorations = 'NONE | RESIZE',
     window_padding = { left = 2.5, right = 2.5, top = 0, bottom = 0 },
     window_frame = {
-        button_fg = colors.foreground,
-        button_bg = colors.background,
-        button_hover_fg = colors.background,
-        button_hover_bg = colors.foreground,
+        button_fg = handlers.options.colors.foreground,
+        button_bg = handlers.options.colors.background,
+        button_hover_fg = handlers.options.colors.background,
+        button_hover_bg = handlers.options.colors.foreground,
     },
     enable_tab_bar = true,
     use_fancy_tab_bar = false,
@@ -151,7 +32,7 @@ return {
     line_height = font.options.line_height,
     font_size = font.options.size,
     font_rules = font.get_rules(false),
-    colors = colors,
+    colors = handlers.options.colors,
     inactive_pane_hsb = { saturation = 0.98, brightness = 0.9 },
     window_background_opacity = 1,
     keys = require('keymap'),
