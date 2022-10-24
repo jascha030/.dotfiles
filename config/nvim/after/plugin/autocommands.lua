7ef02c57e548747e7209452c17948df60ffd4efa
Jascha030 <contact@jaschavanaalst.nl>
2022-10-24 22:06:36 +0200

    🔧 Open folds by default.

--- config/nvim/after/plugin/autocommands.lua
+++ config/nvim/after/plugin/autocommands.lua
@@ -1,3 +1,26 @@
+local vim = vim
+local api = vim.api
+
+local function nvim_create_augroups(definitions)
+    for group_name, definition in pairs(definitions) do
+        api.nvim_command('augroup ' .. group_name)
+        api.nvim_command('autocmd!')
+
+        for _, def in ipairs(definition) do
+            local command = table.concat(vim.tbl_flatten({ 'autocmd', def }), ' ')
+            api.nvim_command(command)
+        end
+
+        api.nvim_command('augroup END')
+    end
+end
+
+nvim_create_augroups({
+    open_folds = {
+        { 'BufReadPost,FileReadPost', '*', 'normal zR' },
+    },
+})
+
 vim.cmd([[
   autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 ]])
