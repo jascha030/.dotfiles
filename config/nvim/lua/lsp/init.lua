07753390b3e0d94613320ecde13970939404f9ac
Jascha030 <contact@jaschavanaalst.nl>
2022-10-26 13:28:06 +0200

    ♻️ Simplification nation.

--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -2,43 +2,65 @@ local M = {}
 
 local loaded = false
 local default = nil
+local BORDERS = { border = 'rounded' }
 
-local function setup_lsp(conf)
-    for _, sign in ipairs(conf.diagnostic.signs.active) do
+local function setup_lsp()
+    local signs = {
+        { name = 'DiagnosticSignError', text = '' },
+        { name = 'DiagnosticSignWarn', text = '' },
+        { name = 'DiagnosticSignHint', text = '' },
+        { name = 'DiagnosticSignInfo', text = '' },
+    }
+
+    for _, sign in ipairs(signs) do
         vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
     end
 
-    vim.diagnostic.config(conf.diagnostic)
-    vim.lsp.handlers['textDocument/hover'] = conf.handlers.hover
-    vim.lsp.handlers['textDocument/signatureHelp'] = conf.handlers.signature_help
+    vim.diagnostic.config({
+        -- virtual_text = true,
+        float = {
+            focusable = false,
+            style = 'minimal',
+            border = 'rounded',
+            source = 'always',
+            header = '',
+            prefix = '',
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+    })
+
+    vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, BORDERS)
+    vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, BORDERS)
+
     default = require('lsp.config').options.server
 end
 
-local function get_server_config(server_name, opts)
+local function get_server_config(server_name)
     local ok, server_config = pcall(require, 'lsp.config.' .. server_name)
 
     if not ok then
         return vim.tbl_deep_extend('force', {}, default)
     end
 
-    if opts and type(server_config) == 'function' then
-        local conf_ok
-        conf_ok, server_config = pcall(server_config, opts)
-
-        if not conf_ok then
-            return vim.tbl_deep_extend('force', {}, default)
-        end
-    end
-
     return vim.tbl_deep_extend('force', {}, default, server_config)
 end
 
-local function setup_mason(opts)
-    require('mason').setup(opts['mason'] or {})
-    require('mason-lspconfig').setup(opts['mason-lspconfig'] or {})
+local function setup_mason()
+    require('mason').setup({ ui = BORDERS })
+    require('mason-lspconfig').setup({
+        ensure_installed = {
+            'bashls',
+            'intelephense',
+            'rust_analyzer',
+            'sumneko_lua',
+        },
+    })
+
     require('mason-lspconfig').setup_handlers({
         function(server)
-            local conf = get_server_config(server, opts.server_opts[server] or nil)
+            local conf = get_server_config(server)
 
             if server == 'rust_analyzer' then
                 require('rust-tools').setup({ server = conf })
@@ -90,7 +112,7 @@ function M.setup(opts)
     config.extend(opts)
 
     setup_lsp(config.options.lsp)
-    setup_mason(config.options.extensions)
+    setup_mason()
     setup_null_ls()
 
     require('lsp_signature').setup()
