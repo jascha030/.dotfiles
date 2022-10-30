2b96c1623338b4494034eea5fc60bf7f2b7411ef
Jascha030 <contact@jaschavanaalst.nl>
2022-10-30 16:05:43 +0100

    💩 comment php ftplugin code.

--- config/nvim/after/ftplugin/php.lua
+++ config/nvim/after/ftplugin/php.lua
@@ -1,32 +1,31 @@
-local M = {}
-
-function M.dap()
-    local dap = require('dap')
-
-    dap.adapters.php = {
-        type = 'executable',
-        command = 'node',
-        args = { '/Users/jaschavanaalst/.config/dap/vscode-php-debug/out/phpDebug.js' },
-    }
-
-    dap.configurations.php = {
-        {
-            type = 'php',
-            request = 'launch',
-            name = 'Listen for xdebug',
-            port = '9003',
-            pathMappings = {
-                ['/var/www/html'] = '${workspaceFolder}/app/public',
-            },
-        },
-    }
-
-    vim.api.nvim_exec([[au FileType dap-repl lua require('dap.ext.autocompl').attach()]], false)
-    vim.keymap.set('n', '<leader>d', [[<cmd>:DapToggleRepl<CR>]], { silent = true })
-    vim.keymap.set('n', 'DD', [[<cmd>:DapToggleBreakpoint<CR>]], { silent = true })
-
-    require('nvim-dap-virtual-text').setup()
-    require('dapui').setup()
-end
-
-return M
+-- local M = {}
+--
+-- function M.dap()
+--     local dap = require('dap')
+--
+--     dap.adapters.php = {
+--         type = 'executable',
+--         command = 'node',
+--         args = { '/Users/jaschavanaalst/.config/dap/vscode-php-debug/out/phpDebug.js' },
+--     }
+--
+--     dap.configurations.php = {
+--         {
+--             type = 'php',
+--             request = 'launch',
+--             name = 'Listen for xdebug',
+--             port = '9003',
+--             pathMappings = {
+--                 ['/var/www/html'] = '${workspaceFolder}/app/public',
+--             },
+--         },
+--     }
+--
+--     vim.api.nvim_exec([[au FileType dap-repl lua require('dap.ext.autocompl').attach()]], false)
+--     vim.keymap.set('n', '<leader>d', [[<cmd>:DapToggleRepl<CR>]], { silent = true })
+--     vim.keymap.set('n', 'DD', [[<cmd>:DapToggleBreakpoint<CR>]], { silent = true })
+--
+--     require('nvim-dap-virtual-text').setup()
+--     require('dapui').setup()
+-- end
+--
