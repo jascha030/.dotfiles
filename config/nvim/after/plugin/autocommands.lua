88f70ad4caac912f77b50d69fc1abf7cad1f0fa5
Jascha030 <contact@jaschavanaalst.nl>
2022-12-19 20:36:33 +0100

    ✨ Add local env file for credentials n such.

--- config/nvim/after/plugin/autocommands.lua
+++ config/nvim/after/plugin/autocommands.lua
@@ -33,6 +33,7 @@ vim.cmd([[
     autocmd BufReadPost *.ejs.t set ft=embedded_template
     autocmd BufReadPost *.*ignore set ft=gitignore
     autocmd BufReadPost gitignore_global set ft=gitignore
+    autocmd BufReadPost env.local set ft=bash
     autocmd BufReadPost *.cnf set ft=dosini
     autocmd BufReadPost *.antigenrc set ft=zsh
     autocmd BufReadPost Deployfile set ft=json
