e46420597634d2134d8fc28fdbf70144269ce290
Jascha030 <contact@jaschavanaalst.nl>
2022-12-31 16:20:00 +0100

    🐛 Fix oopsie

--- config/nvim/lua/utils/conf.lua
+++ config/nvim/lua/utils/conf.lua
@@ -43,7 +43,6 @@ local Conf = setmetatable({}, {
 
 local function _setup()
     -- vim.o.runtimepath = vim.o.runtimepath .. ',' .. os.getenv('XDG_CONFIG_HOME')
-
     local map = vim.api.nvim_set_keymap
     local default_m_opts = { noremap = true }
 
@@ -60,7 +59,6 @@ local function _setup()
     for mtype, tmaps in pairs(Conf.keymaps) do
         for kmap, args in pairs(tmaps) do
             local a, o = args[1], args[2] or default_m_opts
-
             map(mtype, kmap, a, o)
         end
     end
