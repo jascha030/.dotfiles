ad6bc7a21035fb812db428238ed8f220b825ee48
Jascha030 <contact@jaschavanaalst.nl>
2022-12-31 17:54:37 +0100

    🎨 Cleanup a bit

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,5 +1,4 @@
 --[[========================== Jascha030's =============================--
---                                                                      --
 --   __  __  __  __  ______                __       __  __  ______      --
 --  /\ \/\ \/\ \/\ \/\__  _\   /'\_/`\    /\ \     /\ \/\ \/\  _  \     --
 --  \ \ `\\ \ \ \ \ \/_/\ \/  /\      \   \ \ \    \ \ \ \ \ \ \L\ \    --
@@ -7,27 +6,18 @@
 --    \ \ \`\ \ \ \_/ \ \_\ \__\ \ \_/\ \   \ \ \L\ \\ \ \_\ \ \ \/\ \  --
 --     \ \_\ \_\ `\___/ /\_____\\ \_\\ \_\   \ \____/ \ \_____\ \_\ \_\ --
 --      \/_/\/_/`\/__/  \/_____/ \/_/ \/_/    \/___/   \/_____/\/_/\/_/ --
---                                                                      --
---                                                                      --
 --[[================ beep-beep-Config-2.0-beep-boop ====================]]
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+BORDER = { border = 'rounded' }
+
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+  vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--single-branch', lazyrepo, lazypath })
 end
 
 vim.opt.runtimepath:prepend(lazypath)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
 
 require('utils.conf').setup()
-require('lazy').setup("plugins", {})
-
+require('lazy').setup('plugins', {})
