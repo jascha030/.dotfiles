8aa63c83bb14efb1b2b9d46c537c409e72c7c3e2
Jascha030 <contact@jaschavanaalst.nl>
2023-01-09 09:05:42 +0100

    🐛 Fix phpactor config.

--- config/nvim/lua/lsp/config/phpactor.lua
+++ config/nvim/lua/lsp/config/phpactor.lua
@@ -1,5 +1,10 @@
 return {
-    language_server_phpstan = {
-        enabled = true,
-    }
+    setup = {
+        init_options = {
+            language_server_phpstan = {
+                enabled = true,
+                bin = '/Users/jaschavanaalst/.phive/phars/phpstan-1.9.6.phar',
+            },
+        },
+    },
 }
