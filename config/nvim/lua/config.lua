06ab043595db5a48dc7fb457a345d8124ef63440
Jascha030 <contact@jaschavanaalst.nl>
2023-01-16 22:28:41 +0100

    🔧 Add keymap for telescope git_files

--- config/nvim/lua/config.lua
+++ config/nvim/lua/config.lua
@@ -35,6 +35,7 @@ return {
         ['n'] = {
             ['ff'] = { ':lua require("telescope.builtin").find_files()<CR>' },
             ['FF'] = { ':lua require("telescope").extensions.file_browser.file_browser()<CR>' },
+            ['<C-p>'] = { ':lua require("telescope.builtin").git_files()<CR>'},
             ['fg'] = { ':lua require("telescope.builtin").live_grep()<CR>' },
             ['<C-f>'] = { ':lua require("telescope.builtin").current_buffer_fuzzy_find()<CR>' },
             ['<C-l>'] = { ':lua vim.lsp.buf.format()<CR>' },
@@ -51,9 +52,6 @@ return {
             ['m'] = { '<cmd>Mason<CR>' },
             ['<C-_>'] = { '<cmd>CommentToggle<CR>' },
             ['<leader>CP'] = { '<cmd>PickColor<CR>' },
-            ['<leader>pc'] = { '<cmd>PackerCompile<CR>' },
-            ['<leader>pu'] = { '<cmd>PackerUpdate<CR>' },
-            ['<leader>pi'] = { '<cmd>PackerInstall<CR>' },
             -- TreeSitter
             ['<leader>tc'] = { '<cmd>TSHighlightCapturesUnderCursor<CR>' },
             ['<leader>p'] = { '<cmd>TSPlaygroundToggle<CR>' },
@@ -70,7 +68,7 @@ return {
     },
     devicons = {
         default_icon = '',
-        icons = require('core.icons'),
+        icons = require('core.icons').icons,
         overrides = {
             ['Alias'] = { icon = 'alias', filetypes = 'aliases' },
             ['Autols'] = { icon = 'fileinfo', filetypes = 'auto-ls' },
