ad4e996f4342d616e14da6aa5f3944b6cafdae31
Jascha030 <contact@jaschavanaalst.nl>
2023-04-26 11:24:49 +0200

    🔧 Update null_ls conf

--- config/nvim/lua/plugins/null_ls.lua
+++ config/nvim/lua/plugins/null_ls.lua
@@ -1,31 +1,41 @@
 return {
     'jose-elias-alvarez/null-ls.nvim',
-    config = function()
-        local builtins = require('null-ls').builtins
-        local formatting = builtins.formatting
-        local diagnostics = builtins.diagnostics
+    opts = function(_, opts)
+        local nls = require('null-ls')
 
-        require('null-ls').setup({
-            sources = {
-                formatting.stylua.with({
-                    extra_args = { '--config-path', os.getenv('XDG_CONFIG_HOME') .. '/stylua.toml' },
-                }),
-                diagnostics.eslint,
-                diagnostics.zsh,
-                diagnostics.twigcs,
-                formatting.blade_formatter,
-                formatting.beautysh,
-                formatting.phpcsfixer.with({
-                    args = {
-                        '--no-interaction',
-                        '--quiet',
-                        '--config=' .. os.getenv('HOME') .. '/.config/.php-cs-fixer.php',
-                        'fix',
-                        '$FILENAME',
-                    },
-                }),
-                builtins.completion.spell,
-            },
+        vim.list_extend(opts.sources, {
+            nls.builtins.diagnostics.markdownlint,
+            nls.builtins.diagnostics.selene.with({
+                condition = function(utils)
+                    return utils.root_has_file({ 'selene.toml' })
+                end,
+            }),
+            nls.builtins.formatting.isort,
+            nls.builtins.formatting.black,
+            nls.builtins.diagnostics.flake8,
+            nls.builtins.diagnostics.luacheck.with({
+                condition = function(utils)
+                    return utils.root_has_file({ '.luacheckrc' })
+                end,
+            }),
+            nls.formatting.stylua.with({
+                extra_args = { '--config-path', os.getenv('XDG_CONFIG_HOME') .. '/stylua.toml' },
+            }),
+            nls.diagnostics.eslint,
+            nls.diagnostics.zsh,
+            nls.diagnostics.twigcs,
+            nls.formatting.blade_formatter,
+            nls.formatting.beautysh,
+            nls.formatting.phpcsfixer.with({
+                args = {
+                    '--no-interaction',
+                    '--quiet',
+                    '--config=' .. os.getenv('HOME') .. '/.config/.php-cs-fixer.php',
+                    'fix',
+                    '$FILENAME',
+                },
+            }),
+            nls.builtins.completion.spell,
         })
     end,
 }
