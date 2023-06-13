15a03f3f5300d7b0371347883214c8518c3490da
Jascha030 <contact@jaschavanaalst.nl>
2023-06-13 09:41:14 +0200

    🔧 Diagnostics on hover

--- config/nvim/lua/lsp/keymaps.lua
+++ config/nvim/lua/lsp/keymaps.lua
@@ -16,6 +16,7 @@ function M.on_attach(client, buffer)
     self:map('K', vim.lsp.buf.hover, { desc = 'Hover' })
     self:map('gK', vim.lsp.buf.signature_help, { desc = 'Signature Help', has = 'signatureHelp' })
     self:map('<c-k>', vim.lsp.buf.signature_help, { mode = 'i', desc = 'Signature Help', has = 'signatureHelp' })
+
     self:map(']d', M.diagnostic_goto(true), { desc = 'Next Diagnostic' })
     self:map('[d', M.diagnostic_goto(false), { desc = 'Prev Diagnostic' })
     self:map(']e', M.diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
@@ -24,6 +25,33 @@ function M.on_attach(client, buffer)
     self:map('[w', M.diagnostic_goto(false, 'WARNING'), { desc = 'Prev Warning' })
     self:map('<leader>ca', vim.lsp.buf.code_action, { desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' })
     self:map('<C-a>', vim.lsp.buf.code_action, { desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' })
+
+    function OpenDiagnosticIfNoFloat()
+        for _, winid in pairs(vim.api.nvim_tabpage_list_wins(0)) do
+            if vim.api.nvim_win_get_config(winid).zindex then
+                return
+            end
+        end
+
+        vim.diagnostic.open_float(0, {
+            scope = 'cursor',
+            focusable = false,
+            close_events = {
+                'CursorMoved',
+                'CursorMovedI',
+                'BufHidden',
+                'InsertCharPre',
+                'WinLeave',
+            },
+        })
+    end
+    -- Show diagnostics under the cursor when holding position
+    vim.api.nvim_create_augroup('lsp_diagnostics_hold', { clear = true })
+    vim.api.nvim_create_autocmd({ 'CursorHold' }, {
+        pattern = '*',
+        command = 'lua OpenDiagnosticIfNoFloat()',
+        group = 'lsp_diagnostics_hold',
+    })
 
     local format = require('lsp.format').format
     self:map('<C-f>', format, { desc = 'Format Document', has = 'documentFormatting' })
