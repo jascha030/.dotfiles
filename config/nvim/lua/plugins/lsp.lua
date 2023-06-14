91c622ffb0ab68aaa8c26443393ebf4eedb90df2
Jascha030 <contact@jaschavanaalst.nl>
2023-06-14 22:21:52 +0200

    💄 Disable floating text diagnostics again.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -25,7 +25,8 @@ return {
             diagnostics = {
                 underline = true,
                 update_in_insert = false,
-                virtual_text = { spacing = 4, prefix = '●' },
+                -- virtual_text = { spacing = 4, prefix = '●' },
+                virtual_text = false,
                 severity_sort = true,
             },
             format = {
