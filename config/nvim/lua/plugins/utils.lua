cab9f87bb9a068efe40ca7dbd65c28fada6c5ef0
Jascha030 <contact@jaschavanaalst.nl>
2023-06-27 12:54:50 +0200

    🚚 Move whichkey config to utils.

--- config/nvim/lua/plugins/utils.lua
+++ config/nvim/lua/plugins/utils.lua
@@ -33,4 +33,21 @@ return {
     { 'windwp/nvim-autopairs', opts = {} },
     { 'petertriho/nvim-scrollbar', config = true },
     { 'luukvbaal/stabilize.nvim', config = true },
+    {
+        'folke/which-key.nvim',
+        config = function()
+            local keymaps = require('utils.conf').keymaps
+            local wk = require('which-key')
+
+            for mtype, tmaps in pairs(keymaps) do
+                local results = {}
+
+                for kmap, args in pairs(tmaps) do
+                    results[kmap] = { args[1], args[1] }
+                end
+
+                wk.register(results, { nowait = true, mode = mtype })
+            end
+        end,
+    },
 }
