ecd505d4be8a81da1386d6d835a90eb13a03c603
Jascha030 <contact@jaschavanaalst.nl>
2023-07-06 10:53:00 +0200

    💄 Setup noice to work reasonably well with current config.

--- config/nvim/lua/plugins/ui.lua
+++ config/nvim/lua/plugins/ui.lua
@@ -20,40 +20,40 @@ return {
             require('alpha').setup(opts)
         end,
     },
-    -- {
-    --     'folke/noice.nvim',
-    --     event = 'VeryLazy',
-    --     dependencies = {
-    --         'MunifTanjim/nui.nvim',
-    --     },
-    --     opts = {
-    --         lsp = {
-    --             hover = {
-    --                 enabled = false,
-    --             },
-    --             progress = {
-    --                 enabled = false,
-    --             },
-    --             notify = {
-    --                 enabled = false,
-    --             },
-    --             override = {
-    --                 ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-    --                 ['vim.lsp.util.stylize_markdown'] = true,
-    --                 ['cmp.entry.get_documentation'] = true,
-    --             },
-    --             message = {
-    --                 enabled = false,
-    --             },
-    --         },
-    --         presets = {
-    --             bottom_search = true,         -- use a classic bottom cmdline for search
-    --             command_palette = true,       -- position the cmdline and popupmenu together
-    --             long_message_to_split = true, -- long messages will be sent to a split
-    --             lsp_doc_border = true,        -- add a border to hover docs and signature help
-    --         },
-    --     },
-    -- },
+    {
+        'folke/noice.nvim',
+        event = 'VeryLazy',
+        dependencies = {
+            'MunifTanjim/nui.nvim',
+        },
+        opts = {
+            lsp = {
+                hover = {
+                    enabled = false,
+                },
+                progress = {
+                    enabled = false,
+                },
+                notify = {
+                    enabled = false,
+                },
+                override = {
+                    ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
+                    ['vim.lsp.util.stylize_markdown'] = true,
+                    ['cmp.entry.get_documentation'] = true,
+                },
+                message = {
+                    enabled = false,
+                },
+            },
+            presets = {
+                bottom_search = true, -- use a classic bottom cmdline for search
+                -- command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                lsp_doc_border = true, -- add a border to hover docs and signature help
+            },
+        },
+    },
     'sheerun/vim-polyglot',
     {
         'nvim-neo-tree/neo-tree.nvim',
