c91b5805a7bb85798a94118f42d3105945107910
Jascha030 <contact@jaschavanaalst.nl>
2023-08-17 16:16:26 +0200

    🐛 I was confused.

--- config/nvim/lua/lsp/config/phpactor.lua
+++ config/nvim/lua/lsp/config/phpactor.lua
@@ -1,10 +1,9 @@
 return {
+    root_dir = require('lspconfig.util').root_pattern('composer.json', '.git', 'wp-config-sample.php', 'wp-config.php'),
     setup = {
         init_options = {
-            language_server_phpstan = {
-                enabled = true,
-                bin = '/Users/jaschavanaalst/.phive/phars/phpstan-1.9.6.phar',
-            },
+            ['language_server_phpstan.enabled'] = true,
+            ['language_server_phpstan.bin'] = '/Users/jaschavanaalst/.phive/phars/phpstan-1.9.6.phar',
         },
     },
 }
