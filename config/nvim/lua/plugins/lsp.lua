245d7a914154435e1998cb7b599579fa09a26849
Jascha030 <contact@jaschavanaalst.nl>
2023-08-31 00:58:26 +0200

    ♻️ Utilise Big D*ck lsp stuff in lazy config for lsp.

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -34,7 +34,7 @@ return {
                 },
                 build = function()
                     require('phpactor.handler.update')()
-                end
+                end,
             },
             {
                 'j-hui/fidget.nvim',
@@ -48,6 +48,7 @@ return {
             { 'folke/lua-dev.nvim', lazy = true },
             { 'folke/neodev.nvim', opts = {}, lazy = true },
             { 'ray-x/lsp_signature.nvim' },
+            { 'lvimuser/lsp-inlayhints.nvim', config = true },
             {
                 'saecki/crates.nvim',
                 event = { 'BufRead Cargo.toml' },
@@ -56,7 +57,6 @@ return {
                 lazy = true,
             },
         },
-        ---@class PluginLspOpts
         opts = {
             diagnostics = {
                 underline = true,
@@ -70,42 +70,12 @@ return {
             },
         },
         config = function(_, opts)
-            require('lsp').setup()
-            require('nu').setup({})
-
-            require('core.utils').on_attach(function(client, buffer)
-                local caps = client.server_capabilities
-                -- Enable completion triggered by <C-X><C-O>
-                -- See `:help omnifunc` and `:help ins-completion` for more information.
-                if caps.completionProvider then
-                    vim.bo[buffer].omnifunc = 'v:lua.vim.lsp.omnifunc'
-                end
-
-                require('lsp.keymaps').on_attach(client, buffer)
-
-                if client.name == 'phpactor' then
-                    client.server_capabilities.hoverProvider = false
-                end
-            end)
-
-            -- diagnostics
-            for name, icon in pairs(require('core.icons').icons.diagnostics) do
-                vim.fn.sign_define(name, { text = icon, texthl = name, numhl = '' })
-            end
-
-            vim.diagnostic.config(opts.diagnostics)
-
-            vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
-                silent = true,
-                border = BORDER,
-            })
-
-            vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-                border = BORDER,
-            })
-
-            local get_server_config = require('core.utils').get_server_config
+            local lsp = require('lsp')
             local lspconfig = require('lspconfig')
+
+            lsp.setup(opts)
+
+            require('nu').setup({})
 
             require('mason-lspconfig').setup({
                 ensure_installed = servers,
@@ -113,25 +83,25 @@ return {
             })
 
             require('mason-lspconfig').setup_handlers({
-                function(server)
-                    lspconfig[server].setup(get_server_config(server))
+                function(server_name)
+                    lspconfig[server_name].setup(lsp.get_server_config(server_name))
                 end,
                 ['lua_ls'] = function()
                     require('neodev').setup({})
 
-                    lspconfig.lua_ls.setup(get_server_config('lua_ls'))
+                    lspconfig.lua_ls.setup(lsp.get_server_config('lua_ls'))
                 end,
                 ['rust_analyzer'] = function()
                     require('rust-tools').setup({
                         tools = {
                             reload_workspace_from_cargo_toml = true,
                             inlay_hints = {
-                                auto = true,
+                                auto = false,
                                 only_current_line = false,
                                 show_parameter_hints = true,
                             },
                         },
-                        server = get_server_config('rust_analyzer'),
+                        server = lsp.get_server_config('rust_analyzer'),
                     })
                 end,
                 phpactor = function()
@@ -140,10 +110,12 @@ return {
                             bin = '/usr/local/bin/phpactor',
                         },
                         lspconfig = {
-                            enabled = true,
-                            options = require('lsp.config.phpactor'),
+                            enabled = false,
+                            -- options = require('lsp.config.phpactor'),
                         },
                     })
+
+                    lspconfig.phpactor.setup(lsp.get_server_config('phpactor'))
                 end,
             })
         end,
