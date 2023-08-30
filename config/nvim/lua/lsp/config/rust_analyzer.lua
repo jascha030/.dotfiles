63ecc26c21997fa6861a294814b1f92b2aebdccf
Jascha030 <contact@jaschavanaalst.nl>
2023-08-31 00:56:01 +0200

    🎨 Formatting

--- config/nvim/lua/lsp/config/rust_analyzer.lua
+++ config/nvim/lua/lsp/config/rust_analyzer.lua
@@ -1,22 +1,22 @@
 return {
-	on_attach = function(_, bufnr)
-		local rt = require('rust-tools')
+    on_attach = function(_, bufnr)
+        local rt = require('rust-tools')
 
-		vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-	end,
-	settings = {
-		['rust-analyzer'] = {
-			assist = {
-				importGranularity = 'module',
-				importPrefix = 'by_self',
-			},
-			cargo = {
-				allFeatures = true,
-				loadOutDirsFromCheck = true,
-			},
-			procMacro = {
-				enable = true,
-			},
-		},
-	},
+        vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
+    end,
+    settings = {
+        ['rust-analyzer'] = {
+            assist = {
+                importGranularity = 'module',
+                importPrefix = 'by_self',
+            },
+            cargo = {
+                allFeatures = true,
+                loadOutDirsFromCheck = true,
+            },
+            procMacro = {
+                enable = true,
+            },
+        },
+    },
 }
