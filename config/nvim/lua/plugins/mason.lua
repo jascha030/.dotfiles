c8bd221f963155c99d49e99d5f09225c477cfe77
Jascha030 <contact@jaschavanaalst.nl>
2023-09-06 00:08:38 +0200

    🚚 Move mason config to own file.

---
+++ config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,50 @@
+local M = {
+    'williamboman/mason.nvim',
+    cmd = 'Mason',
+    keys = {
+        { 'm', '<cmd>Mason<cr>', desc = 'Mason' },
+    },
+    opts = {
+        ensure_installed = {
+            'lua-language-server',
+            'rust-analyzer',
+            'stylua',
+            'shellcheck',
+            'shfmt',
+            'flake8',
+            'phpactor',
+        },
+        ui = BORDERS,
+    },
+    lazy = true,
+}
+
+function M.config(_, opts)
+    require('mason').setup(opts)
+
+    local mr = require('mason-registry')
+
+    local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+            local p = mr.get_package(tool)
+            if not p:is_installed() then
+                p:install()
+            end
+        end
+    end
+
+    if mr.refresh then
+        mr.refresh(ensure_installed)
+    else
+        ensure_installed()
+    end
+
+    for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+            p:install()
+        end
+    end
+end
+
+return M
