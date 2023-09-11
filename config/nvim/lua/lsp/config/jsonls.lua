c02add78efe7deb3ff2e1eb497462d7c82f34740
Jascha030 <contact@jaschavanaalst.nl>
2023-09-11 10:11:10 +0200

    🔧 Lsp schemastore for both Json and Yaml.

--- config/nvim/lua/lsp/config/jsonls.lua
+++ config/nvim/lua/lsp/config/jsonls.lua
@@ -2,7 +2,9 @@ return {
     settings = {
         json = {
             schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
+            validate = {
+                enable = true,
+            },
         },
     },
 }
