aff44b37758206ec7db2af0998ab3dd9852dfa67
Jascha030 <contact@jaschavanaalst.nl>
2023-09-27 09:05:36 +0200

    🩹 Hmmm, doesn't fully seem to do what I want it to yet.

--- config/nvim/lua/jascha030/plugins/devicons.lua
+++ config/nvim/lua/jascha030/plugins/devicons.lua
@@ -1,21 +1,13 @@
-local icons = nil
-
 local M = {
     'yamatsum/nvim-nonicons',
     dependencies = { 'kyazdani42/nvim-web-devicons' },
-    opts = {},
+    event = { 'VeryLazy' }
 }
 
-function M.setup()
-    if nil == icons then
-        icons = require('jascha030.devicons')
-    end
+function M.config(_, opts)
+    require('nvim-nonicons').setup(opts)
 
-   ---@diagnostic disable-next-line: undefined-field, need-check-nil 
-    icons.setup(require('jascha030.config').options.devicons)
-
-    require('nvim-web-devicons').set_up_highlights()
- 
+    require('jascha030.config.devicons').setup(require('jascha030').options.devicons)
 end
 
 return M
