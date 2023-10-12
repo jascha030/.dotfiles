2b0863d6b172d3ce560c7fd3f4744fc0fa51b4f1
Jascha030 <contact@jaschavanaalst.nl>
2023-10-13 00:15:03 +0200

    ♻️ Why not as lua...

--- config/nvim/lua/jascha030/lsp/servers/phpactor.lua
+++ config/nvim/lua/jascha030/lsp/servers/phpactor.lua
@@ -2,8 +2,29 @@ return {
     root_dir = require('lspconfig.util').root_pattern('composer.json', '.git', 'wp-config-sample.php', 'wp-config.php'),
     setup = {
         init_options = {
+            -- ['language_server_phpstan.bin'] = '/Users/jaschavanaalst/.phive/phars/phpstan-1.9.6.phar',
+            ['language_server_phpstan.bin'] = '%project_root%/vendor/bin/phpstan',
+            ['language_server_php_cs_fixer.enabled'] = true,
             ['language_server_phpstan.enabled'] = true,
-            ['language_server_phpstan.bin'] = '/Users/jaschavanaalst/.phive/phars/phpstan-1.9.6.phar',
+            ['language_server_php_cs_fixer.bin'] = '/Users/jaschavanaalst/tools/php-cs-fixer',
+            ['language_server_php_cs_fixer.show_diagnostics'] = true,
+            ['phpunit.enabled'] = true,
+            ['completion.dedupe'] = true,
+            ['completion.dedupe_match_fqn'] = true,
+            ['composer.autoloader_path'] = { '%project_root%/vendor/autoload.php' },
+            ['indexer.exclude_patterns'] = {
+                '/vendor/**/Tests/**/*',
+                '/vendor/**/tests/**/*',
+                '/.var/cache/**/*',
+                '/var/cache/**/*',
+                '/vendor-bin/**/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php',
+            },
+            ['indexer.stub_paths'] = {
+                '%project_root%/vendor-bin/phpstan/vendor',
+                '%project_root%/vendor-bin/stubs/vendor',
+                '/Users/jaschavanaalst/.composer/vendor-bin/stubs/vendor/jetbrains/phpstorm-stubs',
+                '/Users/jaschavanaalst/.composer/vendor-bin/stubs/vendor/php-stubs/wordpress-stubs',
+            },
         },
     },
 }
