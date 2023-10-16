95fc3c6fe21560e32672ec92124cb5fde2e2077f
Jascha030 <contact@jaschavanaalst.nl>
2023-10-16 23:37:32 +0200

    ➕ Add folke/flash.nvim

--- config/nvim/lua/jascha030/plugins/utils.lua
+++ config/nvim/lua/jascha030/plugins/utils.lua
@@ -24,6 +24,52 @@ return {
         lazy = true,
     },
     {
+        'folke/flash.nvim',
+        event = 'VeryLazy',
+        keys = {
+            {
+                '<tab>s',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require('flash').jump()
+                end,
+                desc = 'Flash',
+            },
+            {
+                'S',
+                mode = { 'n', 'x', 'o' },
+                function()
+                    require('flash').treesitter()
+                end,
+                desc = 'Flash Treesitter',
+            },
+            {
+                'r',
+                mode = 'o',
+                function()
+                    require('flash').remote()
+                end,
+                desc = 'Remote Flash',
+            },
+            {
+                'R',
+                mode = { 'o', 'x' },
+                function()
+                    require('flash').treesitter_search()
+                end,
+                desc = 'Treesitter Search',
+            },
+            {
+                '<c-s>',
+                mode = { 'c' },
+                function()
+                    require('flash').toggle()
+                end,
+                desc = 'Toggle Flash Search',
+            },
+        },
+    },
+    {
         'phaazon/hop.nvim',
         branch = 'v2',
         name = 'hop',
@@ -38,7 +84,7 @@ return {
         main = 'ibl',
         opts = {
             indent = {
-                char = '│'
+                char = '│',
             },
             exclude = {
                 filetypes = {
