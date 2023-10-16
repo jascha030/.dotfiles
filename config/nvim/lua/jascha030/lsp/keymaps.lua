5d1ef921266a34720f23e4e9391ac3a38dbf5009
Jascha030 <contact@jaschavanaalst.nl>
2023-10-16 23:43:36 +0200

    🚚 Moved Keymap on_attach to main module.

--- config/nvim/lua/jascha030/lsp/keymaps.lua
+++ config/nvim/lua/jascha030/lsp/keymaps.lua
@@ -1,44 +1,7 @@
 ---@class jascha030.lsp.Keymaps
 local M = {}
 
-function M.on_attach(client, bufnr)
-    local self = M.new(client, bufnr)
-
-    local fmt = function()
-        require('jascha030.lsp').format(client, bufnr)
-    end
-
-    self:map('<leader>cd', vim.diagnostic.open_float, { desc = 'Line Diagnostics' })
-    self:map('<leader>cl', 'LspInfo', { desc = 'Lsp Info' })
-    self:map('<leader>xd', 'Telescope diagnostics', { desc = 'Telescope Diagnostics' })
-
-    self:map('gd', 'Telescope lsp_definitions', { desc = 'Goto Definition' })
-    self:map('gr', 'Telescope lsp_references', { desc = 'References' })
-    self:map('gD', 'Telescope lsp_declarations', { desc = 'Goto Declaration' })
-    self:map('gI', 'Telescope lsp_implementations', { desc = 'Goto Implementation' })
-    self:map('gt', 'Telescope lsp_type_definitions', { desc = 'Goto Type Definition' })
-
-    self:map('K', vim.lsp.buf.hover, { desc = 'Hover' })
-    self:map('gK', vim.lsp.buf.signature_help, { desc = 'Signature Help', has = 'signatureHelp' })
-    self:map('<C-k>', vim.lsp.buf.signature_help, { mode = 'i', desc = 'Signature Help', has = 'signatureHelp' })
-
-    self:map(']d', M.diagnostic_goto(true), { desc = 'Next Diagnostic' })
-    self:map('[d', M.diagnostic_goto(false), { desc = 'Prev Diagnostic' })
-    self:map(']e', M.diagnostic_goto(true, 'ERROR'), { desc = 'Next Error' })
-    self:map('[e', M.diagnostic_goto(false, 'ERROR'), { desc = 'Prev Error' })
-    self:map(']w', M.diagnostic_goto(true, 'WARNING'), { desc = 'Next Warning' })
-    self:map('[w', M.diagnostic_goto(false, 'WARNING'), { desc = 'Prev Warning' })
-
-    self:map('<C-a>', vim.lsp.buf.code_action, { desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' })
-    self:map('<leader>a', vim.lsp.buf.code_action, { desc = 'Code Action', mode = { 'n', 'v' }, has = 'codeAction' })
-    self:map('<leader>r', M.rename, { expr = true, desc = 'Rename', has = 'rename' })
-
-    self:map('<C-l>', fmt, {
-        desc = 'Format Document',
-        has = 'documentFormatting',
-    })
-end
-
+---@return jascha030.lsp.Keymaps
 function M.new(client, buffer)
     return setmetatable({ client = client, buffer = buffer }, { __index = M })
 end
