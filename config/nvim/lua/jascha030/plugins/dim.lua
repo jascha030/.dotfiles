36e9e075b9b7a03c5eb8c38de8747fd8f255c194
Jascha030 <contact@jaschavanaalst.nl>
2023-11-06 12:13:26 +0100

    🔧 Fix autoconfig

--- config/nvim/lua/jascha030/plugins/dim.lua
+++ config/nvim/lua/jascha030/plugins/dim.lua
@@ -1,9 +1,11 @@
+---@type LazyPluginSpec
 local M = {
     '0oAstro/dim.lua',
     dependencies = {
         'nvim-treesitter/nvim-treesitter',
         'neovim/nvim-lspconfig',
     },
+    config = true,
 }
 
 return M
