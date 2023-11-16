7cb3554f20df6a418543cf024a3de3053248f712
Jascha030 <contact@jaschavanaalst.nl>
2023-11-16 02:07:22 +0100

    🔥 Meh

--- config/nvim/lua/jascha030/lsp/servers/rust_analyzer.lua
+++ config/nvim/lua/jascha030/lsp/servers/rust_analyzer.lua
@@ -1,9 +1,4 @@
 return {
-    on_attach = function(_, bufnr)
-        local rt = require('rust-tools')
-
-        vim.keymap.set('n', '<Leader>a', rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
     settings = {
         ['rust-analyzer'] = {
             assist = {
