493db4483084b8aeaafa6ded0a5dba6cf7e3729b
Jascha030 <contact@jaschavanaalst.nl>
2023-11-28 00:17:17 +0100

    ♻️ tired of zR-ing

--- config/nvim/after/plugin/autocommands.lua
+++ config/nvim/after/plugin/autocommands.lua
@@ -15,7 +15,7 @@ local function nvim_create_augroups(definitions)
     end
 end
 
--- nvim_create_augroups({ open_folds = { { 'BufReadPost,FileReadPost', '*', 'normal zR' } } })
+nvim_create_augroups({ open_folds = { { 'BufReadPost,FileReadPost', '*', 'normal zR' } } })
 
 vim.api.nvim_create_autocmd('BufRead', {
     group = vim.api.nvim_create_augroup('CmpSourceCargo', { clear = true }),
