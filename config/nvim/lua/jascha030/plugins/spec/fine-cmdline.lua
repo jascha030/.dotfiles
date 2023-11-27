f5a33daba4beae47dd67676c1cb1d064db7508a5
Jascha030 <contact@jaschavanaalst.nl>
2023-11-27 10:40:15 +0100

    💄 Make fine-cmdline look like the one from noice.nvim :P

--- config/nvim/lua/jascha030/plugins/spec/fine-cmdline.lua
+++ config/nvim/lua/jascha030/plugins/spec/fine-cmdline.lua
@@ -1,35 +1,56 @@
+local lreq = require('jascha030.lreq')
+local fineline = lreq('fine-cmdline')
+
 local M = {
     'VonHeikemen/fine-cmdline.nvim',
-    dependencies = {
-        'MunifTanjim/nui.nvim',
-    },
+    dependencies = { 'MunifTanjim/nui.nvim' },
     opts = {
+        cmdline = {
+            prompt = '  ',
+        },
         popup = {
             position = {
-                row = '30%',
+                row = '40%',
                 col = '50%',
             },
             size = {
-                width = '40%',
+                width = '30%',
             },
+            relative = 'editor',
             border = {
+                padding = {
+                    top = 0,
+                    bottom = 0,
+                    left = 2,
+                    right = 2,
+                },
                 style = 'rounded',
+                text = {
+                    top = ' Cmdline ',
+                    top_align = 'center',
+                },
             },
             win_options = {
-                winhighlight = 'Normal:Normal,FloatBorder:FloatBorder',
+                winhighlight = 'Normal:FineCmdlineFloat,FloatBorder:FineFloatBorder',
+                winblend = 0,
             },
+        },
+        hooks = {
+            set_keymaps = function(imap, _)
+                local fn = fineline.fn
+
+                imap('<Esc>', fn.close)
+                imap('<C-c>', fn.close)
+                imap('<Up>', fn.up_search_history)
+                imap('<Down>', fn.down_search_history)
+            end,
         },
     },
     config = function(_, opts)
-        require('fine-cmdline').setup(opts)
+        fineline.setup(opts)
     end,
     keys = {
-        {
-            ':',
-            '<cmd>FineCmdline<CR>',
-            mode = 'n',
-            { noremap = true },
-        },
+        { ':', '<cmd>FineCmdline<CR>', mode = 'n', { noremap = true } },
     },
 }
 
