210f657143601348c15488cf7e56694b9721922d
Jascha030 <contact@jaschavanaalst.nl>
2023-11-29 18:28:40 +0100

    🚧 Slightly less infuriating Noice config.

--- config/nvim/lua/jascha030/plugins/spec/noice.lua
+++ config/nvim/lua/jascha030/plugins/spec/noice.lua
@@ -1,7 +1,7 @@
 local M = {
     'folke/noice.nvim',
     event = 'VeryLazy',
-    cond = false,
+    -- cond = false,
     dependencies = {
         'MunifTanjim/nui.nvim',
         'rcarriga/nvim-notify',
@@ -25,6 +25,8 @@ local M = {
         lsp = {
             hover = {
                 enabled = true,
+                silent = true, -- set to true to not show a message if hover is not available
+                opts = {}, -- merged with defaults from documentation
             },
             progress = {
                 enabled = false,
@@ -43,17 +45,29 @@ local M = {
         },
         cmdline = {
             enabled = true,
+            -- opts = {
+            --     enter = false,
+            --     replace = true,
+            -- },
         },
-        redirect = {
-
+        messages = {
+            enabled = false, -- enables the Noice messages UI
+            view = 'cmdline',
+            -- view = 'cmdline', -- default view for messages
+            view_error = 'mini', -- view for errors
+            view_warn = 'mini', -- view for warnings
+            view_history = 'messages', -- view for :messages
+            view_search = 'virtualtext', -- view for search count messages. Set to `false` to disable
         },
+        redirect = {},
         presets = {
             bottom_search = true, -- use a classic bottom cmdline for search
-            -- command_palette = true, -- position the cmdline and popupmenu together
-            long_message_to_split = true, -- long messages will be sent to a split
-            inc_rename = true, -- enables an input dialog for inc-rename.nvim
-            lsp_doc_border = true, -- add a border to hover docs and signature help
+            command_palette = true, -- position the cmdline and popupmenu together
+            -- long_message_to_split = true, -- long messages will be sent to a split
+            -- inc_rename = true, -- enables an input dialog for inc-rename.nvim
+            -- lsp_doc_border = true, -- add a border to hover docs and signature help
         },
+        views = {},
     },
 }
 
