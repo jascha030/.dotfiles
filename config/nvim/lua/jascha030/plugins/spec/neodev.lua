cd0ba8a116ec14a91c8aa0c1cfdf85f9b5bb4656
Jascha030 <contact@jaschavanaalst.nl>
2023-12-29 13:22:12 +0100

    🔧 Simplify neodev config.

--- config/nvim/lua/jascha030/plugins/spec/neodev.lua
+++ config/nvim/lua/jascha030/plugins/spec/neodev.lua
@@ -1,30 +1,7 @@
 local M = {
     'folke/neodev.nvim',
     name = 'neodev',
-    opts = function()
-        return {
-            pathStrict = false,
-            -- override = function(root_dir, options)
-            --     local util = require('neodev.util')
-            --     -- local util = require('neodev.util')
-            --
-            --     if util.has_file(root_dir, '~/.config/nvim') or util.has_file(root_dir, '~/.dotfiles') then
-            --         options.enabled = true
-            --         options.plugins = true
-            --     end
-            -- end,
-            override = function(_, library)
-                library.enabled = true
-                library.plugins = true
-            end,
-            -- setup_jsonls = false,
-            -- lspconfig = true,
-        }
-    end,
+    lazy = true,
 }
-
--- function M.config(_, opts)
---     require('neodev').setup(opts)
--- end
 
 return M
