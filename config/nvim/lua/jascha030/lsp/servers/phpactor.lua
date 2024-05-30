2fd5a89dc03e97e44def828f16e75c04adfc85ae
Jascha030 <contact@jaschavanaalst.nl>
2024-05-31 00:56:22 +0200

    🐛 Very small mistake... catastrophical consequences.

--- config/nvim/lua/jascha030/lsp/servers/phpactor.lua
+++ config/nvim/lua/jascha030/lsp/servers/phpactor.lua
@@ -1,3 +1,3 @@
 return {
-    root_dir = require('lspconfig.util').root_pattern('composer.json', '.git', 'wp-config-sample.php', 'wp-config.php'),
+    root_dir = require('lspconfig.util').root_pattern('.git', 'wp-config-sample.php', 'wp-config.php', 'composer.json'),
 }
