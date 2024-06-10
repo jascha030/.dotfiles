bb9a3599f2945ae66764daa9b833603434bcf831
Jascha030 <contact@jaschavanaalst.nl>
2024-06-10 13:20:54 +0200

    ✨ More commands

--- config/nvim/after/ftplugin/Trouble.lua
+++ config/nvim/after/ftplugin/Trouble.lua
@@ -1,2 +1,2 @@
-vim.keymap.set('n', 'TT', '<cmd>TroubleClose<CR>', { silent = true })
-vim.keymap.set('n', 'q', '<cmd>TroubleClose<CR>', { silent = true })
+vim.keymap.set('n', 'TT', '<cmd>TroubleClose<CR>', { silent = true, buffer = true })
+vim.keymap.set('n', 'q', '<cmd>TroubleClose<CR>', { silent = true, buffer = true })
