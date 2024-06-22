1376b8e2b8fdf66af39c8a031642ce62c69e1786
Jascha030 <contact@jaschavanaalst.nl>
2024-06-22 22:33:13 +0200

    ➕ Setup neotest for phpunit 🎉

--- config/nvim/lua/jascha030/plugins/neotest.lua
+++ config/nvim/lua/jascha030/plugins/neotest.lua
@@ -1,12 +1,17 @@
 ---@type LazyPluginSpec
 local M = {
     'nvim-neotest/neotest',
-    -- cond = false,
-    lazy = true,
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-treesitter/nvim-treesitter',
-        'loganswartz/neotest-phpunit',
+        'olimorris/neotest-phpunit',
+        'nvim-neotest/nvim-nio',
+        'antoinemadec/FixCursorHold.nvim',
+    },
+    keys = {
+        { '<leader>rr', "<cmd>lua require('neotest').run.run()<cr>", desc = 'Run Nearest Test' },
+        { '<leader>rl', "<cmd>lua require('neotest').run.run_last()<cr>", desc = 'Run Last Test' },
+        { '<leader>RR', "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", desc = 'Run File Tests' },
     },
 }
 
@@ -24,30 +29,34 @@ function M.config()
 
     require('neotest').setup({
         adapters = {
-            require('neotest-phpunit'),
+            require('neotest-phpunit')({
+                phpunit_cmd = function()
+                    return 'tools/phpunit.phar'
+                end,
+            }),
         },
     })
 
-    vim.api.nvim_create_user_command("NeotestRun", function()
-        require("neotest").run.run()
+    vim.api.nvim_create_user_command('NeotestRun', function()
+        require('neotest').run.run()
     end, {})
-    vim.api.nvim_create_user_command("NeotestRunCurrent", function()
-        require("neotest").run.run(vim.fn.expand("%"))
+    vim.api.nvim_create_user_command('NeotestRunCurrent', function()
+        require('neotest').run.run(vim.fn.expand('%'))
     end, {})
-    vim.api.nvim_create_user_command("NeotestRunDap", function()
-        require("neotest").run.run({ strategy = "dap" })
+    vim.api.nvim_create_user_command('NeotestRunDap', function()
+        require('neotest').run.run({ strategy = 'dap' })
     end, {})
-    vim.api.nvim_create_user_command("NeotestStop", function()
-        require("neotest").run.stop()
+    vim.api.nvim_create_user_command('NeotestStop', function()
+        require('neotest').run.stop()
     end, {})
-    vim.api.nvim_create_user_command("NeotestAttach", function()
-        require("neotest").run.attach()
+    vim.api.nvim_create_user_command('NeotestAttach', function()
+        require('neotest').run.attach()
     end, {})
-    vim.api.nvim_create_user_command("NeotestOutput", function()
-        require("neotest").output.open()
+    vim.api.nvim_create_user_command('NeotestOutput', function()
+        require('neotest').output.open()
     end, {})
-    vim.api.nvim_create_user_command("NeotestSummary", function()
-        require("neotest").summary.toggle()
+    vim.api.nvim_create_user_command('NeotestSummary', function()
+        require('neotest').summary.toggle()
     end, {})
 end
 
