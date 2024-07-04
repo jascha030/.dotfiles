bf992e70b29fd32ec758723f23b979638384a7ca
Jascha030 <contact@jaschavanaalst.nl>
2024-07-04 23:16:26 +0200

    🔧 stop stylelint bitching about typescript files that have little to nothing to do related to CSS.

---
+++ config/nvim/lua/jascha030/lsp/servers/stylelint_lsp.lua
@@ -0,0 +1,17 @@
+return {
+    filetypes = {
+        'css',
+        'less',
+        'scss',
+        'sugarss',
+        'vue',
+        'wxss',
+        -- 'javascript',
+        -- 'javascriptreact',
+        -- 'typescript',
+        -- 'typescriptreact',
+    },
+    settings = {
+        stylelintplus = {},
+    },
+}
