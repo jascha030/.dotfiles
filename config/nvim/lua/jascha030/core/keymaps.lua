83599154d1cdc720d1fd64c6087baec9d9aa5460
Jascha030 <contact@jaschavanaalst.nl>
2024-07-15 13:12:38 +0200

    ♻️ Refactor jascha030.core.keymaps and jascha030.init options to use tables that are more aligned with which-key's ne (v3w spec.

--- config/nvim/lua/jascha030/core/keymaps.lua
+++ config/nvim/lua/jascha030/core/keymaps.lua
@@ -1,20 +1,17 @@
 --- @class KeymapsUtil
 local M = {
-    -- maps = {},
-    default_opts = {
-        noremap = true,
-    },
+    default_opts = { noremap = true },
 }
 
 -- @TODO: Store in M.maps, with optional description for which-key.
-function M.map(mtype, lhs, rhs, opts)
-    vim.api.nvim_set_keymap(mtype, lhs, rhs, opts or M.default_opts)
+function M.map(mode, lhs, rhs, opts)
+    vim.api.nvim_set_keymap(mode, lhs, rhs, opts or M.default_opts)
 end
 
 function M.set_keymaps(options)
-    for mtype, type_maps in pairs(options) do
-        for lhs, args in pairs(type_maps) do
-            M.map(mtype, lhs, args[1], args[2] or M.default_opts)
+    for mode, type_maps in pairs(options) do
+        for _, args in pairs(type_maps) do
+            M.map(mode, args[1], args[2], args.opts or M.default_opts)
         end
     end
 end
