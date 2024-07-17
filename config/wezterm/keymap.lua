ea96e3ab60a81d3284d5047d85ea8ef9d534a43e
Jascha030 <contact@jaschavanaalst.nl>
2024-07-17 18:47:12 +0200

    🔧 Tweak pane/tab management related Wezterm Keymappings.

--- config/wezterm/keymap.lua
+++ config/wezterm/keymap.lua
@@ -10,36 +10,36 @@ function M.split_nav(resize_or_move, mods, key, vimKey, dir)
     local event = 'SplitNav_' .. resize_or_move .. '_' .. dir
 
     wezterm.on(event, function(win, pane)
-        if M.is_nvim(pane) then
-            if type(vimKey) == 'string' then
-                -- pass the keys through to vim/nvim
-                win:perform_action({ SendKey = { key = 'w', mods = 'CTRL' } }, pane)
-                win:perform_action({ SendKey = { key = vimKey } }, pane)
-            end
+        -- if M.is_nvim(pane) then
+        --     if type(vimKey) == 'string' then
+        --         -- pass the keys through to vim/nvim
+        --         win:perform_action({ SendKey = { key = 'w', mods = 'CTRL' } }, pane)
+        --         win:perform_action({ SendKey = { key = vimKey } }, pane)
+        --     end
+        -- else
+        if resize_or_move == 'resize' then
+            win:perform_action({ AdjustPaneSize = { dir, 3 } }, pane)
         else
-            if resize_or_move == 'resize' then
-                win:perform_action({ AdjustPaneSize = { dir, 3 } }, pane)
-            else
-                local panes = pane:tab():panes_with_info()
-                local is_zoomed = false
+            local panes = pane:tab():panes_with_info()
+            local is_zoomed = false
 
-                for _, p in ipairs(panes) do
-                    if p.is_zoomed then
-                        is_zoomed = true
-                    end
+            for _, p in ipairs(panes) do
+                if p.is_zoomed then
+                    is_zoomed = true
                 end
-
-                wezterm.log_info('is_zoomed: ' .. tostring(is_zoomed))
-
-                if is_zoomed then
-                    dir = (dir == 'Up' or dir == 'Right') and 'Next' or 'Prev'
-                    wezterm.log_info('dir: ' .. dir)
-                end
-
-                win:perform_action({ ActivatePaneDirection = dir }, pane)
-                win:perform_action({ SetPaneZoomState = is_zoomed }, pane)
             end
+
+            wezterm.log_info('is_zoomed: ' .. tostring(is_zoomed))
+
+            if is_zoomed then
+                dir = (dir == 'Up' or dir == 'Right') and 'Next' or 'Prev'
+                wezterm.log_info('dir: ' .. dir)
+            end
+
+            win:perform_action({ ActivatePaneDirection = dir }, pane)
+            win:perform_action({ SetPaneZoomState = is_zoomed }, pane)
         end
+        -- end
     end)
 
     return {
@@ -54,47 +54,52 @@ function M.is_nvim(pane)
 end
 
 return {
-    { key = 'v', mods = 'CMD',    action = wezterm.action.PasteFrom('Clipboard') },
-    { key = 'c', mods = 'CMD',    action = wezterm.action.CopyTo('ClipboardAndPrimarySelection') },
+    { key = 'v', mods = 'CMD', action = wezterm.action.PasteFrom('Clipboard') },
+    { key = 'c', mods = 'CMD', action = wezterm.action.CopyTo('ClipboardAndPrimarySelection') },
     { key = 's', mods = 'LEADER', action = wezterm.action({ SplitVertical = { domain = 'CurrentPaneDomain' } }) },
     { key = 'v', mods = 'LEADER', action = wezterm.action({ SplitHorizontal = { domain = 'CurrentPaneDomain' } }) },
     { key = 'g', mods = 'LEADER', action = wezterm.action.PaneSelect },
+    { key = 'G', mods = 'LEADER', action = wezterm.action.PaneSelect({ mode = 'SwapWithActive' }) },
+    { key = 'Space', mods = 'LEADER', action = wezterm.action.PaneSelect({ mode = 'SwapWithActive' }) },
+    { key = 'f', mods = 'LEADER', action = wezterm.action.PaneSelect({ mode = 'SwapWithActive' }) },
     {
-        key = 'G',
+        key = 'f',
         mods = 'LEADER',
-        action = wezterm.action.PaneSelect({
-            mode = 'SwapWithActive',
-        })
+        action = wezterm.action.SpawnCommandInNewTab({
+            label = 'Spawn tab in dotfiles dir.',
+            args = { 'zsh', '-l' },
+            cwd = '/Users/jaschavanaalst/.dotfiles/',
+        }),
     },
     M.split_nav('move', 'ALT', 'LeftArrow', 'h', 'Left'),
     M.split_nav('move', 'ALT', 'DownArrow', 'j', 'Down'),
     M.split_nav('move', 'ALT', 'UpArrow', 'k', 'Up'),
     M.split_nav('move', 'ALT', 'RightArrow', 'l', 'Right'),
-    { key = 'l',          mods = 'LEADER',       action = 'ActivateLastTab' },
-    { key = 'c',          mods = 'LEADER',       action = wezterm.action({ SpawnTab = 'CurrentPaneDomain' }) },
-    { key = '&',          mods = 'LEADER|SHIFT', action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
-    { key = 'x',          mods = 'LEADER',       action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
-    { key = 'LeftArrow',  mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Left', 5 } }) },
-    { key = 'DownArrow',  mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Down', 5 } }) },
-    { key = 'UpArrow',    mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Up', 5 } }) },
+    { key = 'l', mods = 'LEADER', action = 'ActivateLastTab' },
+    { key = 'c', mods = 'LEADER', action = wezterm.action({ SpawnTab = 'CurrentPaneDomain' }) },
+    { key = '&', mods = 'LEADER|SHIFT', action = wezterm.action({ CloseCurrentTab = { confirm = true } }) },
+    { key = 'x', mods = 'LEADER', action = wezterm.action({ CloseCurrentPane = { confirm = true } }) },
+    { key = 'LeftArrow', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Left', 5 } }) },
+    { key = 'DownArrow', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Down', 5 } }) },
+    { key = 'UpArrow', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Up', 5 } }) },
     { key = 'RightArrow', mods = 'LEADER|SHIFT', action = wezterm.action({ AdjustPaneSize = { 'Right', 5 } }) },
-    { key = '1',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 0 }) },
-    { key = '2',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 1 }) },
-    { key = '3',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 2 }) },
-    { key = '4',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 3 }) },
-    { key = '5',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 4 }) },
-    { key = '6',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 5 }) },
-    { key = '7',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 6 }) },
-    { key = '8',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 7 }) },
-    { key = '9',          mods = 'LEADER',       action = wezterm.action({ ActivateTab = 8 }) },
-    { key = ']',          mods = 'CMD',          action = wezterm.action.EmitEvent('opacity-up') },
-    { key = '[',          mods = 'CMD',          action = wezterm.action.EmitEvent('opacity-down') },
-    { key = '=',          mods = 'CMD',          action = wezterm.action.EmitEvent('font-size-up') },
-    { key = '-',          mods = 'CMD',          action = wezterm.action.EmitEvent('font-size-down') },
-    { key = 'p',          mods = 'CMD',          action = wezterm.action.EmitEvent('line-height-up') },
-    { key = 'o',          mods = 'CMD',          action = wezterm.action.EmitEvent('line-height-down') },
-    { key = '0',          mods = 'CMD',          action = wezterm.action.EmitEvent('reset-font') },
-    { key = '9',          mods = 'CMD',          action = wezterm.action.EmitEvent('toggle-font') },
-    { key = 'L',          mods = 'CMD',          action = wezterm.action.ShowDebugOverlay },
-    { key = 'P',          mods = 'CMD',          action = wezterm.action.ActivateCommandPalette },
+    { key = '1', mods = 'LEADER', action = wezterm.action({ ActivateTab = 0 }) },
+    { key = '2', mods = 'LEADER', action = wezterm.action({ ActivateTab = 1 }) },
+    { key = '3', mods = 'LEADER', action = wezterm.action({ ActivateTab = 2 }) },
+    { key = '4', mods = 'LEADER', action = wezterm.action({ ActivateTab = 3 }) },
+    { key = '5', mods = 'LEADER', action = wezterm.action({ ActivateTab = 4 }) },
+    { key = '6', mods = 'LEADER', action = wezterm.action({ ActivateTab = 5 }) },
+    { key = '7', mods = 'LEADER', action = wezterm.action({ ActivateTab = 6 }) },
+    { key = '8', mods = 'LEADER', action = wezterm.action({ ActivateTab = 7 }) },
+    { key = '9', mods = 'LEADER', action = wezterm.action({ ActivateTab = 8 }) },
+    { key = ']', mods = 'CMD', action = wezterm.action.EmitEvent('opacity-up') },
+    { key = '[', mods = 'CMD', action = wezterm.action.EmitEvent('opacity-down') },
+    { key = '=', mods = 'CMD', action = wezterm.action.EmitEvent('font-size-up') },
+    { key = '-', mods = 'CMD', action = wezterm.action.EmitEvent('font-size-down') },
+    { key = 'p', mods = 'CMD', action = wezterm.action.EmitEvent('line-height-up') },
+    { key = 'o', mods = 'CMD', action = wezterm.action.EmitEvent('line-height-down') },
+    { key = '0', mods = 'CMD', action = wezterm.action.EmitEvent('reset-font') },
+    { key = '9', mods = 'CMD', action = wezterm.action.EmitEvent('toggle-font') },
+    { key = 'L', mods = 'CMD', action = wezterm.action.ShowDebugOverlay },
+    { key = 'P', mods = 'CMD', action = wezterm.action.ActivateCommandPalette },
 }
