97bc70f0af5538b506952802299291172b4daa55
Jascha030 <contact@jaschavanaalst.nl>
2024-08-20 10:51:28 +0200

    🚚 Move Trouble.lua > trouble.lua

--- config/nvim/after/ftplugin/Trouble.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set('n', 'TT', '<cmd>TroubleClose<CR>', { silent = true, buffer = true })
-vim.keymap.set('n', 'q', '<cmd>TroubleClose<CR>', { silent = true, buffer = true })
---
+++ config/nvim/after/ftplugin/trouble.lua
@@ -0,0 +1,2 @@
+vim.keymap.set('n', 'TT', '<cmd>Trouble close<CR>', { silent = true, buffer = true })
+vim.keymap.set('n', 'q', '<cmd>Trouble close<CR>', { silent = true, buffer = true })
