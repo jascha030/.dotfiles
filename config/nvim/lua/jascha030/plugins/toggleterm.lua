c6a7ad17261a0175770f2b0eb6be35eade843ad2
Jascha030 <contact@jaschavanaalst.nl>
2024-08-29 10:51:33 +0200

    ♻️ Less infuriating keymaps on toggleterm.

--- config/nvim/lua/jascha030/plugins/toggleterm.lua
+++ config/nvim/lua/jascha030/plugins/toggleterm.lua
@@ -3,70 +3,75 @@ local M = {
     'akinsho/toggleterm.nvim',
     lazy = false,
     version = '*',
+    keys = {
+        -- { mode = 't', '<esc><esc>', [[<C-\><C-n>]], buffer = 0, desc = 'Leave terminal mode' },
+        { mode = 't', '<C-t>', [[<C-\><C-n>]], buffer = 0, desc = 'Leave terminal mode' },
+        { mode = 't', '<C-w>', [[:close<CR>]], buffer = 0, desc = 'Close terminal' },
+    },
 }
-
-local silent_opts = {
-    noremap = true,
-    silent = true,
-}
-
-function M.keys(_, _)
-    return {
-        { mode = 't', '<C-t>', [[<C-\><C-n>]], noremap = true, buffer = 0, desc = "Leave terminal mode" },
-        { mode = 't', '<C-w>', [[:close<CR>]], noremap = true, buffer = 0, desc = "Close terminal" },
-    }
-end
 
 function M.config(_, opts)
+    local silent_opts = {
+        noremap = true,
+        silent = true,
+    }
+
     local map = vim.keymap.set
     local toggleterm = require('toggleterm')
     local Terminal = require('toggleterm.terminal').Terminal
     local fpmlog, terms, lazygit = nil, {}, nil
 
-    local function create(cmd, direction, dir)
-        direction = direction or 'float'
-        dir = dir or 'git_dir'
-
+    local function create(cmd, options)
         local term_opts = {
             cmd = cmd,
-            dir = dir,
-            direction = direction,
+            dir = 'git_dir',
+            direction = 'float',
             hidden = true,
             on_open = function(term)
                 vim.cmd('startinsert!')
-                map('t', '<esc><esc>', [[<C-\><C-n>]], { noremap = true, buffer = term.bufnr })
-                map('n', 'q', [[<cmd>close<CR>]], { noremap = true, buffer = term.bufnr })
-            end
+
+                map('t', '<esc><esc>', [[<C-\><C-n>]], { buffer = term.bufnr })
+                map('n', 'q', [[<cmd>close<CR>]], { buffer = term.bufnr })
+            end,
         }
 
-        if direction == 'float' then
+        if options ~= nil then
+            term_opts = vim.tbl_deep_extend('force', term_opts, options)
+        end
+
+        if term_opts.direction == 'float' then
             term_opts.float_opts = BORDERS
         end
 
         return Terminal:new(term_opts)
     end
 
-    function _G.tterm_terminal(direction)
+    local function tterm_terminal(direction)
         direction = direction or 'float'
 
         if terms[direction] == nil then
-            terms[direction] = create('/bin/zsh --login', direction)
+            terms[direction] = create('/bin/zsh --login', { direction = direction })
         end
 
         terms[direction]:toggle()
     end
 
-    function _G.tterm_lazygit()
+    local function tterm_lazygit()
         if lazygit == nil then
-            lazygit = create('lazygit')
+            lazygit = create('lazygit', {
+                on_open = function(term)
+                    vim.cmd('startinsert!')
+                    map('n', 'q', [[<cmd>close<CR>]], { buffer = term.bufnr })
+                end,
+            })
         end
 
         lazygit:toggle()
     end
 
-    function _G.tterm_fpmlog()
+    local function tterm_fpmlog()
         if fpmlog == nil then
-            fpmlog = create('fpmlog', 'horizontal')
+            fpmlog = create('fpmlog', { direction = 'horizontal' })
         end
 
         fpmlog:toggle()
@@ -82,13 +87,17 @@ function M.config(_, opts)
         tterm_terminal('horizontal')
     end, silent_opts)
 
-    map('n', '<leader>t', [[<cmd>lua tterm_terminal()<CR>]], silent_opts)
-    map('n', '<C-t>', [[<cmd>lua tterm_terminal()<CR>]], silent_opts)
-    map('n', '<leader>fl', [[<cmd>lua tterm_fpmlog()<CR>]], silent_opts)
-    map('n', '<leader>g', [[<cmd>lua tterm_lazygit()<CR>]], silent_opts)
-    -- map('n', '<C-t>', [[<cmd>lua tterm_terminal()<CR>]], silent_opts)
-    map('n', '<leader>fl', [[<cmd>lua tterm_fpmlog()<CR>]], silent_opts)
-    map('n', '<leader>g', [[<cmd>lua tterm_lazygit()<CR>]], silent_opts)
+    map('n', '<leader>t', function()
+        tterm_terminal()
+    end, silent_opts)
+
+    map('n', '<leader>fl', function()
+        tterm_fpmlog()
+    end, silent_opts)
+
+    map('n', '<leader>g', function()
+        tterm_lazygit()
+    end, silent_opts)
 end
 
 return M
