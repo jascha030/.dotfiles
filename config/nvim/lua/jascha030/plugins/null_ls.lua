89b53c2c6d725337ff3f45851c7179bf827128c6
Jascha030 <contact@jaschavanaalst.nl>
2024-11-01 15:54:20 +0100

    🐛 Apparently I completely nuked my none/null-ls config the last time.

--- config/nvim/lua/jascha030/plugins/null_ls.lua
+++ config/nvim/lua/jascha030/plugins/null_ls.lua
@@ -5,6 +5,7 @@ local M = {
     'nvimtools/none-ls.nvim',
     name = 'null-ls',
     dependencies = {
+        'nvim-lua/plenary.nvim',
         'nvimtools/none-ls-extras.nvim',
         'gbprod/none-ls-php.nvim',
         'gbprod/none-ls-luacheck.nvim',
@@ -12,6 +13,8 @@ local M = {
         'gbprod/none-ls-psalm.nvim',
         'gbprod/none-ls-ecs.nvim',
     },
+    -- event = { 'BufReadPre', 'BufNewFile' }, -- to enable uncomment this
+    -- lazy = true,
 }
 
 function M.opts()
@@ -40,16 +43,25 @@ function M.opts()
 
     local config_dir = os.getenv('XDG_CONFIG_HOME')
     local cwd = vim.fn.getcwd
+    local code_actions = lreq('null-ls.code-actions')
 
     -- stylua: ignore start
     return {
         debug = false,
         sources = {
-            require('none-ls.code_actions.eslint_d').with({ condition = function(utils) return utils.root_has_file('node_modules/.bin/eslint') end, }),
+            require('none-ls.code_actions.eslint_d'), -- .with({ condition = function(utils) return utils.root_has_file('node_modules/.bin/eslint') end, }),
             nls.builtins.completion.spell,
-            -- Diagnostics 
-            require('none-ls-luacheck.diagnostics.luacheck').with({ condition = function(utils) return utils.root_has_file({ '.luacheckrc' }) end, }),
-            require('none-ls-psalm.diagnostics').with({ condition = function(utils) return utils.root_has_file('psalm.xml') end, }),
+            -- Diagnostics
+            require('none-ls-luacheck.diagnostics.luacheck').with({
+                condition = function(utils)
+                    return utils.root_has_file({ '.luacheckrc' })
+                end,
+            }),
+            require('none-ls-psalm.diagnostics').with({
+                condition = function(utils)
+                    return utils.root_has_file('psalm.xml')
+                end,
+            }),
             nls.builtins.diagnostics.markdownlint,
             nls.builtins.diagnostics.selene.with({ condition = function(utils) return utils.root_has_file({ 'selene.toml' }) end, }),
             nls.builtins.diagnostics.zsh,
@@ -59,27 +71,32 @@ function M.opts()
             }),
             -- Formatting
             require('none-ls.formatting.jq'),
-            require('none-ls.formatting.eslint_d').with({ condition = function(utils) return utils.root_has_file('node_modules/.bin/eslint') end, }),
+            require('none-ls.formatting.eslint_d').with({
+                condition = function(utils)
+                    return utils.root_has_file(
+                        'node_modules/.bin/eslint')
+                end,
+            }),
             require('none-ls-ecs.formatting').with({ condition = function(utils) return utils.root_has_file('ecs.php') end, }),
             nls.builtins.formatting.markdownlint,
             nls.builtins.formatting.black,
             nls.builtins.formatting.blade_formatter,
             nls.builtins.formatting.isort,
             nls.builtins.formatting.shellharden,
-            nls.builtins.formatting.shfmt.with({ filetypes = { 'sh', 'zsh', 'bash' } }),
+            nls.builtins.formatting.shfmt.with({ filetypes = { 'sh', 'bash' } }),
             nls.builtins.formatting.yamlfix,
             nls.builtins.formatting.yamlfmt.with({ filetypes = { 'yaml' } }),
             nls.builtins.formatting.stylua.with({ extra_args = { '--config-path', fb_conf_path(cwd() .. '/stylua.toml', config_dir .. '/stylua.toml'), }, }),
+            require('none-ls-shellcheck.diagnostics'),
+            require('none-ls-shellcheck.code_actions')
         },
     }
     -- stylua: ignore end
 end
 
+--
 function M.config(_, opts)
     nls.setup(opts)
-
-    nls.register(require('none-ls-shellcheck.diagnostics'))
-    nls.register(require('none-ls-shellcheck.code_actions'))
 end
 
 return M
