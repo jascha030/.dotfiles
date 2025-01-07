32ceed3a477fedb17a988397a665f1b460c2d4bb
Jascha030 <contact@jaschavanaalst.nl>
2025-01-07 14:10:37 +0100

    🔧 Add Ghostty vim files to rtp.

--- config/nvim/lua/jascha030/init.lua
+++ config/nvim/lua/jascha030/init.lua
@@ -55,12 +55,19 @@ function M.setup(opts)
     require('jascha030.core.keymaps').set_keymaps(Config.get('keymaps'))
     require('jascha030.core.options').set_opts(Config.get('opts'))
 
-    vim.keymap.set('', '<Esc>', '<cmd>noh<cr><Esc>', { desc = "clears search highlights", noremap = true, silent = true })
+    vim.keymap.set(
+        '',
+        '<Esc>',
+        '<cmd>noh<cr><Esc>',
+        { desc = 'clears search highlights', noremap = true, silent = true }
+    )
 
     vim.cmd([[
 		let &t_TI = "\<Esc>[>4;2m"
 		let &t_TE = "\<Esc>[>4;m"
 	]])
+
+    vim.opt.runtimepath:append(',/Applications/Ghostty.app/Contents/Resources/vim/vimfiles')
 end
 
 return M
