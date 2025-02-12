23e01b8e0ae63a609b15b84107c8fac8f4e31dd2
Jascha030 <contact@jaschavanaalst.nl>
2025-02-12 21:41:51 +0100

    💡

--- config/nvim/lua/jascha030/core/config.lua
+++ config/nvim/lua/jascha030/core/config.lua
@@ -1,6 +1,15 @@
 ---@alias jascha030.core.config.PathConfigOption string[]|table{path: string, prepend: boolean}
 ---@alias jascha030.core.config.PathConfigOptionType "env"|"rtp"
 
+---@class jascha030.core.config.ConfigOptions
+---@field colorscheme boolean|string 
+---@field debug boolean 
+---@field keymaps jascha030.core.config.KeymapConfigOptions
+---@field opts jascha030.core.config.VimConfigOptions
+---@field path jascha030.core.config.PathConfigOptions
+---@field polyglot table
+---@field augroups table
+
 ---@class jascha030.core.config.PathConfigOptions
 ---@field env? jascha030.core.config.PathConfigOption[]
 ---@field rtp? jascha030.core.config.PathConfigOption[]
