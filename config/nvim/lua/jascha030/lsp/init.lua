6bb15c58a0ab37cc759d602bef7f3b6f637906b6
Jascha030 <contact@jaschavanaalst.nl>
2025-04-03 11:58:36 +0200

    🔧 Move config logic from lspconfig to jascha030.lsp

--- config/nvim/lua/jascha030/lsp/init.lua
+++ config/nvim/lua/jascha030/lsp/init.lua
@@ -1,70 +1,32 @@
+-- Disable capabilities for certain clients
+---@todo: maybe just do this per client instead of capability.
+local DISABLED_CAPABILITIES_CLIENTS = {
+    ['documentFormattingProvider'] = {
+        'intelephense',
+        'jsonls',
+        'lua_ls',
+    },
+    ['documentRangeFormattingProvider'] = {
+        'intelephense',
+        'jsonls',
+        'lua_ls',
+    },
+    ['hoverProvider'] = {
+        'phpactor',
+    },
+    ['referencesProvider'] = {
+        'phpactor',
+    },
+}
+
 ---@diagnostic disable: duplicate-doc-field
 
 ---@class jascha030.lsp.Module
 ---@field public keymaps jascha030.lsp.Keymaps
 ---@field public config jascha030.lsp.Config
 local M = {}
-local utils = require('jascha030.utils')
 
-M = setmetatable({}, { __index = utils.create_submod_loader('jascha030.lsp', true) })
-
-vim.lsp.config('*', {
-    capabilities = require('jascha030.lsp.capabilities').make_capabilities(),
-})
-
--- Adds extra inline highlights to the given buffer.
----@param buf integer
-local function add_inline_highlights(buf)
-    local patterns = {
-        ['@%S+'] = '@parameter',
-        ['^%s*(Parameters:)'] = '@text.title',
-        ['^%s*(Return:)'] = '@text.title',
-        ['^%s*(See also:)'] = '@text.title',
-        ['{%S-}'] = '@parameter',
-    }
-
-    for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
-        for pattern, hl_group in pairs(patterns) do
-            ---@type integer? from
-            local from, to = 1, nil
-
-            while from do
-                from, to = line:find(pattern, from)
-                if from then
-                    vim.api.nvim_buf_set_extmark(
-                        buf,
-                        vim.api.nvim_create_namespace('jascha030/lsp_float'),
-                        l - 1,
-                        from - 1,
-                        { end_col = to, hl_group = hl_group }
-                    )
-                end
-                from = to and to + 1 or nil
-            end
-        end
-    end
-end
-
--- HACK: Override `vim.lsp.util.stylize_markdown` to use Treesitter.
----@param bufnr integer
----@param contents string[]
----@param opts table
----@return string[]
----@diagnostic disable-next-line: duplicate-set-field
-vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
-    contents = vim.lsp.util._normalize_markdown(contents, {
-        width = vim.lsp.util._make_floating_popup_size(contents, opts),
-    })
-
-    ---@diagnostic disable-next-line: inject-field
-    vim.bo[bufnr].filetype = 'markdown'
-    vim.treesitter.start(bufnr)
-    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
-
-    add_inline_highlights(bufnr)
-
-    return contents
-end
+M = setmetatable({}, { __index = require('jascha030.utils').create_submod_loader('jascha030.lsp', true) })
 
 ---@param on_attach fun(client, buffer)
 ---@param group string|nil
@@ -120,45 +82,105 @@ function M.inlay_hints(opts)
     end
 end
 
-local function diagnostic_handler(_, result, ctx, config)
-    result.diagnostics = vim.tbl_filter(function(diagnostic)
-        return diagnostic.source ~= 'phpactor'
-    end, result.diagnostics)
+-- Adds extra inline highlights to the given buffer.
+---@param buf integer
+local function add_inline_highlights(buf)
+    local patterns = {
+        ['@%S+'] = '@parameter',
+        ['^%s*(Parameters:)'] = '@text.title',
+        ['^%s*(Return:)'] = '@text.title',
+        ['^%s*(See also:)'] = '@text.title',
+        ['{%S-}'] = '@parameter',
+    }
 
-    vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
+    for l, line in ipairs(vim.api.nvim_buf_get_lines(buf, 0, -1, false)) do
+        for pattern, hl_group in pairs(patterns) do
+            ---@type integer? from
+            local from, to = 1, nil
+
+            while from do
+                from, to = line:find(pattern, from)
+                if from then
+                    vim.api.nvim_buf_set_extmark(
+                        buf,
+                        vim.api.nvim_create_namespace('jascha030/lsp_float'),
+                        l - 1,
+                        from - 1,
+                        { end_col = to, hl_group = hl_group }
+                    )
+                end
+                from = to and to + 1 or nil
+            end
+        end
+    end
 end
 
--- Disable capabilities for certain clients
----@todo: maybe just do this per client instead of capability.
-local DISABLED_CAPABILITIES_CLIENTS = {
-    ['documentFormattingProvider'] = {
-        'intelephense',
-        'jsonls',
-        'lua_ls',
-    },
-    ['documentRangeFormattingProvider'] = {
-        'intelephense',
-        'jsonls',
-        'lua_ls',
-    },
-    ['hoverProvider'] = {
-        'phpactor',
-    },
-    ['referencesProvider'] = {
-        'phpactor',
-    },
-}
+
+-- HACK: Override `vim.lsp.util.stylize_markdown` to use Treesitter.
+---@param bufnr integer
+---@param contents string[]
+---@param opts table
+---@return string[]
+---@diagnostic disable-next-line: duplicate-set-field
+vim.lsp.util.stylize_markdown = function(bufnr, contents, opts)
+    contents = vim.lsp.util._normalize_markdown(contents, {
+        width = vim.lsp.util._make_floating_popup_size(contents, opts),
+    })
+
+    ---@diagnostic disable-next-line: inject-field
+    vim.bo[bufnr].filetype = 'markdown'
+    vim.treesitter.start(bufnr)
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, contents)
+
+    add_inline_highlights(bufnr)
+
+    return contents
+end
 
 function M.setup(opts)
     opts = opts or {}
 
-    -- Configure diagnostics
-    ---@param icon DiagnosticSignIcon
-    for _, icon in pairs(require('jascha030.core.icons').get_diagnostic_signs()) do
-        vim.fn.sign_define(icon.name, { text = icon.text, texthl = icon.name, numhl = icon.name })
-    end
+    local lspconfig = require('lspconfig')
+    local get_server_config = require('jascha030.lsp.config').get_server_config
 
-    vim.diagnostic.config(vim.deepcopy(opts.diagnostics))
+    -- For some reason this one is not available through Mason, so we have to do it manually.
+    opts.servers.sourcekit()
+
+    vim.lsp.config('*', {
+        capabilities = require('jascha030.lsp.capabilities').make_capabilities(),
+        handlers = {
+            -- ['textDocument/publishDiagnostics'] = diagnostic_handler,
+        },
+    })
+
+    require('lspconfig.configs').vtsls = require('vtsls').lspconfig
+    require('lspconfig.ui.windows').default_options.border = BORDER
+    require('mason-lspconfig').setup({
+        automatic_installation = true,
+        ensure_installed = SERVERS,
+        handlers = {
+            function(server)
+                lspconfig[server].setup(get_server_config(server))
+            end,
+            ['rust_analyzer'] = function()
+                require('rust-tools').setup({
+                    server = get_server_config('rust_analyzer'),
+                    tools = {
+                        runnables = {
+                            use_telescope = true,
+                        },
+                        reload_workspace_from_cargo_toml = true,
+                        inlay_hints = {
+                            auto = true,
+                            only_current_line = false,
+                            show_parameter_hints = true,
+                        },
+                    },
+                })
+            end,
+        },
+    })
+
 
     -- Default on_attach handlers
     ---@param client vim.lsp.Client
@@ -177,6 +199,14 @@ function M.setup(opts)
             client.server_capabilities.documentFormattingProvider = true
         end
     end
+
+    vim.iter(vim.api.nvim_get_runtime_file('lua/jascha030/lsp/servers/*.lua', true))
+        :map(function(server_config_path)
+            return vim.fs.basename(server_config_path):match('^(.*)%.lua$')
+        end)
+        :each(function(server_name)
+            vim.lsp.enable(server_name)
+        end)
 
     M.lsp_attach(on_attach)
     M.lsp_attach(M.keymaps.on_attach)
