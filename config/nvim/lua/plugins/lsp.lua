96de1b89daba1c8527901c1cccf833612dbfa21f
Jascha030 <contact@jaschavanaalst.nl>
2025-04-14 22:24:30 +0200

    👽️ Update fidget.nvim config to be compatible with the current version

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -149,9 +149,26 @@ return {
         name = 'fidget',
         event = { 'LspAttach' },
         opts = {
-            text = { spinner = 'dots' },
-            window = { relative = 'editor', blend = 0, zindex = nil },
-            sources = { phpactor = { ignore = true } },
+            progress = {
+                ignore = {
+                    -- 'phpactor',
+                },
+            },
+            notification = {
+                window = {
+                    normal_hl = 'Comment', -- Base highlight group in the notification window
+                    winblend = 0, -- Background color opacity in the notification window
+                    border = 'none', -- Border around the notification window
+                    zindex = 45, -- Stacking priority of the notification window
+                    max_width = 0, -- Maximum width of the notification window
+                    max_height = 0, -- Maximum height of the notification window
+                    x_padding = 1, -- Padding from right edge of window boundary
+                    y_padding = 0, -- Padding from bottom edge of window boundary
+                    align = 'bottom', -- How to align the notification window
+                    -- relative = 'editor', -- What the notification window position is relative to
+                    relative = 'win', -- What the notification window position is relative to
+                },
+            },
         },
     },
     {
