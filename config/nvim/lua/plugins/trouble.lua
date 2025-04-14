6cd7267c457fc69b94b633f8bbd6ee7905479e39
Jascha030 <contact@jaschavanaalst.nl>
2025-04-14 22:24:44 +0200

    🔧 Switch up my keymaps for Trouble.nvim

--- config/nvim/lua/plugins/trouble.lua
+++ config/nvim/lua/plugins/trouble.lua
@@ -3,7 +3,24 @@ local M = {
     'folke/trouble.nvim',
     name = 'trouble',
     keys = {
-        { 'TT', '<cmd>Trouble toggle diagnostics<cr>', mode = 'n', desc = 'Toggle Trouble' },
+        {
+            '<leader>tt',
+            '<cmd>Trouble toggle diagnostics<cr>',
+            mode = 'n',
+            desc = 'Toggle Trouble',
+        },
+        {
+            '<leader>tn',
+            '<cmd>Trouble next<cr>',
+            mode = 'n',
+            desc = 'Next Trouble',
+        },
+        {
+            '<leader>tp',
+            '<cmd>Trouble previous<cr>',
+            mode = 'n',
+            desc = 'Previous Trouble',
+        },
     },
     cmd = {
         'Trouble',
@@ -14,7 +31,7 @@ local M = {
     opts = function(_, opts)
         return vim.tbl_deep_extend('force', opts or {}, {
             position = 'bottom',
-            win_config = { border = BORDER },
+            -- win_config = { border = BORDER },
             use_diagnostic_signs = true,
             picker = {
                 actions = require('trouble.sources.snacks').actions,
