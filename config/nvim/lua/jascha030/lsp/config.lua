58a98b23bb2f342654def42a3c195e6faac3ab01
Jascha030 <contact@jaschavanaalst.nl>
2025-05-11 03:34:22 +0200

    ♻️ Refactor lsp.capabilities into lsp.config submod.

--- config/nvim/lua/jascha030/lsp/config.lua
+++ config/nvim/lua/jascha030/lsp/config.lua
@@ -8,13 +8,72 @@ local function config_error(server, message, level)
     error('Failed to load config for ' .. server .. ' ' .. message, level)
 end
 
-local defaults = {
-    flags = { debounce_text = 150 },
-}
+local function make_capabilities()
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+
+    capabilities.textDocument.foldingRange = {
+        dynamicRegistration = false,
+        lineFoldingOnly = true,
+    }
+
+    capabilities.textDocument.formatting = {
+        dynamicRegistration = false,
+    }
+
+    capabilities.textDocument.semanticTokens.augmentsSyntaxTokens = false
+    capabilities.textDocument.completion.completionItem = {
+        contextSupport = true,
+        snippetSupport = true,
+        deprecatedSupport = true,
+        commitCharactersSupport = true,
+        resolveSupport = {
+            properties = {
+                'documentation',
+                'detail',
+                'additionalTextEdits',
+            },
+        },
+        labelDetailsSupport = true,
+        documentationFormat = { 'markdown', 'plaintext' },
+    }
+
+    capabilities.experimental = {
+        hoverActions = true,
+        hoverRange = true,
+        serverStatusNotification = true,
+        codeActionGroup = true,
+        ssr = true,
+        commands = {
+            'rust-analyzer.runSingle',
+            'rust-analyzer.debugSingle',
+            'rust-analyzer.showReferences',
+            'rust-analyzer.gotoLocation',
+            'editor.action.triggerParameterHints',
+        },
+    }
+
+    capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+
+    return require('blink.cmp').get_lsp_capabilities(capabilities, true)
+end
+
+---@return vim.lsp.ClientConfig
+local function extend(config)
+    local merge = { {}, {
+        capabilities = make_capabilities(),
+        flags = { debounce_text = 150 },
+    } }
+
+    if type(config) == 'table' and not vim.tbl_isempty(config) then
+        table.insert(merge, config)
+    end
+
+    return vim.tbl_deep_extend('force', unpack(merge))
+end
 
 ---@param server string LSP server name
----@return table
-function M.get_server_config(server)
+---@return vim.lsp.ClientConfig
+function M.get(server)
     local ok, config = pcall(require, 'jascha030.lsp.servers.' .. server)
     if not ok then
         config = {}
@@ -30,24 +89,14 @@ function M.get_server_config(server)
             config_error(server, 'provided callback should be a table, got ' .. type(config) .. ' instead', 2)
         end
 
-        return M.extend(config)
+        return extend(config)
     end
 
     if type(config) ~= 'table' then
         config_error(server, 'a table was expected, got ' .. type(config) .. ' instead', 2)
     end
 
-    return M.extend(config)
-end
-
-function M.extend(config)
-    local merge = { {}, vim.deepcopy(defaults) }
-
-    if type(config) == 'table' and not vim.tbl_isempty(config) then
-        table.insert(merge, config)
-    end
-
-    return vim.tbl_deep_extend('force', unpack(merge))
+    return extend(config)
 end
 
 return M
