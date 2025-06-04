c1ce23d09f3ac6010bf9df1963c11ecc4c2c9f98
Jascha030 <contact@jaschavanaalst.nl>
2025-06-04 21:38:18 +0200

    ♻️ Integrate copilot in blink.

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -1,5 +1,6 @@
 local function blink_build(plugin)
     local ret = vim.system({ 'cargo', 'build', '--release' }, { cwd = plugin.dir }):wait()
+
     vim.notify(ret.code == 0 and '[blink.cmp] build success!' or '[blink.cmp] build failed!')
 end
 
@@ -10,30 +11,33 @@ local M = {
         dependencies = {
             'rafamadriz/friendly-snippets',
             'xzbdmw/colorful-menu.nvim',
+            'giuxtaposition/blink-cmp-copilot',
         },
         build = blink_build,
         ---@module "blink.cmp"
         ---@type blink.cmp.Config
         opts = {
-            appearance = {
-                use_nvim_cmp_as_default = true,
-                -- use_nvim_cmp_as_default = false,
-            },
             keymap = {
                 ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
                 ['<C-e>'] = { 'hide' },
-                ['<CR>'] = { 'accept', 'fallback' },
+                ['<CR>'] = { 'select_and_accept', 'fallback' },
                 ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
                 ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
                 ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
                 ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
             },
             completion = {
+                accept = { auto_brackets = { enable = true } },
                 menu = {
+                    border = BORDER,
                     draw = {
-                        -- We don't need label_description now because label and label_description are already
-                        -- combined together in label by colorful-menu.nvim.
-                        columns = { { 'kind_icon' }, { 'label', gap = 1 } },
+                        align_to = 'label',
+                        padding = 1,
+                        gap = 4,
+                        columns = {
+                            { 'label', 'label_description', gap = 1 },
+                            { 'kind_icon', 'kind', gap = 1 },
+                        },
                         components = {
                             label = {
                                 text = function(ctx)
@@ -60,7 +64,7 @@ local M = {
                 ghost_text = { enabled = true },
             },
             sources = {
-                default = { 'lsp', 'path', 'snippets', 'buffer' },
+                default = { 'lsp', 'path', 'snippets', 'buffer', 'copilot' },
                 providers = {
                     lazydev = {
                         name = 'LazyDev',
@@ -72,17 +76,65 @@ local M = {
                         module = 'blink.compat.source',
                         fallbacks = { 'lsp' },
                     },
+                    copilot = {
+                        name = 'copilot',
+                        module = 'blink-cmp-copilot',
+                        score_offset = 100,
+                        async = true,
+                        transform_items = function(_, items)
+                            local CompletionItemKind = require('blink.cmp.types').CompletionItemKind
+                            local kind_idx = #CompletionItemKind + 1
+                            CompletionItemKind[kind_idx] = 'Copilot'
+                            for _, item in ipairs(items) do
+                                item.kind = kind_idx
+                            end
+                            return items
+                        end,
+                    },
                 },
             },
         },
         opts_extend = {
             'sources.default',
         },
+        appearance = {
+            use_nvim_cmp_as_default = false,
+            kind_icons = Jascha030.icons.get_icons().cmp_icons,
+        },
+    },
+    {
+        'zbirenbaum/copilot.lua',
+        cmd = 'Copilot',
+        build = '<cmd>Copilot auth<cr>',
+        event = { 'InsertEnter' },
+        keys = {
+            {
+                '<leader>uC',
+                function()
+                    if require('copilot.client').is_disabled() then
+                        require('copilot.command').enable()
+                    else
+                        require('copilot.command').disable()
+                    end
+                end,
+            },
+        },
+        opts = {
+            enabled = true,
+            suggestion = {
+                enabled = false,
+            },
+            panel = {
+                enabled = false,
+            },
+        },
     },
     {
         'saghen/blink.compat',
         version = false,
-        opts = { impersonate_nvim_cmp = true },
+        opts = {
+            impersonate_nvim_cmp = true,
+        },
     },
     {
         'L3MON4D3/LuaSnip',
