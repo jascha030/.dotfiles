77d4be6f5ae084a6d492c050034a2a9f19514a2e
Jascha030 <contact@jaschavanaalst.nl>
2025-07-28 17:39:54 +0200

    🔧

--- config/nvim/lua/plugins/completion.lua
+++ config/nvim/lua/plugins/completion.lua
@@ -119,6 +119,13 @@ local M = {
         },
         opts = {
             enabled = true,
+            should_attach = function(_, bufname)
+                if string.match(bufname, 'env') then
+                    return false
+                end
+
+                return true
+            end,
             suggestion = {
                 enabled = false,
             },
