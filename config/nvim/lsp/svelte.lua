64200c9d149c66c2167e5cc37759ca8526f52660
Jascha030 <contact@jaschavanaalst.nl>
2025-12-11 00:15:18 +0100

    🔧 Update svelte/ts related lsp configs.

---
+++ config/nvim/lsp/svelte.lua
@@ -0,0 +1,8 @@
+---@diagnostic disable: missing-fields
+return Jascha030.lsp.config_extend({
+    settings = {
+        typescript = {
+            enable = true,
+        },
+    },
+})
--- config/nvim/lsp/vtsls.lua
+++ config/nvim/lsp/vtsls.lua
@@ -1,37 +1,30 @@
 ---@diagnostic disable: missing-fields
-local util = require('lspconfig.util')
-
 local inlayHints = {
-    parameterNames = {
-        enabled = 'all',
-    },
-    parameterTypes = {
-        enabled = true,
-    },
-    variableTypes = {
-        enabled = true,
-    },
-    propertyDeclarationTypes = {
-        enabled = true,
-    },
-    functionLikeReturnTypes = {
-        enabled = true,
-    },
-    enumMemberValues = {
-        enabled = true,
-    },
+    parameterNames = { enabled = 'all' },
+    parameterTypes = { enabled = true },
+    variableTypes = { enabled = true },
+    propertyDeclarationTypes = { enabled = true },
+    functionLikeReturnTypes = { enabled = true },
+    enumMemberValues = { enabled = true },
 }
 
 ---@type vim.lsp.Config
-local vtsls = {
-    -- root_dir = function(filename, _)
-    --     return vim.fs.dirname(vim.fs.find('.git', { path = filename, upward = true })[1])
-    --         or util.root_pattern('.git', 'package.json', 'tsconfig.json', 'jsconfig.json')(filename)
-    -- end,
+return Jascha030.lsp.config_extend({
+    filetypes = {
+        'javascript',
+        'javascriptreact',
+        'javascript.jsx',
+        'typescript',
+        'typescriptreact',
+        'typescript.tsx',
+        'svelte',
+    },
     root_markers = {
         'tsconfig.json',
         'jsconfig.json',
         'package.json',
+        'vite.config.js',
+        'vite.config.ts',
         '.git',
     },
     single_file_support = false,
@@ -42,31 +35,6 @@ local vtsls = {
             autoUseWorkspaceTsdk = true,
         },
         javascript = { inlayHints },
-        typescript = {
-            -- format = {
-            --     enable = false,
-            -- },
-            -- implementationsCodeLens = {
-            --     enabled = true,
-            --     showOnInterfaceMethods = true,
-            -- },
-            inlayHints,
-            -- preferences = {
-            --     useAliasesForRenames = false,
-            -- },
-            -- preferences = {
-            --     useAliasesForRenames = false,
-            --     preferTypeOnlyAutoImports = true,
-            -- },
-            -- tsserver = {
-            -- maxTsServerMemory = 8192,
-            -- },
-            -- referencesCodeLens = {
-            --     showOnAllFunctions = true,
-            --     enabled = true,
-            -- },
-        },
+        typescript = { inlayHints },
     },
-}
-
-return Jascha030.lsp.config_extend(vtsls)
+})
