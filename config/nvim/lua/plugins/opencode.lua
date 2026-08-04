27e49c86cf842ab4f7ad80d0634935e790b43c2c
Jascha030 <contact@jaschavanaalst.nl>
2026-08-04 21:35:07 +0200

    ♻️ Refactor opencode.nvim config due to updates

--- config/nvim/lua/plugins/opencode.lua
+++ config/nvim/lua/plugins/opencode.lua
@@ -1,11 +1,43 @@
 ---@type LazyPluginSpec
 return {
-    'nickjvandyke/opencode.nvim',
-    version = '*',
+    'NickvanDyke/opencode.nvim',
     dependencies = { 'folke/snacks.nvim' },
     event = 'VeryLazy',
     config = function()
+        local opencode_cmd = 'opencode --port'
+        local snacks_terminal_opts = {
+            win = {
+                position = 'right',
+                enter = false,
+            },
+        }
+
+        ---@type opencode.Opts
+        vim.g.opencode_opts = {
+            lsp = {
+                enabled = true,
+            },
+            server = {
+                start = function()
+                    require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+                end,
+            },
+        }
+
         vim.o.autoread = true
+
+        vim.api.nvim_create_autocmd('User', {
+            pattern = { 'OpencodeEvent:tui.command.execute' },
+            callback = function(args)
+                local event = args.data.event
+                if event.properties.command == 'prompt.submit' then
+                    local win = require('snacks.terminal').get(opencode_cmd, { create = false })
+                    if win then
+                        win:show()
+                    end
+                end
+            end,
+        })
     end,
     keys = {
         {
@@ -27,7 +59,12 @@ return {
         {
             '<leader>oo',
             function()
-                require('snacks.terminal').toggle('opencode --port', { win = { position = 'right', enter = true } })
+                require('snacks.terminal').toggle('opencode --port', {
+                    win = {
+                        position = 'right',
+                        enter = true,
+                    },
+                })
             end,
             mode = { 'n', 't' },
             desc = 'Toggle opencode',
